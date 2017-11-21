11ec5d911b9a9097535adbbf4f62957a7ab05356
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2017-11-21 10:22:55 +0100

    (chore)project: initialize project running djangocms-installer
    Initializing the djangocms project using the standard installer.
    Added pipenv as the package manager for improved stability.

---
+++ fun_cms/urls.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from cms.sitemaps import CMSSitemap
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^sitemap\.xml$', sitemap,
+        {'sitemaps': {'cmspages': CMSSitemap}}),
+]
+
+urlpatterns += i18n_patterns(
+    url(r'^admin/', include(admin.site.urls)),  # NOQA
+    url(r'^', include('cms.urls')),
+)
+
+# This is only needed when using runserver.
+if settings.DEBUG:
+    urlpatterns = [
+        url(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        ] + staticfiles_urlpatterns() + urlpatterns
