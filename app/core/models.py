a9fccf90cb31f53dcd9cf66112e08d4f0b8b68a5
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2018-01-24 11:24:43 +0100

    feat(elasticsearch): add command to regenerate ES courses index
    The courses index is behind an alias. When we want to regenerate
    it, a new index is created and populated in bulk. The indexes are
    swapped only when the new index is ready.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,12 @@
+
 from django.db import models
 
-# Create your models here.
+
+def get_courses():
+    """
+    Generator returning all courses.
+
+    TODO: Just here for the POC. Move to the search plugin when it
+          exists and return courses from calls to the Open edX API?
+    """
+    raise NotImplementedError()
