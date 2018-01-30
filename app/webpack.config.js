f754551bbca82abb0187f1680a4a42b42a947c77
Mehdi Benadda <me@mbenadda.com>
2018-01-30 17:38:30 +0100

    chore(tests) add a unit testing environment for our JS
    Make a webpack bundle that will be loaded by karma through a preprocessor
    and use a classic karma/jasmine combination to run the unit tests.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: [ ".ts", ".tsx", ".js", ".json" ]
   },
 
   module: {
