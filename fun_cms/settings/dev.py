367dbbafa5ccd71a22fdc81c4a0b8507d5f2beae
Julien Maupetit <julien@maupetit.net>
2018-02-08 09:39:49 +0100

    feature(settings) switch to django-configurations
    * Add django-configurations in base requirements
    * Move file based settings to class-based configurations:
      - Base
      - Development
      - Test
      - ContinuousIntegration
      - Staging
      - PreProduction
      - Production
    * Move settings parts to class-based mixins in fun_cms/configurations
    * Update wsgi.py and manage.py scripts to use configurations
    * Define an environment file per configuration (e.g. env.d/development)
    * Use this environment file in docker-compose configuration
    * Configure CircleCI environment

--- fun_cms/settings/dev.py
+++
@@ -1,9 +0,0 @@
-from .base import *  # noqa
-
-DEBUG = True
-ALLOWED_HOSTS = ['*']
-
-try:
-    from .local import *  # noqa
-except ImportError:
-    pass
--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fun_cms.settings.dev")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fun_cms.settings')
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
 
-    from django.core.management import execute_from_command_line
+    from configurations.management import execute_from_command_line  # noqa
 
     execute_from_command_line(sys.argv)
