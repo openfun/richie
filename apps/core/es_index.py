2a95a3b57af0bb9803baeb3a40b2558dde7cd044
Mehdi Benadda <me@mbenadda.com>
2018-02-16 10:15:38 +0100

    😎(elasticsearch) refactor regenerate_index command into a module
    We needed to rework the regenerate_index command so it actually performs
    indexation. We needed to make it operate from classes that provide all
    the necessary information to generate one index for one type of document,
    with the relevant mapping and ES-specific keys.
    As we worked on this, we realized it was smarter (and simpler) to put the
    actual logic handling the indexing in a separate module and have the command
    simply use this module.

---
+++ apps/core/es_index.py
@@ -0,0 +1,119 @@
+from itertools import chain
+
+from django.conf import settings
+from django.utils import timezone
+from django.utils.module_loading import import_string
+from elasticsearch.client import IndicesClient
+from elasticsearch.exceptions import NotFoundError
+from elasticsearch.helpers import bulk
+
+
+def get_indexable_from_string(indexable_module_string):
+    """
+    Load the indexable from the module.Class path in settings
+    """
+    IndexableClass = import_string(indexable_module_string)
+    return IndexableClass()
+
+
+def get_indexes_by_alias(existing_indexes, alias):
+    """
+    Get existing index(es) for an alias. Support multiple existing aliases so the command
+    always 'cleans up' the aliases even if they got messed up somehow.
+    """
+    for index, details in existing_indexes.items():
+        if alias in details.get('aliases', {}):
+            yield index, alias
+
+
+def perform_create_index(indexable, logger):
+    """
+    Create a new index in ElasticSearch from an indexable instance
+    """
+    indices_client = IndicesClient(client=settings.ES_CLIENT)
+    # Create a new index name, suffixing its name with a timestamp
+    new_index = '{:s}_{:s}'.format(
+        indexable.index_name,
+        timezone.now().strftime('%Y-%m-%d-%Hh%Mm%S.%fs'),
+    )
+
+    # Create the new index
+    logger.info('Creating a new Elasticsearch index "{:s}"...'.format(new_index))
+    indices_client.create(index=new_index)
+    indices_client.put_mapping(
+        body=indexable.mapping,
+        doc_type=indexable.document_type,
+        index=new_index,
+    )
+
+    # Populate the new index with data provided from our indexable class
+    bulk(
+        actions=indexable.get_data_for_es(new_index, 'create'),
+        chunk_size=settings.ES_CHUNK_SIZE,
+        client=settings.ES_CLIENT,
+        stats_only=True,
+    )
+
+    # Return the name of the index we just created in ElasticSearch
+    return new_index
+
+
+def regenerate_indexes(logger):
+    """
+    Create new indexes for our indexables and replace possible existing indexes with
+    a new one only once it has successfully built it.
+    """
+    # Prepare the client we'll be using to handle indexes
+    indices_client = IndicesClient(client=settings.ES_CLIENT)
+
+    # Get all existing indexes once; we'll look up into this list many times
+    try:
+        existing_indexes = indices_client.get_alias('*')
+    except NotFoundError:
+        # Provide a fallback empty list so we don't have to check for its existence later on
+        existing_indexes = []
+
+    # Dynamically import the modules from the config mixin strings
+    indexables = list(map(get_indexable_from_string, settings.ES_INDEXES))
+
+    # Create a new index for each of those modules
+    # NB: we're mapping perform_create_index which produces side-effects
+    indexes_to_create = zip(
+        list(map(lambda ix: perform_create_index(ix, logger), indexables)),
+        indexables,
+    )
+
+    # Prepare to alias them so they can be swapped-in for the previous versions
+    actions_to_create_aliases = [
+        {'add': {'index': index, 'alias': ix.index_name}} for index, ix in indexes_to_create
+    ]
+
+    # Get the previous indexes for every alias
+    indexes_to_unalias = map(
+        lambda ix: get_indexes_by_alias(existing_indexes, ix.index_name),
+        indexables,
+    )
+
+    # Prepare to un-alias them so they can be swapped-out for the new versions
+    # NB: use chain to flatten the list of generators
+    actions_to_delete_aliases = [
+        {'remove': {'index': index, 'alias': alias}} for index, alias in chain(
+            indexes_to_unalias,
+        )
+    ]
+
+    # Identify orphaned indexes
+    # NB: we *must* do this before the update_aliases call so we don't immediately prune
+    # version n-1 of all our indexes
+    useless_indexes = [
+        index for index, details in existing_indexes.items() if 'aliases' not in details
+    ]
+
+    # Replace the old indexes with the new ones in 1 atomic operation to avoid outage
+    indices_client.update_aliases(dict(
+        actions=actions_to_create_aliases + actions_to_delete_aliases
+    ))
+
+    # Cleanup step: do prune older indexes that are now useless
+    for useless_index in useless_indexes:
+        indices_client.delete(index=useless_index, ignore=[400, 404])
---
+++ apps/core/tests/test_es_index.py
@@ -0,0 +1,190 @@
+from datetime import datetime
+from unittest import mock
+
+import pytz
+
+from django.conf import settings
+from django.test import TestCase
+from django.test.utils import override_settings
+from django.utils import timezone
+from elasticsearch.client import IndicesClient
+
+from ..es_index import get_indexes_by_alias, perform_create_index, regenerate_indexes
+
+
+class ESIndexTestCase(TestCase):
+    """
+    Test the functions that generate and maintain our elasticsearch indexes.
+    """
+
+    def setUp(self):
+        """
+        Make sure all indexes are deleted before each new test is run.
+        """
+        super().setUp()
+        self.indices_client = IndicesClient(client=settings.ES_CLIENT)
+        self.indices_client.delete(index='_all')
+
+    def test_get_indexes_by_alias(self, *args):
+        """
+        Receive a generator that contains the n-1 index for an alias.
+        """
+        alias = 'fun_cms_courses'
+        existing_indexes = {
+            'fun_cms_courses_2014-05-04-03h12m33.123456s': {},
+            'fun_cms_courses_2015-05-04-03h12m33.123456s': {
+                'aliases': {'fun_cms_courses': True},
+            },
+            'fun_cms_organizations_2017-05-04-03h12m33.123456s': {
+                'aliases': {'fun_cms_organizations': True},
+            },
+        }
+        self.assertEqual(list(get_indexes_by_alias(existing_indexes, alias)), [
+            ('fun_cms_courses_2015-05-04-03h12m33.123456s', 'fun_cms_courses'),
+        ])
+
+    def test_get_indexes_by_alias_with_duplicate(self, *args):
+        """
+        Clean up the aliases when starting from a broken state: duplicate indexes for an alias.
+        """
+        alias = 'fun_cms_courses'
+        existing_indexes = {
+            'fun_cms_courses_2013-05-04-03h12m33.123456s': {},
+            'fun_cms_courses_2014-05-04-03h12m33.123456s': {
+                'aliases': {'fun_cms_courses': True},
+            },
+            'fun_cms_courses_2015-05-04-03h12m33.123456s': {
+                'aliases': {'fun_cms_courses': True},
+            },
+            'fun_cms_organizations_2017-05-04-03h12m33.123456s': {
+                'aliases': {'fun_cms_organizations': True},
+            },
+        }
+        self.assertEqual(list(get_indexes_by_alias(existing_indexes, alias)), [
+            ('fun_cms_courses_2014-05-04-03h12m33.123456s', 'fun_cms_courses'),
+            ('fun_cms_courses_2015-05-04-03h12m33.123456s', 'fun_cms_courses'),
+        ])
+
+    def test_get_indexes_by_alias_empty(self, *args):
+        """
+        Don't wrongly push values when there is nothing to return.
+        """
+        alias = 'fun_cms_courses'
+        existing_indexes = {
+            'fun_cms_courses_2013-05-04-03h12m33.123456s': {},
+            'fun_cms_organizations_2017-05-04-03h12m33.123456s': {
+                'aliases': {'fun_cms_organizations': True},
+            },
+        }
+        self.assertEqual(list(get_indexes_by_alias(existing_indexes, alias)), [])
+
+    # Make sure indexing still works when the number of records is higher than chunk size
+    @override_settings(ES_CHUNK_SIZE=2)
+    def test_perform_create_index(self, *args):
+        """
+        Perform all side-effects through the ES client and return the index name (incl. timestamp)
+        """
+        # Create an indexable from scratch that mimicks the expected shape of the dynamic
+        # import in es_index
+        class IndexableClass():
+            document_type = 'course'
+            index_name = 'fun_cms_courses'
+            mapping = {
+                'properties': {
+                    'code': {'type': 'keyword'},
+                    'name': {'type': 'text'},
+                },
+            }
+
+            def get_data_for_es(self, index, action):
+                for i in range(0, 10):
+                    yield {
+                        '_id': i,
+                        '_index': index,
+                        '_op_type': action,
+                        '_type': 'course',
+                        'code': 'course-{:d}'.format(i),
+                        'name': 'Course Number {:d}'.format(i),
+                    }
+        indexable = IndexableClass()
+
+        # Set a fake time to check the name of the index
+        now = datetime(2016, 5, 4, 3, 12, 33, 123456, tzinfo=pytz.utc)
+
+        # Make sure our index is empty before we call the function
+        self.assertEqual(self.indices_client.get_alias('*'), {})
+
+        mock_logger = mock.Mock(spec=['info'])
+
+        with mock.patch.object(timezone, 'now', return_value=now):
+            new_index = perform_create_index(indexable, mock_logger)
+        self.indices_client.refresh()
+
+        self.assertEqual(new_index, 'fun_cms_courses_2016-05-04-03h12m33.123456s')
+        self.assertEqual(settings.ES_CLIENT.count()['count'], 10)
+        self.assertEqual(self.indices_client.get_mapping(), {
+            'fun_cms_courses_2016-05-04-03h12m33.123456s': {
+                'mappings': {
+                    'course': {
+                        'properties': {
+                            'code': {'type': 'keyword'},
+                            'name': {'type': 'text'},
+                        },
+                    },
+                },
+            },
+        })
+        mock_logger.info.assert_called()
+
+    @mock.patch.object(settings, 'ES_INDEXES', [
+        'example.ExOneIndexable',
+        'example.ExTwoIndexable',
+    ])
+    @mock.patch(
+        'apps.core.es_index.get_indexable_from_string',
+        lambda name: ExOneIndexable() if name == 'example.ExOneIndexable' else ExTwoIndexable(),
+    )
+    @mock.patch(
+        'apps.core.es_index.get_indexes_by_alias',
+        side_effect=lambda existing_indexes, alias: (alias + '_previous', alias),
+    )
+    @mock.patch(
+        'apps.core.es_index.perform_create_index',
+        side_effect=lambda ix, *args: ix.index_name + '_created_index',
+    )
+    @mock.patch('elasticsearch.client.IndicesClient.delete')
+    @mock.patch(
+        'elasticsearch.client.IndicesClient.get_alias',
+        return_value=dict({'fun_cms_orphan': {}}),
+    )
+    @mock.patch('elasticsearch.client.IndicesClient.update_aliases')
+    def test_regenerate_indexes(self, *args):
+        """
+        Now test the general index regeneration behavior. We don't need to test the side-effects
+        again as those have mostly been tested through perform_create_index.
+        We mostly have to ensure the proper calls happen and the correct actions/deletions are
+        passed to the indices_client.
+        """
+        regenerate_indexes(None)
+
+        self.indices_client.update_aliases.assert_called_with({
+            'actions': [
+                {'add': {'index': 'fun_cms_example_created_index', 'alias': 'fun_cms_example'}},
+                {'add': {'index': 'fun_cms_stub_created_index', 'alias': 'fun_cms_stub'}},
+                {'remove': {'index': 'fun_cms_example_previous', 'alias': 'fun_cms_example'}},
+                {'remove': {'index': 'fun_cms_stub_previous', 'alias': 'fun_cms_stub'}}
+            ],
+        })
+        self.indices_client.delete.assert_called_with(ignore=[400, 404], index='fun_cms_orphan')
+
+
+class ExOneIndexable():
+    document_type = 'example'
+    index_name = 'fun_cms_example'
+    mapping = {'properties': {'name': 'text'}}
+
+
+class ExTwoIndexable():
+    document_type = 'stub'
+    index_name = 'fun_cms_stub'
+    mapping = {'properties': {'code': 'keyword'}}
