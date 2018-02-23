7e7c21a7d8b3c6e09e344c1c64965ae9647c5e8f
Mehdi Benadda <me@mbenadda.com>
2018-02-23 18:02:27 +0100

    😎(search) add a smoke test for the  CourseGlimpse component
    Make sure the component renders and includes the basic data we've
    deemed necessary to display on a course.

---
+++ fun_cms/js/components/courseGlimpse/courseGlimpse.spec.tsx
@@ -0,0 +1,24 @@
+import '../../testSetup.spec';
+
+import { render } from 'enzyme';
+import * as React from 'react';
+
+import Course from '../../types/Course';
+import CourseGlimpse from './courseGlimpse';
+
+describe('components/courseGlimpse', () => {
+  it('renders a course glimpse with its data', () => {
+    const course = {
+      main_university: { name: 'Example university' },
+      start_date: '2018-03-12T08:00:00Z',
+      thumbnails: { small: '/thumbs/small.png' },
+      title: 'Course 42',
+    } as Course;
+    const wrapper = render(<CourseGlimpse course={course} />);
+
+    expect(wrapper.html()).toContain('Course 42');
+    expect(wrapper.html()).toContain('Example university');
+    expect(wrapper.find('img').attr('src')).toContain('/thumbs/small.png');
+    expect(wrapper.html()).toContain('Starts on Mar 12, 2018');
+  });
+});
