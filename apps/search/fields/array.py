2ea3ce7d4b3cc19177fdc7d7de1d8061c589aede
Mehdi Benadda <me@mbenadda.com>
2018-03-14 21:33:52 +0100

    😎(search) implement a custom "Array of {FormField}" form field
    Implement a custom Django form field type to validate and clean
    arrays of values, validating both the array itself and each of
    the items it contains, through the appropriate Django form field
    type (such as CharField or IntegerField).
    This will be useful for some API request query params that can
    pass arrays of UUIDs or slugs to filter searches or return an
    exact matching list.

---
+++ apps/search/fields/array.py
@@ -0,0 +1,43 @@
+"""
+Utility Django form fields that lets us validate every individual entry in an array,
+using the Django-provided validator for that type of field
+"""
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class ArrayField(forms.Field):
+    """
+    Custom form field type that lets us validate and clean arrays of items, where both the array
+    itself and each of its items are validated through the Django Form
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Get a hold of our custom arguments and delegate the rest to Field
+        """
+        # base_type is unknown to Field: pop it out of kwargs and keep it ourselves
+        self.base_type = kwargs.pop('base_type')
+        # Core arguments, including 'required', are left in the kwargs and handled by parent
+        super().__init__(*args, **kwargs)
+
+    def clean(self, value):
+        """
+        Validate our array and iterate over it, delegating item validation to the base_type that
+        was passed to us by the consumer
+        """
+        if value:
+            # We have something to check: attempt to iterate over it
+            try:
+                for base_type_value in value:
+                    self.base_type.validate(base_type_value)
+                return [self.base_type.clean(base_type_value) for base_type_value in value]
+            # Iteration over subvalues failed, the value was invalid
+            except TypeError:
+                raise ValidationError('Failed to iterate over value, got a non-iterable type.')
+        # We're missing a required value
+        elif self.required:
+            raise ValidationError('Missing required value.')
+        # Default to returning an empty array when an optional value is missing to simplify
+        # the job for consumers of the form's data
+        else:
+            return []
---
+++ apps/search/tests/test_form_field_array.py
@@ -0,0 +1,82 @@
+"""
+Tests for the ArrayField custom django form field
+"""
+from django import forms
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.utils import translation
+
+from ..fields.array import ArrayField
+
+
+class ArrayFieldTestCase(TestCase):
+    """
+    Test whether our custom form field returns the proper value when it is valid and
+    properly reports it when it is invalid
+    """
+    def setUp(self):
+        """
+        Force i18n language so we can check the value of error messages without having our tests
+        broken when the local language on the host machine changes
+        """
+        translation.activate('en')
+
+    def test_array_of_valid_int(self):
+        """
+        Happy path: the value is an array and all its items are valid as per the base_type
+        """
+        # Create an ArrayField instance with some params
+        array_of_int = ArrayField(required=False, base_type=forms.IntegerField(min_value=1))
+        # The field is valid and returns cleaned data
+        self.assertEqual(array_of_int.clean([1, 2, 3, 5, 7]), [1, 2, 3, 5, 7])
+
+    def test_array_of_invalid_string(self):
+        """
+        Invalid input: the value is an array but at least 1 item is invalid
+        """
+        # Create an ArrayField instance with some params
+        array_of_string = ArrayField(required=False, base_type=forms.CharField(max_length=4))
+        # Pass invalid values as per our CharField: the field raises an error
+        with self.assertRaises(ValidationError) as context:
+            array_of_string.clean(['ok', 'too_long', 'still'])
+        self.assertEqual(
+            context.exception.messages[0],
+            'Ensure this value has at most 4 characters (it has 8).',
+        )
+
+    def test_missing_required_array(self):
+        """
+        Invalid input: the value is required but missing
+        """
+        # Create an ArrayField instance with some params
+        array_of_string = ArrayField(required=True, base_type=forms.CharField(max_length=4))
+        # The missing value causes the field to raise an error
+        with self.assertRaises(ValidationError) as context:
+            array_of_string.clean(None)
+        self.assertEqual(
+            context.exception.message,
+            'Missing required value.',
+        )
+
+    def test_optional_array_not_provided(self):
+        """
+        Happy path: the value is optional and was not provided
+        """
+        # Create an ArrayField instance with some params
+        array_of_string = ArrayField(required=False, base_type=forms.CharField(max_length=4))
+        # None is valid input and the field returns an empty array
+        self.assertEqual(array_of_string.clean(None), [])
+
+    def test_single_value(self):
+        """
+        Invalid input: ArrayField expects an iterable type
+        """
+        # Create an ArrayField instance with some params
+        array_of_string = ArrayField(required=False, base_type=forms.IntegerField(min_value=1))
+        # The single value, although it would be valid in a list, is not valid by itself
+        with self.assertRaises(ValidationError) as context:
+            array_of_string.clean(3.14)
+        self.assertEqual(
+            context.exception.message,
+            'Failed to iterate over value, got a non-iterable type.',
+        )
