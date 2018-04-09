71b7b88ee6346c79692121ab5057783d5ff6ea20
Mehdi Benadda <me@mbenadda.com>
2018-04-09 18:01:35 +0200

    ✨(front) implement the reducer that handles the course list
    Simply store the latest query in the course subtree of the state.
    Complete it when new items arrive for the same query, replace it
    when a new, different query arrives.

--- richie/js/data/course/reducer.spec.ts
+++ richie/js/data/course/reducer.spec.ts
@@ -52,18 +52,108 @@ describe('data/course reducer', () => {
     expect(courseReducer(previousState, { type: 'TODO_ADD' })).toEqual(previousState);
   });
 
-  it('adds the course to the state when called with COURSE_ADD', () => {
-    const previousState = {
-      byId: { 43: course43  },
-    };
-    expect(courseReducer(previousState, {
-      course: course44,
-      type: 'COURSE_ADD',
-    })).toEqual({
-      byId: {
-        43: course43,
-        44: course44,
-      },
+  describe('COURSE_ADD', () => {
+    it('adds the course to the state', () => {
+      const previousState = {
+        byId: { 43: course43 },
+      };
+      expect(courseReducer(
+        previousState,
+        {
+          course: course44,
+          type: 'COURSE_ADD',
+        },
+      )).toEqual({
+        byId: {
+          43: course43,
+          44: course44,
+        },
+      });
+    });
+  });
+
+  describe('COURSE_LIST_GET_SUCCESS', () => {
+    it('creates a new query when there is none', () => {
+      const previousState = {
+        byId: { 43: course43 },
+      };
+      expect(courseReducer(
+        previousState,
+        {
+          apiResponse: {
+            meta: { limit: 12, offset: 2, total_count: 4 },
+            objects: [ course43, course44 ],
+          },
+          params: { limit: 12, offset: 2 },
+          type: 'COURSE_LIST_GET_SUCCESS',
+        },
+      )).toEqual({
+        byId: { 43: course43 },
+        currentQuery: { items: { 2: 43, 3: 44 }, queryKey: '{}', total_count: 4 },
+      });
+    });
+
+    it('replaces the existing query if the new one has different params', () => {
+      const previousState = {
+        byId: { 43: course43 },
+        currentQuery: { items: { 2: 43, 3: 44 }, queryKey: '{}', total_count: 4 },
+      };
+      expect(courseReducer(
+        previousState,
+        {
+          apiResponse: {
+            meta: { limit: 2, offset: 0, total_count: 240 },
+            objects: [ course44, course43 ],
+          },
+          params: { limit: 2, match: 'some query', offset: 0 },
+          type: 'COURSE_LIST_GET_SUCCESS',
+        },
+      )).toEqual({
+        byId: { 43: course43 },
+        currentQuery: { items: { 0: 44, 1: 43 }, queryKey: '{"match":"some query"}', total_count: 240 },
+      });
+    });
+
+    it('completes the existing query if the new one shares the same params', () => {
+      const course45 = {
+        end_date: '2018-04-30T06:00:00.000Z',
+        enrollment_end_date: '2018-02-28T06:00:00.000Z',
+        enrollment_start_date: '2018-02-01T06:00:00.000Z',
+        id: 45,
+        language: 'de',
+        organizations: [98],
+        session_number: 1,
+        short_description: 'Nunc a risus faucibus, pretium ante et, convallis neque.',
+        start_date: '2018-03-01T06:00:00.000Z',
+        subjects: [44, 29],
+        thumbnails: {
+          about: 'https://example.com/about_45.png',
+          big: 'https://example.com/big_45.png',
+          facebook: 'https://example.com/facebook_45.png',
+          small: 'https://example.com/small_45.png',
+        },
+        title: 'Datenstrukturen und algorithmen in Python',
+      };
+
+      const previousState = {
+        byId: { 43: course43 },
+        currentQuery: { items: { 0: 44, 1: 43 }, queryKey: '{"match":"some query"}', total_count: 240 },
+      };
+
+      expect(courseReducer(
+        previousState,
+        {
+          apiResponse: {
+            meta: { limit: 1, offset: 2, total_count: 240 },
+            objects: [ course45 ],
+          },
+          params: { limit: 1, match: 'some query', offset: 2 },
+          type: 'COURSE_LIST_GET_SUCCESS',
+        },
+      )).toEqual({
+        byId: { 43: course43 },
+        currentQuery: { items: { 0: 44, 1: 43, 2: 45 }, queryKey: '{"match":"some query"}', total_count: 240 },
+      });
     });
   });
 });
--- richie/js/data/course/reducer.ts
+++ richie/js/data/course/reducer.ts
@@ -1,7 +1,8 @@
+import get from 'lodash-es/get';
 import { Reducer } from 'redux';
 
 import Course from '../../types/Course';
-import { CourseAdd } from './actions';
+import { CourseAdd, CourseListGetSuccess } from './actions';
 
 const initialState = {};
 
@@ -9,11 +10,19 @@ export interface CourseState {
   byId?: {
     [id: string]: Course;
   };
+  currentQuery?: {
+    // A number-keyed object is more stable than an array to keep a list with a moving starting
+    // index and potential gaps throughout.
+    // NB: we still use string as the index type as keys of an objects are always converted to strings
+    items: { [index: string]: Course['id'] };
+    queryKey: string;
+    total_count: number;
+  };
 }
 
 export const course: Reducer<CourseState> = (
   state: CourseState = initialState,
-  action?: CourseAdd,
+  action?: CourseAdd | CourseListGetSuccess,
 ) => {
   if (!action) { return state; } // Compiler needs help
   switch (action.type) {
@@ -26,8 +35,29 @@ export const course: Reducer<CourseState> = (
           [action.course.id]: action.course,
         },
       };
-  }
 
+    // Create or update the latest course list we fetched from the server
+    case 'COURSE_LIST_GET_SUCCESS':
+      const { objects, meta } = action.apiResponse;
+      // Generate a generic representation of our query as a string with pagination params removed
+      const { limit, offset = 0, ...cleanQuery } = action.params;
+      const queryKey = JSON.stringify(cleanQuery);
+
+      return {
+        ...state,
+        currentQuery: {
+          items: objects.reduce(
+            // Transform the array into an object with indexes as keys
+            (acc, item, index) => ({ ...acc, [offset + index]: item.id }),
+            // Extend the items list if we're receiving more items for the query we have in memory
+            // (e.g. for pagination), replace it otherwise
+            queryKey === get(state, 'currentQuery.queryKey') ? { ...state.currentQuery.items } : {},
+          ),
+          queryKey,
+          total_count: meta.total_count,
+        },
+      };
+  }
   return state;
 };
 
