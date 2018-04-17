c8baf3357a3c114c15228a1a5545e4f972d34411
Mehdi Benadda <me@mbenadda.com>
2018-04-17 12:26:00 +0200

    🔥(front) remove subject & organization fixtures
    Now that we're getting subjects & organizations from the API, we
    don't need to bootstrap the store with our fixtures anymore. Keep the
    configureStore module around as we might have other init data to
    put there later on.

--- richie/js/bootstrap.ts
+++ richie/js/bootstrap.ts
@@ -1,26 +1,7 @@
 import configureStore from './data/configureStore';
-import organizations from './fixtures/organizations';
-import subjects from './fixtures/subjects';
 
 export default function bootstrapStore() {
   return configureStore({
-    resources: {
-      organization: {
-        byId: organizations.results.reduce((acc, organization) => {
-          return {
-            ...acc,
-            [organization.id]: organization,
-          };
-        }, {}),
-      },
-      subject: {
-        byId: subjects.results.reduce((acc, subject) => {
-          return {
-            ...acc,
-            [subject.id]: subject,
-          };
-        }, {}),
-      },
-    },
+    resources: {},
   });
 }
