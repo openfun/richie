ded1af05381923673b058eede42bb05f94bc14bc
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2018-04-22 16:52:40 +0200

    ✅(pages) add test to secure multilingual page rendering
    We have mostly unit tests on this project, so we need to
    start securing how pages are rendered on the CMS. This
    tests makes sure we can browse the site in different
    languages included in the path.

---
+++ apps/core/tests/test_pages.py
@@ -0,0 +1,35 @@
+"""
+Test suite for all utils in the `core` application
+"""
+from cms.api import Page
+from cms.test_utils.testcases import CMSTestCase
+
+from ..helpers import create_i18n_page
+
+
+class CreateCmsDataTests(CMSTestCase):
+    """Create CMS data base test case"""
+
+    def test_pages_i18n(self):
+        """
+        Create an i18n page and check its rendering on the site
+        """
+        content = {'fr': 'Tableau de bord', 'en': 'Dashboard'}
+        create_i18n_page(
+            content,
+            is_homepage=True,
+            published=True,
+            template='richie/fullwidth.html',
+        )
+        # Get the root page in french...
+        root = Page.objects.get_home()
+        response = self.client.get(root.get_absolute_url('fr'))
+        self.assertEqual(200, response.status_code)
+        # ... and make sure the page menu is present in french on the page
+        self.assertIn(content['fr'], response.rendered_content)
+
+        # Get the root page in english...
+        response = self.client.get(root.get_absolute_url('en'))
+        self.assertEqual(200, response.status_code)
+        # ... and make sure the page menu is present in english on the page
+        self.assertIn(content['en'], response.rendered_content)
