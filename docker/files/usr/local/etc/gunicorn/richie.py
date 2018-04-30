c1407cffe1d51a9fde08b8e0b0da5ff3506016c4
Julien Maupetit <julien@maupetit.net>
2018-04-30 11:22:36 +0200

    🔧(gunicorn) redirect logs to stdout/stderr
    Refactor gunicorn configuration file to:
    
    * decrease the log level to INFO
    * redirect error logs to stderr
    * redirect access logs to stdout
    
    Logging to stderr/stdout is considered as a good practice when using
    Docker for a server like nginx, apache, or gunicorn [1].
    
    [1] https://docs.docker.com/config/containers/logging/

--- docker/files/usr/local/etc/gunicorn/richie.py
+++ docker/files/usr/local/etc/gunicorn/richie.py
@@ -1,8 +1,16 @@
 # Gunicorn-django settings
-bind = ['0.0.0.0:8000']
+bind = ["0.0.0.0:8000"]
+name = "richie"
+python_path = "/app"
+
+# Run
 graceful_timeout = 90
-loglevel = 'error'
-name = 'richie'
-python_path = '/app'
 timeout = 90
 workers = 3
+
+# Logging
+# Using '-' for the access log file makes gunicorn log accesses to stdout
+accesslog = "-"
+# Using '-' for the error log file makes gunicorn log errors to stderr
+errorlog = "-"
+loglevel = "info"
