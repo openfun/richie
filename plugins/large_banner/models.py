ed6ff9f8b9f32d7307df69a8d440315b63ee26a1
Julien Maupetit <julien@maupetit.net>
2018-05-02 10:26:33 +0200

    🚨(pylint) ignore false positive warnings

--- plugins/large_banner/models.py
+++ plugins/large_banner/models.py
@@ -7,6 +7,15 @@ from django.utils.translation import ugettext_lazy as _
 from filer.fields.image import FilerImageField
 
 
+# pylint: disable=model-no-explicit-unicode,line-too-long
+#
+# We choose to ignore the 'model-no-explicit-unicode' since we are running
+# python 3, and pylint-django fails at detecting the python 2 compatibility
+# layer [1] handled by Django CMS [2]. And we also ignore the 'line-too-long'
+# warning caused by reference urls.
+#
+# [1] https://docs.djangoproject.com/en/1.11/ref/utils/#django.utils.encoding.python_2_unicode_compatible # noqa
+# [2] https://github.com/divio/django-cms/blob/3.5.2/cms/models/pluginmodel.py#L171
 class LargeBanner(CMSPlugin):
     """
     Model to configure a home page banner with background image, logo and title.
