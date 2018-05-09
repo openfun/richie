84f44d2fe043b71cdc144e07b314d0552c45dadc
Mehdi Benadda <me@mbenadda.com>
2018-05-09 17:55:32 +0200

    ✨(front) show active search filters to let users remove them
    Put active filters at the top of the list and highlight them. This
    way users can know at a glance which filter(s) are active.
    Update SearchFilter to call removeFilter instead of addFilter on click
    if the filter is already active.

--- richie/js/components/searchFilter/searchFilter.tsx
+++ richie/js/components/searchFilter/searchFilter.tsx
@@ -5,15 +5,21 @@ import { FilterValue } from '../../types/FilterDefinition';
 export interface SearchFilterProps {
   addFilter: (filterValue: string) => void;
   filter: FilterValue;
+  isActive: boolean;
+  removeFilter: (filterValue: string) => void;
 }
 
 export const SearchFilter = (props: SearchFilterProps) => {
-  const { filter, addFilter } = props;
+  const { addFilter, filter, isActive, removeFilter } = props;
 
   return (
     <button
-      className="search-filter"
-      onClick={() => addFilter(filter.primaryKey)}
+      className={`search-filter ${isActive ? 'active' : ''}`}
+      onClick={() =>
+        isActive
+          ? removeFilter(filter.primaryKey)
+          : addFilter(filter.primaryKey)
+      }
     >
       {filter.humanName}
       {filter.count || filter.count === 0 ? (
--- richie/js/components/searchFilterGroup/searchFilterGroup.spec.tsx
+++ richie/js/components/searchFilterGroup/searchFilterGroup.spec.tsx
@@ -20,6 +20,7 @@ describe('components/searchFilterGroup', () => {
     const wrapper = shallow(
       <SearchFilterGroup
         addFilter={addFilter}
+        currentValue={undefined}
         filter={filter}
         removeFilter={removeFilter}
       />,
@@ -39,11 +40,53 @@ describe('components/searchFilterGroup', () => {
     const wrapper = shallow(
       <SearchFilterGroup
         addFilter={addFilter}
+        currentValue={undefined}
         filter={filter}
         removeFilter={removeFilter}
       />,
     );
 
     expect(wrapper.find(SearchFilter).length).toEqual(2);
+  });
+
+  it('orders the list by putting active filters at the top', () => {
+    const filter = {
+      humanName: 'Example filter',
+      values: [
+        { primaryKey: 'value-1', humanName: 'Value One' },
+        { primaryKey: 'value-2', humanName: 'Value Two' },
+        { primaryKey: 'value-3', humanName: 'Value Three' },
+      ],
+    } as FilterDefinition;
+    const wrapper = shallow(
+      <SearchFilterGroup
+        addFilter={addFilter}
+        currentValue={'value-2'}
+        filter={filter}
+        removeFilter={removeFilter}
+      />,
+    );
+
+    expect(
+      wrapper
+        .find(SearchFilter)
+        .at(0)
+        .shallow()
+        .text(),
+    ).toContain('Value Two');
+    expect(
+      wrapper
+        .find(SearchFilter)
+        .at(1)
+        .shallow()
+        .text(),
+    ).toContain('Value One');
+    expect(
+      wrapper
+        .find(SearchFilter)
+        .at(2)
+        .shallow()
+        .text(),
+    ).toContain('Value Three');
   });
 });
