57e135d5470a3d8a0e4219acc2e5f3c673865983
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2018-05-16 14:26:53 +0200

    🔧(toolbar) allow to configure position of toolbar menu items
    There is no easy way to determine automatically the position at
    which we should insert the page extension menu items so that
    they are positioned below the item to edit a page. We should
    make it a setting so it can be ajusted depending on what other
    menu items are present on an instance.

---
+++ apps/courses/defaults.py
@@ -0,0 +1,9 @@
+"""
+Settings specific to the courses application
+"""
+from django.conf import settings
+
+
+PAGE_EXTENSION_TOOLBAR_ITEM_POSITION = getattr(
+    settings, "RICHIE_PAGE_EXTENSION_TOOLBAR_ITEM_POSITION", 4
+)
