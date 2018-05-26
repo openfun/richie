5d13786ade3214345598527925d17a5309568a0a
Mehdi Benadda <me@mbenadda.com>
2018-05-26 15:31:56 -0400

    ♻️types) improve typings for API_ENDPOINTS setting
    Use an enum type for the keys so we can easily constrain
    values elsewhere, and use that enum type for the index
    typing for the object.

--- richie/js/settings.d.ts
+++ richie/js/settings.d.ts
@@ -1,9 +1,6 @@
 declare module '*settings.json' {
-  export const API_ENDPOINTS: {
-    courses: string;
-    organizations: string;
-    subjects: string;
-  };
+  export type API_ENDPOINTS_KEYS = 'courses' | 'organizations' | 'subjects';
+  export const API_ENDPOINTS: { [key in API_ENDPOINTS_KEYS]: string };
 
   export const API_LIST_DEFAULT_PARAMS: {
     limit: number;
