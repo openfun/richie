946168810535803765b271a2be605e5ee2e64743
David THENON <sveetch@gmail.com>
2018-07-19 16:09:32 +0200

    ✨(ckeditor) create a new simple text plugin with CKEditor widget
    Common 'TextPlugin' plugin from djangocms_text_ckeditor has bad
    behavior of creating orphan plugin required for its nesting plugin
    feature. This behavior is breaking plugin limit constraint from
    setting 'CMS_PLACEHOLDER_CONF'.
    
    So we are creating our own plugin to a add text with CKEditor UI.
    It does not accept nesting plugin, don't append orphan plugin and
    respect constraint from setting 'CMS_PLACEHOLDER_CONF'. Plugins
    readme has been updated also for some minor fixes.

---
+++ tests/plugins/simple_text_ckeditor/test_model.py
@@ -0,0 +1,69 @@
+"""
+Model tests
+"""
+from django.test import TestCase
+
+from richie.plugins.simple_text_ckeditor.models import SimpleText
+
+
+class SimpleTextTests(TestCase):
+    """Model tests case"""
+
+    def test_html5_cleaner_unclosed_tag(self):
+        """
+        HTML5lib Cleaner fix unclosed element.
+        """
+
+        content = (
+            """<p><a href="http://perdu.com/" class="foo">Lorem """
+            """ipsum</a> dolor"""
+        )
+        attempted = (
+            """<p><a href="http://perdu.com/" class="foo">Lorem """
+            """ipsum</a> dolor</p>"""
+        )
+
+        instance = SimpleText(body=content)
+
+        instance.save()
+
+        self.assertEqual(instance.body, attempted)
+
+    def test_html5_sanitize_tag(self):
+        """
+        HTML5lib Cleaner escape potential malicious tag like iframe.
+
+        Behavior can be modified with settings:
+
+        https://github.com/divio/djangocms-text-ckeditor#configurable-sanitizer
+        """
+
+        content = """<iframe src="http://perdu.com/" border=0></iframe>"""
+        attempted = (
+            """&lt;iframe src="http://perdu.com/" """
+            """border="0"&gt;&lt;/iframe&gt;"""
+        )
+
+        instance = SimpleText(body=content)
+
+        instance.save()
+
+        self.assertEqual(instance.body, attempted)
+
+    def test_html5_sanitize_attribute(self):
+        """
+        HTML5lib Cleaner remove uncommon tag attribute.
+
+        Behavior can be modified with settings:
+
+        https://github.com/divio/djangocms-text-ckeditor#configurable-sanitizer
+        """
+
+        content = """<div class="foo" my-own-attr="bar">Yipee</div>"""
+        attempted = """<div class="foo">Yipee</div>"""
+
+        instance = SimpleText(body=content)
+
+        instance.save()
+
+        self.assertEqual(instance.body, attempted)
