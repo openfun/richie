167dd9dcb62564553d5af509ba0675a21098802d
Truong David <david.truong@edulib.org>
2018-07-30 11:48:39 -0400

    ✨(courses) add organization plugin
    This allows us to add a preview of organizations
    as a plugin in placeholders.It shows the logo and
    the title.

---
+++ src/richie/apps/courses/cms_plugins.py
@@ -0,0 +1,26 @@
+"""
+Organization CMS plugin
+"""
+
+from django.utils.translation import ugettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from .models import OrganizationPluginModel
+
+
+@plugin_pool.register_plugin
+class OrganizationPlugin(CMSPluginBase):
+    """
+    Organization plugin displays a organization's information on other pages
+    """
+
+    model = OrganizationPluginModel
+    module = _("Courses")
+    render_template = "courses/plugins/organization.html"
+    cache = True
+
+    def render(self, context, instance, placeholder):
+        context.update({"instance": instance, "placeholder": placeholder})
+        return context
