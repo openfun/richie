f069b94afbbd8279a60b6ad3e0b1b173f77e0a86
Samuel Paccoud - FUN MOOC <samuel@sampaccoud.com>
2018-08-29 11:23:31 +0200

    ✅(courses) add tests for the course run
    Start by adding a factory, testing the factory and
    finally testing that course runs behave as expected.

---
+++ tests/apps/courses/test_factories_course_run.py
@@ -0,0 +1,39 @@
+"""
+Unit tests for the CourseRun factory
+"""
+from unittest import mock
+
+from django.test import TestCase
+from django.utils import timezone
+
+from richie.apps.courses.factories import CourseRunFactory
+
+
+class CourseRunFactoriesTestCase(TestCase):
+    """
+    Unit test suite to validate the behavior of the CourseRun factory
+    """
+
+    def test_factories_course_run_random_dates(self):
+        """
+        The random dates computed for the course run should make sense.
+        """
+        now = timezone.now()
+        with mock.patch.object(timezone, "now", return_value=now):
+            course_run = CourseRunFactory()
+
+        self.assertTrue(now <= course_run.start)
+        self.assertTrue(
+            course_run.enrollment_start <= course_run.start <= course_run.end
+        )
+        self.assertTrue(
+            course_run.enrollment_start <= course_run.enrollment_end <= course_run.end
+        )
+
+    def test_factories_course_run_null_dates(self):
+        """
+        Setting a date to None should not make the factory fail.
+        """
+        for field in ["start", "end", "enrollment_start", "enrollment_end"]:
+            course_run = CourseRunFactory(**{field: None})
+            self.assertIsNone(getattr(course_run, field))
