edea29db8004ebf7b8abc760dcc4d3b6d519c8e6
Samuel Paccoud - FUN MOOC <samuel@sampaccoud.com>
2018-08-29 15:51:40 +0200

    🐛(courses) public courses have the course runs of their draft version
    The bug was that when publishing a draft course with course runs, the
    course runs were not accessible any more from the published course.
    This is because course runs are basic Django models, not DjangoCMS
    page extensions... so they don't have draft/public versions. We could
    have copied the relation between a draft course and its course runs
    to the public course but it would have required a ManyToMany field
    and more complexity when in fact we don't want to duplicate the course
    runs. So we chose to have course runs attached only to the draft version
    of a course and abstract fetching the course runs so that for a public
    course we go and fetch the course runs from the related draft course.

---
+++ tests/apps/courses/test_models_course_run.py
@@ -0,0 +1,38 @@
+"""
+Unit tests for the Course model
+"""
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from richie.apps.courses.factories import CourseFactory, CourseRunFactory
+
+
+class CourseRunModelsTestCase(TestCase):
+    """
+    Unit test suite to validate the behavior of the CourseRun model
+    """
+
+    def test_models_course_run_fields_course_required(self):
+        """
+        The `course` field should be required
+        """
+        with self.assertRaises(ValidationError) as context:
+            CourseRunFactory(course=None)
+        self.assertEqual(context.exception.messages[0], "This field cannot be null.")
+
+    def test_models_course_run_fields_course_runs(self):
+        """
+        The "course_runs" field should always return the course runs linked to the draft version
+        of the course.
+        """
+        # Create a draft course with course runs
+        course = CourseFactory()
+        course_runs = set(CourseRunFactory.create_batch(2, course=course))
+
+        # The course runs should be accessible from the course
+        self.assertEqual(set(course.course_runs), course_runs)
+
+        # The published course should point to the same course runs
+        course.extended_object.publish("en")
+        course.refresh_from_db()
+        self.assertEqual(set(course.public_extension.course_runs), course_runs)
