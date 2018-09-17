7174f76ab4bdcc098e14edbb1b9f0789c097a8b7
Truong David <david.truong@edulib.org>
2018-09-17 16:18:14 -0400

    👌(courses) update organization_plugin following review
    Update to the organization_plugin html to show:
     -published org only on published page.
     -published org on draft page unless there is
      no published org in which case show the draft org
    This corresponds to how organizations show up on course pages.
    
    Streamline the style file for organization plugin.
    
    Update the tests related to organization plugin to make sure draft
    organization aren't shown on published page.
    
    Change the related_name of organization plugin model to correspond
    to how foreign key relate to other object.

--- src/richie/apps/courses/models/organization.py
+++ src/richie/apps/courses/models/organization.py
@@ -98,12 +98,13 @@ class OrganizationPluginModel(CMSPlugin):
     Organization plugin model handles the relation from OrganizationPlugin
     to their Organization instance
     """
+
     organization = models.ForeignKey(
-            "Organization",
-            related_name="organization",
-            limit_choices_to={"extended_object__publisher_is_draft": True},
-        )
-    
+        "Organization",
+        related_name="plugin_model",
+        limit_choices_to={"extended_object__publisher_is_draft": True},
+    )
+
     class Meta:
         verbose_name = _("organization plugin model")
 
