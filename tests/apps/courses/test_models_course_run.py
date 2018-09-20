789e6f5e8509509d4d80ab4760c9765aa63bb153
Samuel Paccoud - FUN MOOC <samuel@sampaccoud.com>
2018-09-20 17:56:42 +0200

    ✨(courserun) add method attribute to retrieve state of dates
    We need to have an easy way to know what is the status of a
    course run ie whether it is open, closed, on-going, etc at the
    current time. This will be used to display a call to action or
    just some information in the list of course run on a course page.

--- tests/apps/courses/test_models_course_run.py
+++ tests/apps/courses/test_models_course_run.py
@@ -1,8 +1,11 @@
 """
 Unit tests for the Course model
 """
+from datetime import timedelta
+
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.utils import timezone
 
 from richie.apps.courses.factories import CourseFactory, CourseRunFactory
 from richie.apps.courses.models import CourseRun
@@ -12,6 +15,10 @@ class CourseRunModelsTestCase(TestCase):
     """
     Unit test suite to validate the behavior of the CourseRun model
     """
+
+    def setUp(self):
+        super().setUp()
+        self.now = timezone.now()
 
     def test_models_course_run_fields_course_required(self):
         """
@@ -45,3 +52,73 @@ class CourseRunModelsTestCase(TestCase):
         course.extended_object.publish("en")
         course.refresh_from_db()
         self.assertEqual(set(course.public_extension.course_runs.all()), course_runs)
+
+    def test_models_course_run_state_archived(self):
+        """A course run that is passed should return a state "is_archived"."""
+        course_run = CourseRunFactory(
+            start=self.now - timedelta(hours=2), end=self.now - timedelta(hours=1)
+        )
+        self.assertEqual(course_run.state, "is_archived")
+
+    def test_models_course_run_state_ongoing_open(self):
+        """
+        A course run that is on-going and open for enrollment should return a state `is_open`.
+        """
+        course_run = CourseRunFactory(
+            enrollment_start=self.now - timedelta(hours=3),
+            start=self.now - timedelta(hours=2),
+            enrollment_end=self.now + timedelta(hours=1),
+            end=self.now + timedelta(hours=2),
+        )
+        self.assertEqual(course_run.state, "is_open")
+
+    def test_models_course_run_state_ongoing_closed(self):
+        """
+        A course run that is on-going but closed for enrollment should return a state `is_ongoing`.
+        """
+        course_run = CourseRunFactory(
+            enrollment_start=self.now - timedelta(hours=3),
+            start=self.now - timedelta(hours=2),
+            enrollment_end=self.now - timedelta(hours=1),
+            end=self.now + timedelta(hours=1),
+        )
+        self.assertEqual(course_run.state, "is_ongoing")
+
+    def test_models_course_run_state_coming(self):
+        """
+        A course run that is future and not yet open for enrollment should return a state
+        `is_coming`.
+        """
+        course_run = CourseRunFactory(
+            enrollment_start=self.now + timedelta(hours=1),
+            enrollment_end=self.now + timedelta(hours=2),
+            start=self.now + timedelta(hours=3),
+            end=self.now + timedelta(hours=4),
+        )
+        self.assertEqual(course_run.state, "is_coming")
+
+    def test_models_course_run_state_future_open(self):
+        """
+        A course run that is future and not yet open for enrollment should return a state
+        `is_coming`.
+        """
+        course_run = CourseRunFactory(
+            enrollment_start=self.now - timedelta(hours=1),
+            enrollment_end=self.now + timedelta(hours=1),
+            start=self.now + timedelta(hours=2),
+            end=self.now + timedelta(hours=3),
+        )
+        self.assertEqual(course_run.state, "is_open")
+
+    def test_models_course_run_state_future_closed(self):
+        """
+        A course run that is future and already closed for enrollment should return a state
+        `is_closed`.
+        """
+        course_run = CourseRunFactory(
+            enrollment_start=self.now - timedelta(hours=2),
+            enrollment_end=self.now - timedelta(hours=1),
+            start=self.now + timedelta(hours=1),
+            end=self.now + timedelta(hours=2),
+        )
+        self.assertEqual(course_run.state, "is_closed")
