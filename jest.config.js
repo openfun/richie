3d27cab5d86855decb5a88bd11a575f57f3ecba6
Mehdi Benadda <me@mbenadda.com>
2018-09-27 15:25:35 +0200

    🔧(front) configure jest & update babel config for jest
    As we're moving to jest, we need to add a Jest configuration file
    to manage our file locations & our compiling settings. Move from
    .babelrc to babel.config.json so we can trivially require it in
    our Jest config.
    
    Note: we need the `transformIgnorePatterns` key to ensure lodash-es
    goes through compilation. It is the only dependency we use that is
    not precompiled by the library author.

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      babelConfig: require('./babel.config.js'),
+    },
+  },
+  moduleFileExtensions: ['css', 'js', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '\\.(css)$': '<rootDir>/front/__mocks__/styleMock.js',
+  },
+  testMatch: [`${__dirname}/src/richie-front/js/**/*.spec.+(ts|tsx|js)`],
+  testURL: 'https://localhost',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(lodash-es)/)'],
+  preset: 'ts-jest/presets/default',
+};
