77024a114fa44360d83c1c3b563d4db3ea452584
Manuel Raynaud <manu@raynaud.io>
2018-10-04 20:49:30 +0200

    🚸(front) enable usage of redux dev tools extension
    [redux devtools
    extension](https://github.com/zalmoxisus/redux-devtools-extension)
    is a nice browser extension allowing to follow state change in realtime.
    It's a great tools when developing with react + redux.
    
    this tool can be enable only in development if need. The author wrote a
    nice article about that :
    https://medium.com/@zalmoxis/using-redux-devtools-in-production-4c5b56c5600f

--- src/richie-front/js/data/configureStore.ts
+++ src/richie-front/js/data/configureStore.ts
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import { rootReducer, RootState } from './rootReducer';
@@ -7,10 +7,12 @@ import { rootSaga } from './rootSaga';
 const sagaMiddleware = createSagaMiddleware();
 
 export function configureStore(preloadedState: RootState) {
+  const composeEnhancers =
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(sagaMiddleware),
+    composeEnhancers(applyMiddleware(sagaMiddleware)),
   );
 
   sagaMiddleware.run(rootSaga);
