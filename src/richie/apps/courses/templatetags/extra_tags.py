6cacbf7657b2659957637a4612e58cece669b8f4
Samuel Paccoud - FUN MOOC <samuel@sampaccoud.com>
2018-10-31 15:33:09 +0100

    ✨(courses) add "page_placeholder" template tag
    We want to reuse the same template to display the course content on
    the course detail page and on the course run detail page. For this
    we need a template tag that works like the "placeholder" template
    tag but allowing us to specify the page from which the placeholder
    should be taken. When the specified page happens to be the current
    page, the "page_placeholder" shoud behave like the "placeholder"
    template tag. When the specified page is different from the current
    page, the "page_placeholder" should behave like the "show_placeholder"
    template tag and just display its content as readonly.
    
    We also added the possibility to assign the output of the
    "page_placeholder" template tag to a context variable, which was not
    possible on the "placeholder" tag. This will be useful to avoid
    displaying empty blocks when the output of the tag is null thanks to
    the following pattern:
    {% page_placeholder "slot" page as var %}
    {% if var %}
    <h1>{{ var }}</h1>
    {% endif %}

---
+++ src/richie/apps/courses/templatetags/extra_tags.py
@@ -0,0 +1,97 @@
+"""Custom template tags for the courses application of Richie."""
+from django import template
+
+from classytags.arguments import Argument, MultiValueArgument
+from cms.exceptions import PlaceholderNotFound
+from cms.templatetags.cms_tags import (
+    Placeholder,
+    PlaceholderOptions,
+    _get_page_by_untyped_arg,
+)
+from cms.toolbar.utils import get_toolbar_from_request
+from cms.utils import get_site_id
+from cms.utils.placeholder import validate_placeholder_name
+
+# pylint: disable=invalid-name
+register = template.Library()
+
+
+class PagePlaceholder(Placeholder):
+    """
+    This template node is used to output page content and
+    is also used in the admin to dynamically generate input fields.
+    eg:
+    {% page_placeholder "sidebar" page_lookup %}
+    {% page_placeholder "sidebar" page_lookup inherit %}
+    {% page_placeholder "sidebar" page_lookup as varname %}
+    {% page_placeholder "sidebar" page_lookup or %}
+        <div>No content</div>
+    {% endpage_placeholder %}
+    Keyword arguments:
+        name: the name of the placeholder
+        page_lookup: lookup argument for Page. See _get_page_by_untyped_arg() for detailed
+            information on the allowed types and their interpretation for the page_lookup argument.
+        inherit : optional argument which if given will result in inheriting
+            the content of the placeholder with the same name on parent pages
+        varname: context variable name. Output will be added to template context as this variable
+            instead of being returned.
+        or: optional argument which if given will make the template tag a block
+            tag whose content is shown if the placeholder is empty
+    """
+
+    name = "page_placeholder"
+    options = PlaceholderOptions(
+        Argument("name", resolve=False),
+        Argument("page_lookup"),
+        MultiValueArgument("extra_bits", required=False, resolve=False),
+        blocks=[("endpage_placeholder", "nodelist")],
+    )
+
+    # pylint: disable=arguments-differ,too-many-arguments
+    def render_tag(self, context, name, page_lookup, extra_bits, nodelist=None):
+        validate_placeholder_name(name)
+
+        request = context.get("request")
+
+        if request:
+            page = _get_page_by_untyped_arg(page_lookup, request, get_site_id(None))
+
+            toolbar = get_toolbar_from_request(request)
+            renderer = toolbar.get_content_renderer()
+
+            inherit = "inherit" in extra_bits
+
+            # A placeholder is only editable on its own page
+            editable = page == request.current_page
+
+            try:
+                content = renderer.render_page_placeholder(
+                    slot=name,
+                    context=context,
+                    inherit=inherit,
+                    page=page,
+                    nodelist=nodelist,
+                    editable=editable,
+                )
+            except PlaceholderNotFound:
+                content = ""
+        else:
+            content = ""
+
+        if not content and nodelist:
+            content = nodelist.render(context)
+
+        if "as" in extra_bits:
+            try:
+                varname = extra_bits[extra_bits.index("as") + 1]
+            except IndexError:
+                raise template.TemplateSyntaxError(
+                    'the "as" word should be followed by the variable name'
+                )
+            context[varname] = content
+            return ""
+
+        return content
+
+
+register.tag("page_placeholder", PagePlaceholder)
