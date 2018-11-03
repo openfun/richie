d56e188b8469acf848dcd8e742ac617a6272b582
Samuel Paccoud - FUN MOOC <samuel@sampaccoud.com>
2018-11-03 10:40:10 +0100

    👷(project) improve gitlint check for gitmojis
    In Issue #198, it was requested to directly download the list of
    gitmojis from the repository. The list of gitmojis has been updated
    and I need to use one of the new emojis so it was time to make
    this improvement.

--- gitlint/gitlint_emoji.py
+++ gitlint/gitlint_emoji.py
@@ -6,19 +6,9 @@ from __future__ import unicode_literals
 
 import re
 
-from gitlint.rules import CommitMessageTitle, LineRule, RuleViolation
+import requests
 
-regex = (
-    "^(\U0001f3a8|\u26a1\ufe0f|\U0001f525|\U0001f41b|\U0001f691|\u2728|\U0001f4dd|\U0001f680|"
-    "\U0001f484|\U0001f389|\u2705|\U0001f512|\U0001f34e|\U0001f427|\U0001f3c1|\U0001f916|"
-    "\U0001f34f|\U0001f516|\U0001f6a8|\U0001f6a7|\U0001f49a|\u2b07\ufe0f|\u2b06\ufe0f|\U0001f4cc|"
-    "\U0001f477|\U0001f4c8|\u267b\ufe0f|\u2796|\U0001f433|\u2795|\U0001f527|\U0001f310|\u270f"
-    "\ufe0f|\U0001f4a9|\u23ea|\U0001f500|\U0001f4e6|\U0001f47d|\U0001f69a|\U0001f4c4|\U0001f4a5|"
-    "\U0001f371|\U0001f44c|\u267f\ufe0f|\U0001f4a1|\U0001f37b|\U0001f4ac|\U0001f5c3|\U0001f50a|"
-    "\U0001f507|\U0001f465|\U0001f6b8|\U0001f3d7|\U0001f4f1|\U0001f921|\U0001f95a|\U0001f648|"
-    "\U0001f4f8)\(.*\)\s[a-z].*$"
-)
-pattern = re.compile(regex, re.UNICODE)
+from gitlint.rules import CommitMessageTitle, LineRule, RuleViolation
 
 
 class GitmojiTitle(LineRule):
@@ -33,6 +23,15 @@ class GitmojiTitle(LineRule):
     target = CommitMessageTitle
 
     def validate(self, title, _commit):
-        if not pattern.search(title):
-            violation_msg = "Title does not match regex \"<gitmoji>(<scope>) <subject>\""
+        """
+        Download the list possible gitmojis from the project's github repository and check that
+        title contains one of them.
+        """
+        gitmojis = requests.get(
+            "https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json"
+        ).json()["gitmojis"]
+        emojis = [item["emoji"] for item in gitmojis]
+        pattern = "^({:s})\(.*\)\s[a-z].*$".format("|".join(emojis))
+        if not re.search(pattern, title):
+            violation_msg = 'Title does not match regex "<gitmoji>(<scope>) <subject>"'
             return [RuleViolation(self.id, violation_msg, title)]
