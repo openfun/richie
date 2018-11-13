9b8520a18e06357124fc72134b128758399b2332
Samuel Paccoud - FUN MOOC <samuel@sampaccoud.com>
2018-11-13 14:28:55 +0100

    ✨(courses) add admin method to create course snapshots
    This method automatically makes a copy of the course with its extensions,
    and places it below the original course. It creates new title objects for
    each language and sets them to the current timestamp so that each snapshot
    has a unique name.
    
    It then moves all course runs of the course below its
    snapshot so we keep track of the version in which the course was when these
    course runs were created. Finally, it makes sure to respect the publication
    status of the course in each language on the snapshot.
    
    A snapshot can only be triggerd by a user with update and add access rights
    on the original course.

--- src/richie/apps/courses/admin.py
+++ src/richie/apps/courses/admin.py
@@ -1,16 +1,29 @@
 """
 Courses application admin
 """
+import time
+
+from django.conf.urls import url
 from django.contrib import admin
-from django.db import models
+from django.db import models, transaction
+from django.http import HttpResponseBadRequest, HttpResponseForbidden, JsonResponse
+from django.utils.decorators import method_decorator
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.http import require_POST
 
 from cms.admin.placeholderadmin import FrontendEditableAdminMixin
+from cms.api import Page, create_title
 from cms.extensions import PageExtensionAdmin
+from cms.utils import page_permissions
+from cms.utils.admin import jsonify_request
 
 from .fields import CourseRunSplitDateTimeField
 from .forms import LicenceFormAdmin
 from .models import Course, CourseRun, Licence, Organization
 from .widgets import CourseRunSplitDateTimeWidget
+
+REQUIRE_POST = method_decorator(require_POST)
 
 
 class CourseAdmin(PageExtensionAdmin):
@@ -24,6 +37,103 @@ class CourseAdmin(PageExtensionAdmin):
         Display the course title as a read-only field from the related page
         """
         return obj.extended_object.get_title()
+
+    def get_urls(self):
+        """
+        Add admin URL to trigger the snapshot of a course.
+        """
+        url_patterns = super().get_urls()
+        return [
+            url(
+                r"^(?P<course_id>[0-9]+)/snapshot/$",
+                self.admin_site.admin_view(self.snapshot),
+                name="cms_course_snapshot",
+            )
+        ] + url_patterns
+
+    @REQUIRE_POST
+    @transaction.atomic
+    def snapshot(self, request, course_id, *args, **kwargs):
+        """
+        Snapshotting a course is making a copy of the course page with all its permissions and
+        extensions, and placing the copy as the first child of the page being snapshotted, then
+        moving all the course run of the page being snapshotted as children of the new snapshot
+        so we keep record of the course as it was when these course runs were played.
+        """
+        try:
+            page = Page.objects.select_related("node__site").get(
+                publisher_is_draft=True, course__id=course_id
+            )
+        except Page.DoesNotExist:
+            return jsonify_request(
+                HttpResponseBadRequest(
+                    force_text(_("Error! Course could not be found."))
+                )
+            )
+
+        # If the page has a parent that is a course page, it is a snapshot and should therefore
+        # not be allowed to be itself snapshotted.
+        if page.parent_page:
+            try:
+                page.parent_page.course
+            except Course.DoesNotExist:
+                pass
+            else:
+                return jsonify_request(
+                    HttpResponseForbidden(
+                        force_text(_("Error! You can't snapshot a snapshot."))
+                    )
+                )
+
+        site = page.node.site
+
+        # User can only snapshot pages he can see
+        can_snapshot = page_permissions.user_can_change_page(request.user, page, site)
+
+        if can_snapshot:
+            # User can only snapshot a page if he has the permission to add a page under it.
+            can_snapshot = page_permissions.user_can_add_subpage(
+                request.user, page, site
+            )
+
+        if not can_snapshot:
+            return jsonify_request(
+                HttpResponseForbidden(
+                    force_text(
+                        _("Error! You don't have permissions to snapshot this page.")
+                    )
+                )
+            )
+
+        # Copy the page as its own child with its extension.
+        # Titles are set to a timestamp in each language of the original page
+        new_page = page.copy(
+            site, parent_node=page.node, translations=False, extensions=True
+        )
+
+        # The snapshot title and slug is set to a timestamp of the time of snapshot. It is
+        # published only in languages for which the original course page was published.
+        for language in page.get_languages():
+            base = page.get_path(language)
+            timestamp = str(int(time.time()))
+            snapshot_title = _("Snapshot of {:s}").format(page.get_title(language))
+            create_title(
+                language=language,
+                menu_title=timestamp,
+                title="{:s} - {:s}".format(timestamp, snapshot_title),
+                slug=timestamp,
+                path="{:s}/{:s}".format(base, timestamp) if base else timestamp,
+                page=new_page,
+            )
+            if page.is_published(language) is True:
+                new_page.publish(language)
+
+        # Move the existing course run subpages as children of the snapshot
+        # Their publication status will be respected
+        for subpage in page.get_child_pages().filter(courserun__isnull=False):
+            subpage.move_page(new_page.node, position="last-child")
+
+        return JsonResponse({"id": new_page.course.id})
 
 
 class CourseRunAdmin(FrontendEditableAdminMixin, PageExtensionAdmin):
