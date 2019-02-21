d0d2e91f739474fb0dfc04b9338e145fce3e2ddf
David THENON <sveetch@gmail.com>
2019-02-21 02:00:23 +0100

    ✨(plugins) add 'template'field to Section plugin
    Section plugin lacks of 'template' field to be able to have
    different layout from plugin instance to another.
    
    This commit add a new 'template' field to choose different template
    to render plugin, plugin template choices is defined from
    settings.

--- src/richie/plugins/section/cms_plugins.py
+++ src/richie/plugins/section/cms_plugins.py
@@ -7,6 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
+from .defaults import SECTION_TEMPLATES
 from .models import Section
 
 
@@ -16,15 +17,18 @@ class SectionPlugin(CMSPluginBase):
     CMSPlugin to add a content section with a distinct title from content.
     """
 
-    model = Section
-    name = _("Section")
-    render_template = "richie/section/section.html"
-    cache = True
     module = settings.FUN_PLUGINS_GROUP
+    name = _("Section")
+    model = Section
+    # Required from CMSPluginBase signature but not used since we override it
+    # from render()
+    render_template = SECTION_TEMPLATES[0][0]
+    cache = True
     allow_children = True
 
-    fieldsets = ((None, {"fields": ["title"]}),)
+    fieldsets = ((None, {"fields": ["title", "template"]}),)
 
     def render(self, context, instance, placeholder):
         context.update({"instance": instance, "placeholder": placeholder})
+        self.render_template = instance.template
         return context
---
+++ src/richie/plugins/section/defaults.py
@@ -0,0 +1,11 @@
+"""
+Section plugin default settings
+"""
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+SECTION_TEMPLATES = getattr(
+    settings,
+    "RICHIE_SECTION_TEMPLATES",
+    [("richie/section/section.html", _("Default"))],
+)
