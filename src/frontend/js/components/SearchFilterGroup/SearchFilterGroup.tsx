9be2531b613364ba117cd4c7d5efec09da9fa36d
Mehdi Benadda <me@mbenadda.com>
2019-02-28 15:28:57 +0100

    ✨(search) respect the order of filter values as returned by the server
    As we are about to add support for nested categories, it is impractical
    to reorder filter values on the frontend side based on their activity
    status: the underlying logic might be difficult to comprehend for users
    and the reordering itself would make the UI jump around too much.
    
    Instead, we can respect the order of the values in the API response. The
    server can more easily order them based on the available information,
    especially the published-defined order from the CMS itself.

--- src/frontend/js/components/SearchFilterGroup/SearchFilterGroup.tsx
+++ src/frontend/js/components/SearchFilterGroup/SearchFilterGroup.tsx
@@ -1,9 +1,7 @@
-import differenceBy from 'lodash-es/differenceBy';
 import React, { useContext } from 'react';
 
 import { CourseSearchParamsContext } from '../../data/useCourseSearchParams/useCourseSearchParams';
 import { FilterDefinitionWithValues } from '../../types/filters';
-import { Maybe } from '../../utils/types';
 import { SearchFilter } from '../SearchFilter/SearchFilter';
 
 export interface SearchFilterGroupProps {
@@ -14,13 +12,6 @@ export const SearchFilterGroup = ({ filter }: SearchFilterGroupProps) => {
   // We need the search params to determine which filter values are active
   const [courseSearchParams, dispatchCourseSearchParamsUpdate] = useContext(
     CourseSearchParamsContext,
-  );
-
-  // Make a list of FilterValues out of the currently active filter values
-  const currentValues =
-    (courseSearchParams[filter.name] as Maybe<string[]>) || [];
-  const activeFilterValues = filter.values.filter(({ key }) =>
-    currentValues.includes(key),
   );
 
   const updateFilter = (type: 'FILTER_ADD' | 'FILTER_REMOVE') => (
@@ -37,22 +28,13 @@ export const SearchFilterGroup = ({ filter }: SearchFilterGroupProps) => {
     <div className="search-filter-group">
       <h3 className="search-filter-group__title">{filter.human_name}</h3>
       <div className="search-filter-group__list">
-        {/* First we render the active filter values */}
-        {activeFilterValues.map(value => (
+        {filter.values.map(value => (
           <SearchFilter
             addFilter={updateFilter('FILTER_ADD')}
             filter={value}
-            isActive={true}
-            key={value.key}
-            removeFilter={updateFilter('FILTER_REMOVE')}
-          />
-        ))}
-        {/* Then we render the default list of facets, minus any active values that might have been in the facets */}
-        {differenceBy(filter.values, activeFilterValues, 'key').map(value => (
-          <SearchFilter
-            addFilter={updateFilter('FILTER_ADD')}
-            filter={value}
-            isActive={false}
+            isActive={(courseSearchParams[filter.name] || []).includes(
+              value.key,
+            )}
             key={value.key}
             removeFilter={updateFilter('FILTER_REMOVE')}
           />
