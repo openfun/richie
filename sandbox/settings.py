face48bd355078c067ac0334962b60ef5686963b
Julien Maupetit <julien@maupetit.net>
2019-03-15 15:54:33 +0100

    🐛(sandbox) remove STATICFILES_DIRS setting
    The STATICFILES_DIRS is mentionning a directory that no longer exists,
    leading to a collectstatic management command failure.

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -133,7 +133,6 @@ class Base(DRFMixin, ElasticSearchMixin, Configuration):
     MEDIA_URL = "/media/"
     MEDIA_ROOT = os.path.join(DATA_DIR, "media")
     STATIC_ROOT = os.path.join(DATA_DIR, "static")
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
     # Internationalization
     TIME_ZONE = "Europe/Paris"
