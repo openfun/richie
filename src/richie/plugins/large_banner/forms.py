86650d174f334266aaee99d5132cd02a6964083e
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2019-06-26 11:03:10 +0200

    🔧(courses) configure max length and ckeditor on specific placeholders
    Make use of the recently added capability to limit max length and
    configure CKEditor on specific placeholders for simple text and plain
    text plugins.

--- src/richie/plugins/large_banner/forms.py
+++ src/richie/plugins/large_banner/forms.py
@@ -1,11 +1,27 @@
 """
 Large banner plugin forms
 """
+import warnings
+
 from django import forms
+from django.conf import settings
 
 from djangocms_text_ckeditor.widgets import TextEditorWidget
 
 from .models import LargeBanner
+
+CKEDITOR_CONFIGURATION_NAME = "CKEDITOR_BASIC_CONFIGURATION"
+
+# Fallback to the old setting name for backward compatibility
+if getattr(settings, "CKEDITOR_BASIC_SETTINGS", None) and not getattr(
+    settings, "CKEDITOR_BASIC_CONFIGURATION", None
+):
+    warnings.warn(
+        '"CKEDITOR_BASIC_SETTINGS" is deprecated and renamed to "CKEDITOR_BASIC_CONFIGURATION"',
+        DeprecationWarning,
+        stacklevel=0,
+    )
+    CKEDITOR_CONFIGURATION_NAME = "CKEDITOR_BASIC_SETTINGS"
 
 
 class LargeBannerForm(forms.ModelForm):
@@ -19,7 +35,9 @@ class LargeBannerForm(forms.ModelForm):
         """
 
         model = LargeBanner
-        widgets = {"content": TextEditorWidget(configuration="CKEDITOR_BASIC_SETTINGS")}
+        widgets = {
+            "content": TextEditorWidget(configuration=CKEDITOR_CONFIGURATION_NAME)
+        }
         fields = [
             "title",
             "background_image",
