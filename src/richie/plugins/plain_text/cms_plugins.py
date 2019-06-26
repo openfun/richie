d4d466ce8213221107121f5138d7636a82138cd3
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2019-06-26 08:22:57 +0200

    ✨(plugins) extend plain text plugin to allow configuring max length
    We want to be able to limit the number of characters that a contributor
    can type for a given placeholder. We propose a pattern similar to
    what DjangoCMS does for placeholder configuration: a setting that
    matches a placeholder slot name with a max length.

--- src/richie/plugins/plain_text/cms_plugins.py
+++ src/richie/plugins/plain_text/cms_plugins.py
@@ -1,8 +1,11 @@
 """
 Plain text CMS plugin
 """
+from django.conf import settings
+from django.core.validators import MaxLengthValidator
 from django.utils.translation import ugettext_lazy as _
 
+from cms.models import Placeholder
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
@@ -25,6 +28,30 @@ class PlainTextPlugin(CMSPluginBase):
     module = PLUGINS_GROUP
     name = _("Plain text")
     render_template = "richie/plain_text/plain_text.html"
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        """
+        Add a max length validator based on what is configured in settings for the
+        current placeholder.
+        """
+        form = super().get_form(request, obj=obj, change=change, **kwargs)
+
+        placeholder_id = request.GET.get("placeholder_id")
+        if not placeholder_id and not obj:
+            return form
+
+        if placeholder_id:
+            placeholder = Placeholder.objects.only("slot").get(id=placeholder_id)
+        else:
+            placeholder = obj.placeholder
+
+        max_length = getattr(settings, "RICHIE_PLAINTEXT_MAXLENGTH", {}).get(
+            placeholder.slot
+        )
+        if max_length:
+            form.base_fields["body"].validators.append(MaxLengthValidator(max_length))
+
+        return form
 
     def render(self, context, instance, placeholder):
         """
---
+++ tests/plugins/plain_text/test_factories.py
@@ -0,0 +1,15 @@
+"""
+Factory tests
+"""
+from django.test import TestCase
+
+from richie.plugins.plain_text.factories import PlainTextFactory
+
+
+class PlainTextFactoriesTestCase(TestCase):
+    """Tests for the PlainText factory"""
+
+    def test_factories_plaintext_create_success(self):
+        """Factory creation success."""
+        plaintext = PlainTextFactory()
+        self.assertIsNotNone(plaintext.body)
