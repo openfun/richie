0c180a5d71052516360baa84944c9f1287a64602
Mehdi Benadda <me@mbenadda.com>
2019-07-11 16:25:41 +0200

    🏷️(resources) expect resource types to have a title
    Factorize the "title" property as a string on `Resource` instead of
    repeating it on each specific type of resource interface so we can
    more easily make use of it.

--- src/frontend/js/types/Organization.ts
+++ src/frontend/js/types/Organization.ts
@@ -2,7 +2,6 @@ import { Resource } from './Resource';
 
 export interface Organization extends Resource {
   logo: string | null;
-  title: string;
 }
 
 export type OrganizationForSuggestion = Pick<Organization, 'id' | 'title'>;
