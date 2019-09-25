d86968f7d8fb8b11b484735d3e638401c8b02ad8
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2019-09-25 22:46:56 +0200

    ♻️(category) factorize method to get related page extensions
    We were repeating exactly the same code in each method to get pages
    related to a given category via a plugin in their placeholder.
    We can share the code without changing the API.

--- src/richie/apps/courses/models/category.py
+++ src/richie/apps/courses/models/category.py
@@ -67,10 +67,11 @@ class Category(BasePageExtension):
             extended_object__node__parent__cms_pages__publisher_is_draft=self_is_draft,
         )
 
-    def get_courses(self, language=None):
+    def get_page_extensions(self, model_name, language=None):
         """
-        Return a query to get the courses related to this category ie for which a plugin for
-        this category is linked to the course page via any placeholder.
+        Return a query to get the page extentions of a given model type related to this category
+        ie for which a plugin for this category is linked to the organization page via any
+        placeholder.
         """
         is_draft = self.extended_object.publisher_is_draft
         category = self if is_draft else self.draft_extension
@@ -78,16 +79,17 @@ class Category(BasePageExtension):
 
         bfs = "extended_object__placeholders__cmsplugin__courses_categorypluginmodel__page"
         filter_dict = {
-            "extended_object__node__parent__cms_pages__course__isnull": True,
             "extended_object__publisher_is_draft": is_draft,
             "extended_object__placeholders__cmsplugin__language": language,
             bfs: category.extended_object,
         }
 
-        course_model = apps.get_model(app_label="courses", model_name="course")
+        page_extension_model = apps.get_model(
+            app_label="courses", model_name=model_name
+        )
         # pylint: disable=no-member
         return (
-            course_model.objects.filter(**filter_dict)
+            page_extension_model.objects.filter(**filter_dict)
             .select_related("extended_object")
             .prefetch_related(
                 Prefetch(
@@ -97,6 +99,15 @@ class Category(BasePageExtension):
                 )
             )
             .distinct()
+        )
+
+    def get_courses(self, language=None):
+        """
+        Return a query to get the courses related to this category ie for which a plugin for
+        this category is linked to the course page via any placeholder.
+        """
+        return self.get_page_extensions("course", language=language).filter(
+            extended_object__node__parent__cms_pages__course__isnull=True
         )
 
     def get_blogposts(self, language=None):
@@ -104,62 +115,15 @@ class Category(BasePageExtension):
         Return a query to get the blogposts related to this category ie for which a
         plugin for this category is linked to the blogpost page via any placeholder.
         """
-        is_draft = self.extended_object.publisher_is_draft
-        category = self if is_draft else self.draft_extension
-        language = language or translation.get_language()
+        return self.get_page_extensions("blogpost", language=language)
 
-        bfs = "extended_object__placeholders__cmsplugin__courses_categorypluginmodel__page"
-        filter_dict = {
-            "extended_object__publisher_is_draft": is_draft,
-            "extended_object__placeholders__cmsplugin__language": language,
-            bfs: category.extended_object,
-        }
-
-        blogpost_model = apps.get_model(app_label="courses", model_name="blogpost")
-        # pylint: disable=no-member
-        return (
-            blogpost_model.objects.filter(**filter_dict)
-            .select_related("extended_object")
-            .prefetch_related(
-                Prefetch(
-                    "extended_object__title_set",
-                    to_attr="prefetched_titles",
-                    queryset=Title.objects.filter(language=language),
-                )
-            )
-            .distinct()
-        )
 
     def get_persons(self, language=None):
         """
         Return a query to get the persons related to this category ie for which a plugin for
         this category is linked to the person page via any placeholder.
         """
-        is_draft = self.extended_object.publisher_is_draft
-        category = self if is_draft else self.draft_extension
-        language = language or translation.get_language()
-
-        bfs = "extended_object__placeholders__cmsplugin__courses_categorypluginmodel__page"
-        filter_dict = {
-            "extended_object__publisher_is_draft": is_draft,
-            "extended_object__placeholders__cmsplugin__language": language,
-            bfs: category.extended_object,
-        }
-
-        person_model = apps.get_model(app_label="courses", model_name="person")
-        # pylint: disable=no-member
-        return (
-            person_model.objects.filter(**filter_dict)
-            .select_related("extended_object")
-            .prefetch_related(
-                Prefetch(
-                    "extended_object__title_set",
-                    to_attr="prefetched_titles",
-                    queryset=Title.objects.filter(language=language),
-                )
-            )
-            .distinct()
-        )
+        return self.get_page_extensions("person", language=language)
 
     def get_children_categories(self):
         """
