d34e11769650478549d8bcc88c52d41a4b439846
Mehdi Benadda <me@mbenadda.com>
2019-10-23 16:38:34 +0200

    📝(versions) add versioning for the documentation
    Docusaurus supports versioning the documentation. We just created
    the versions page and enabled versining by cutting a `v1.12.0` of the
    documentation (matching the current release).

---
+++ website/pages/en/versions.js
@@ -0,0 +1,114 @@
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary');
+
+const Container = CompLibrary.Container;
+
+const CWD = process.cwd();
+
+const versions = require(`${CWD}/versions.json`);
+
+module.exports = function Versions(props) {
+  const { config: siteConfig } = props;
+  const latestVersion = versions[0];
+  const repoUrl = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}`;
+
+  return (
+    <div className="docMainWrapper wrapper">
+      <Container className="mainContainer versionsContainer">
+        <div className="post">
+          <header className="postHeader">
+            <h1>{siteConfig.title} versions and documentation</h1>
+          </header>
+          <p>
+            New versions of this project are shipped regularly. Every new
+            version includes its own version of the documentation.
+          </p>
+          <p>
+            Versions below <code>1.12.0</code> did not have a dedicated
+            documentation website.
+          </p>
+          <h3 id="latest">Current version (Stable)</h3>
+          <table className="versions">
+            <tbody>
+              <tr>
+                <th>{latestVersion}</th>
+                <td>
+                  <a
+                    href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
+                      props.language ? props.language + '/' : ''
+                    }quick-start`}
+                  >
+                    Documentation
+                  </a>
+                </td>
+                <td>
+                  <a href={`${repoUrl}/releases/tag/v${latestVersion}.0`}>
+                    Release Notes
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            This is the version that is configured automatically when you first
+            install this project.
+          </p>
+          <h3 id="rc">Pre-release versions</h3>
+          <table className="versions">
+            <tbody>
+              <tr>
+                <th>master</th>
+                <td>
+                  <a
+                    href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
+                      props.language ? props.language + '/' : ''
+                    }next/quick-start`}
+                  >
+                    Documentation
+                  </a>
+                </td>
+                <td>
+                  <a href={repoUrl}>Source Code</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <p>Other text describing this section.</p>
+          <h3 id="archive">Past Versions</h3>
+          <p>Here you can find previous versions of the documentation.</p>
+          <table className="versions">
+            <tbody>
+              {versions.map(
+                version =>
+                  version !== latestVersion && (
+                    <tr>
+                      <th>{version}</th>
+                      <td>
+                        <a
+                          href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
+                            props.language ? props.language + '/' : ''
+                          }${version}/quick-start`}
+                        >
+                          Documentation
+                        </a>
+                      </td>
+                      <td>
+                        <a href={`${repoUrl}/releases/tag/v${version}`}>
+                          Release Notes
+                        </a>
+                      </td>
+                    </tr>
+                  ),
+              )}
+            </tbody>
+          </table>
+          <p>
+            You can find past versions of this project on{' '}
+            <a href={repoUrl}>GitHub</a>.
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
+};
---
+++ website/versioned_docs/version-1.12/quick-start.md
@@ -0,0 +1,76 @@
+---
+id: version-1.12-quick-start
+title: Getting started with Richie
+sidebar_label: Quick start
+original_id: quick-start
+---
+
+If you're just looking for a quick preview of `Richie`, you can take a look and have a tour of `Richie` on our dedicated [demo site](https://demo.richie.education).
+
+Login/password are `admin`/`admin`. The database is regularly flushed.
+
+## Architecture
+
+`Richie` is a **container-native application** but can also be installed
+[on your machine](native-installation.md).
+
+For development, the project is defined using a [docker-compose file](../docker-compose.yml) and
+consists of 4 services:
+
+- **db**: the `Postgresql` database,
+- **elasticsearch**: the search engine,
+- **app**: the actual `DjangoCMS` project with all our application code,
+- **node**: used for front-end related tasks, _i.e._ transpiling `TypeScript` sources, bundling
+  them into a JS package, and building the CSS files from Sass sources.
+
+At "France Université Numérique", we deploy our applications on `OpenShift`/`Kubernetes` using
+[`Arnold`](https://github.com/openfun/arnold).
+
+## Getting started
+
+### Docker
+
+First, make sure you have a recent version of Docker and
+[Docker Compose](https://docs.docker.com/compose/install) installed on your laptop:
+
+```bash
+$ docker -v
+  Docker version 1.13.1, build 092cba3
+
+$ docker-compose --version
+  docker-compose version 1.17.1, build 6d101fb
+```
+
+⚠️ You may need to run the following commands with `sudo` but this can be avoided by assigning your
+user to the `docker` group.
+
+### Project bootstrap
+
+The easiest way to start working on the project is to use our `Makefile`:
+
+    $ make bootstrap
+
+This command builds the `app` container, installs front-end and back-end dependencies, builds the
+front-end application and styles, and performs database migrations. It's a good idea to use this
+command each time you are pulling code from the project repository to avoid dependency-related or
+migration-related issues.
+
+Now that your `Docker` services are ready to be used, start the full CMS by running:
+
+    $ make run
+
+### Adding content
+
+Once the CMS is up and running, you can create a superuser account:
+
+    $ make superuser
+
+You can create a basic demo site by running:
+
+    $ make demo-site
+
+Note that if you don't create the demo site and start from a blank CMS, you will get some errors
+requesting you to create some required root pages. So it is easier as a first approach to test the
+CMS with the demo site.
+
+You should be able to view the site at [localhost:8070](http://localhost:8070)
