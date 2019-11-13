73a4b46b190b9c6509399af8c8814db933f31103
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2019-11-13 08:36:47 +0100

    ✨(search) add toolbar menu item to bootstrap elasticsearch index
    There might be some cases in which a staff user needs to regenerate
    the search index from scratch. We propose to trigger it via an Ajax
    menu item in the toolbar. Only staff users who were granted the
    specific permission to manage the Elasticsearch index can do it.
    
    We do it synchronously for the moment because:
    - it should take less than 30 seconds,
    - only authorized users can do it and it's rare,
    - we want to avoid introducing a dependency to Celery or other
      asynchronous tasks handlers.

---
+++ tests/apps/search/test_cms_toolbars.py
@@ -0,0 +1,53 @@
+"""Test suite of the toolbar extension for search related it pages
+"""
+from django.contrib.auth.models import AnonymousUser, Permission
+
+from cms.api import create_page
+from cms.test_utils.testcases import CMSTestCase
+from cms.toolbar.items import AjaxItem
+
+from richie.apps.core.factories import UserFactory
+
+from ..core.utils import CheckToolbarMixin
+
+
+# pylint: disable=too-many-ancestors
+class SearchCMSToolbarTestCase(CheckToolbarMixin, CMSTestCase):
+    """Testing the integration of search related items in the toolbar."""
+
+    def test_cms_toolbars_search_bootstrap_elasticsearch(self):
+        """
+        Validate that the link to bootstrap elasticsearch is present on any page
+        but only for staff users with the required permission.
+        """
+        superuser = UserFactory(is_staff=True, is_superuser=True)
+        staff_with_permission = UserFactory(is_staff=True)
+        user_with_permission = UserFactory()
+        staff = UserFactory(is_staff=True)
+        user = UserFactory()
+        anonymous = AnonymousUser()
+
+        # Add permission to bootstrap ES for users concerned
+        can_manage = Permission.objects.get(codename="can_manage_elasticsearch")
+        staff_with_permission.user_permissions.add(can_manage)
+        user_with_permission.user_permissions.add(can_manage)
+
+        cases = [
+            (superuser, self.check_active),
+            (staff_with_permission, self.check_active),
+            (staff, self.check_missing),
+            (user_with_permission, self.check_absent),
+            (user, self.check_absent),
+            (anonymous, self.check_absent),
+        ]
+
+        # Create a page with nothing special
+        page = create_page(
+            "A page", template="richie/single_column.html", language="en"
+        )
+
+        for user, method in cases:
+            toolbar = self.get_toolbar_for_page(page, user)
+            item = method(toolbar, "Regenerate search index...", item_type=AjaxItem)
+            if item:
+                self.assertEqual(item.action, "/api/v1.0/bootstrap-elasticsearch")
---
+++ tests/apps/search/test_views_bootstrap_elasticsearch.py
@@ -0,0 +1,103 @@
+"""Test suite for the bootstrap_elasticsearch view of richie's search app."""
+import json
+from unittest import mock
+
+from django.contrib.messages import get_messages
+
+from cms.test_utils.testcases import CMSTestCase
+
+from richie.apps.core.factories import UserFactory
+
+
+class BootstrapElasticsearchViewTestCase(CMSTestCase):
+    """
+    Integration test suite to validate the behavior of the `bootstrap_elasticsearch` view.
+    """
+
+    @mock.patch("django.core.management.call_command")
+    def test_views_bootstrap_elasticsearch_with_permission(self, mock_command):
+        """Confirm triggering the search index bootstrapping works as expected."""
+        user = UserFactory(is_staff=True)
+        self.client.login(username=user.username, password="password")
+
+        # Add the necessary permission
+        self.add_permission(user, "can_manage_elasticsearch")
+
+        url = "/api/v1.0/bootstrap-elasticsearch"
+        response = self.client.post(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        self.assertEqual(content, {})
+
+        # Check the presence of a confirmation message
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(
+            str(messages[0]), "The search index was successfully bootstrapped"
+        )
+
+        mock_command.assert_called_once_with("bootstrap_elasticsearch")
+
+    @mock.patch("django.core.management.call_command")
+    def test_views_bootstrap_elasticsearch_no_permission(self, mock_command):
+        """Bootstrapping ES should be forbidden if the permission is not not granted."""
+        user = UserFactory(is_staff=True)
+        self.client.login(username=user.username, password="password")
+
+        url = "/api/v1.0/bootstrap-elasticsearch"
+        response = self.client.post(url, follow=True)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.content, b"You are not allowed to manage the search index."
+        )
+        self.assertFalse(mock_command.called)
+
+    @mock.patch("django.core.management.call_command")
+    def test_views_bootstrap_elasticsearch_post_required(self, mock_command):
+        """Bootstrapping ES can only be triggered with a POST method."""
+        user = UserFactory(is_staff=True)
+        self.client.login(username=user.username, password="password")
+
+        # Add the necessary permission
+        self.add_permission(user, "can_manage_elasticsearch")
+
+        url = "/api/v1.0/bootstrap-elasticsearch"
+
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.put(url, follow=True)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.delete(url, follow=True)
+        self.assertEqual(response.status_code, 405)
+
+        self.assertFalse(mock_command.called)
+
+    @mock.patch("django.core.management.call_command")
+    def test_views_bootstrap_elasticsearch_anonymous(self, mock_command):
+        """An anonymous user should not be allowed to bootstrap ES."""
+        url = "/api/v1.0/bootstrap-elasticsearch"
+        response = self.client.post(url, follow=True)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertFalse(mock_command.called)
+
+    @mock.patch("django.core.management.call_command")
+    def test_views_bootstrap_elasticsearch_not_staff_with_permission(
+        self, mock_command
+    ):
+        """A user with permissions that is not staff should not be allowed to bootstrap ES."""
+        user = UserFactory()
+        self.client.login(username=user.username, password="password")
+
+        # Add the necessary permission
+        self.add_permission(user, "can_manage_elasticsearch")
+
+        url = "/api/v1.0/bootstrap-elasticsearch"
+        response = self.client.post(url, follow=True)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.content, b"You are not allowed to manage the search index."
+        )
+        self.assertFalse(mock_command.called)
