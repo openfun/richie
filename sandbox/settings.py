b4ae4ae4d151abc696c2e8549e794cb78718a27c
Mehdi Benadda <me@mbenadda.com>
2019-12-24 15:28:54 +0100

    ✨(core) add a context processor for frontend context
    Frontend components throughout Richie have common expectations
    about the props they need to bootstrap. In addition to their own
    props, they all support and/or need additional context.
    
    As we're adding environment, release, and sentry_dsn to this shared
    context, we can't handcraft it in each django template that uses
    a frontend component anymore.
    
    This is why we're adding a context_processor to easily make all
    this data available to the frontend whenever it is used.

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -141,6 +141,7 @@ class Base(DRFMixin, RichieCoursesConfigurationMixin, Configuration):
                     "django.template.context_processors.static",
                     "cms.context_processors.cms_settings",
                     "richie.apps.core.context_processors.site_metas",
+                    "richie.apps.core.context_processors.frontend_context",
                 ],
                 "loaders": [
                     "django.template.loaders.filesystem.Loader",
