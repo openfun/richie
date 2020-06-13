e420692555a36027ec39006ca1d885338e77ac15
Mehdi Benadda <me@mbenadda.com>
2020-06-13 14:52:14 +0200

    ✨(api) open endpoints for enrollments
    As we manage enrollments through Richie, we will need to be able
    to create them as well as retrieve them from the frontend. To that
    end, we're adding create/retrieve/list endpoints for enrollments
    in our new enrollments app.
    
    We chose to use a new viewset, including for enrollment creation, to
    keep these features in the enrollments app and not create a dependency
    on the enrollments app from the courses app.
    
    As enrollments are private information belonging to one particular
    user, we restricted the queryset on these endpoints to enrollments
    owned by the current logged-in user.

--- src/richie/apps/courses/urls.py
+++ src/richie/apps/courses/urls.py
@@ -3,13 +3,14 @@ API routes exposed by our Courses app.
 """
 from rest_framework import routers
 
-from .api import CourseRunsViewSet
+from .api import CourseRunsViewSet, EnrollmentViewSet
 
 # Instantiate a router
 ROUTER = routers.SimpleRouter()
 
 # Define our app's routes with the router
 ROUTER.register(r"course-runs", CourseRunsViewSet, "course-runs")
+ROUTER.register(r"enrollments", EnrollmentViewSet, "enrollments")
 
 # Use the standard name for our urlpatterns so urls.py can import it effortlessly
 urlpatterns = ROUTER.urls
