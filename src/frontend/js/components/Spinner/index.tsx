38476022233c6758c547572583b7e0fd732fc96b
Mehdi Benadda <me@mbenadda.com>
2020-06-19 17:14:06 +0200

    ♿️(frontend) improve <Spinner /> labelling
    <Spinner /> uses children to have text contents so it can be
    accessible. This text content is, however, not associated with the
    role="status" element.
    
    Adding aria-labelledby passthrough makes it easy for the consuming
    component to associate the spinner with its text label if needed.

--- src/frontend/js/components/Spinner/index.tsx
+++ src/frontend/js/components/Spinner/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 interface SpinnerProps {
+  'aria-labelledby'?: string;
   size?: 'small' | 'large';
-  children: React.ReactNode;
 }
 
 /** Component. Displays a rotating CSS loader. */
-export const Spinner = ({ children, size }: SpinnerProps) => {
+export const Spinner: React.FC<SpinnerProps> = (props) => {
+  const { children, size } = props;
+  const spinnerProps = props['aria-labelledby']
+    ? { ['aria-labelledby']: props['aria-labelledby'] }
+    : {};
+
   return (
-    <div className="spinner-container" role="status">
+    <div className="spinner-container" role="status" {...spinnerProps}>
       <div className={`spinner spinner--${size || 'small'}`} />
       <div className="offscreen">{children}</div>
     </div>
