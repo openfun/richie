ab48ab3376126a2d41bbdce4acad39002f62d437
jbpenrath <jb.penrath@gmail.com>
2020-09-22 14:30:52 +0200

    🐛(frontend) handle error invalid configuration
    The path to get context from __richie_frontend_context__ was wrong so context
    variable was always undefined.

---
+++ src/frontend/js/utils/errors/handle.spec.ts
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+import * as Sentry from '@sentry/browser';
+
+jest.mock('@sentry/browser');
+
+describe('handle', () => {
+  (window as any).__richie_frontend_context__ = {
+    context: {
+      sentry_dsn: 'https://sentry.richie',
+      environment: 'test',
+    },
+  };
+
+  const { handle } = require('./handle');
+
+  it('should initialize sentry', () => {
+    expect(Sentry.init).toBeCalledTimes(1);
+    expect(Sentry.configureScope).toBeCalledTimes(1);
+  });
+
+  it('should report error to sentry', () => {
+    handle('An error for test');
+    expect(Sentry.captureException).toBeCalledTimes(1);
+  });
+});
