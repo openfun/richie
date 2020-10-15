e372bc15d106bdf6832c2b365af94529d882a062
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2020-10-15 13:58:45 +0200

    🚸(plugins) improve sanitizing of input text on CKEditor plugin
    CKEditor is submitting text with web entities (e.g. &eacute;) and
    potentially exotic characters. Our support team complained that the
    count of the max number of characters was wrong. This commit fixes
    it by normalizing and unescaping input strings. We do it for counting
    and also before saving it to database.

---
+++ tests/plugins/simple_text_ckeditor/test_forms.py
@@ -0,0 +1,26 @@
+"""
+Forms tests
+"""
+from django.test import TestCase
+
+from richie.plugins.simple_text_ckeditor.forms import CKEditorPluginForm
+
+
+class CKEditorPluginFormsTestCase(TestCase):
+    """Tests for the SimpleText forms"""
+
+    def test_forms_simpletext_clean_body(self):
+        """
+        Upon submission, the form should be cleaned:
+        - normalization,
+        - unescaping,
+        - strip useless spaces.
+        """
+        data = {
+            "body": "<div> <h1>株ＫＡ&nbsp;&ecirc;  </h1>   <b>&eacute;</b></div>",
+        }
+        form = CKEditorPluginForm(data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(
+            form.cleaned_data["body"], "<div><h1>株KA\xa0ê  </h1><b>é</b></div>"
+        )
