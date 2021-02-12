8121750987029eb94b2b7a6826cad1fc021b5074
jbpenrath <jb.penrath@gmail.com>
2021-02-12 19:32:08 +0100

    🐛(courses) show an error banner when template is misused
    Page based on [model]_detail template must have a model attached, if not,
    an 500 error occured. To prevent this error to appear, we manage the case where
    no model is attached to the page and display an error banner to alert the user
    that he/she is misusing the template.
    It concerns 4 models: category, course, organization and person.
    
    Issue:  #1280

--- tests/apps/courses/test_templates_category_detail.py
+++ tests/apps/courses/test_templates_category_detail.py
@@ -8,7 +8,7 @@ from django.test.utils import override_settings
 from cms.api import add_plugin
 from cms.test_utils.testcases import CMSTestCase
 
-from richie.apps.core.factories import UserFactory
+from richie.apps.core.factories import PageFactory, UserFactory
 from richie.apps.core.helpers import create_i18n_page
 from richie.apps.courses.cms_plugins import CategoryPlugin
 from richie.apps.courses.factories import (
@@ -320,4 +320,38 @@ class CategoryCMSTestCase(CMSTestCase):
         self._extension_cms_draft_content(
             PersonFactory,
             '<h2 class="person-glimpse__title">{:s}</h2>',
+        )
+
+    def test_template_category_detail_without_category(self):
+        """
+        A category template page without attached category should show an error banner
+        explaining to the user that he/she is misusing the template.
+        """
+        page = PageFactory(
+            template="courses/cms/category_detail.html",
+            title__language="en",
+            should_publish=True,
+        )
+
+        with self.assertTemplateUsed(
+            "courses/cms/fragment_error_detail_template_banner.html"
+        ):
+            response = self.client.get(page.get_absolute_url())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            (
+                '<div class="banner banner--error banner--rounded" role="alert">'
+                '<svg class="banner__icon"><use href="#icon-cross" /></svg>'
+                '<p class="banner__message">'
+                "A category object is missing on this category page. "
+                "Please select another page template."
+                "<br />"
+                "If what you need is a category page, you need to create it "
+                'via the wizard and choose "New category page".'
+                "</p>"
+                "</div>"
+            ),
+            html=True,
         )
