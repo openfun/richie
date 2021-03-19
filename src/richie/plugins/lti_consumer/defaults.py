c01b0c0e99dad01cc7751bbfffd19d9433759a22
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2021-03-19 08:21:34 +0100

    🚸(plugins) improve empty label in LTI provider select box
    We didn't want to add a specific choice in the "lti_provider_id" field
    for custom configuration. But it is still possible to customize the
    empty label to make it look like an option. In the database, we keep
    the benefit of having the None and not a fake provider id when none is
    selected...

--- src/richie/plugins/lti_consumer/defaults.py
+++ src/richie/plugins/lti_consumer/defaults.py
@@ -1,9 +1,10 @@
 """Default settings for Richie's LTI consumer plugin."""
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 STUDENT = "student"
 INSTRUCTOR = "instructor"
-PREDEFINED_LTI_PROVIDERS = [
+PREDEFINED_LTI_PROVIDERS = [(None, _("Custom provider configuration"))] + [
     (provider_key, provider.get("display_name", provider_key))
     for provider_key, provider in getattr(settings, "RICHIE_LTI_PROVIDERS", {}).items()
 ]
