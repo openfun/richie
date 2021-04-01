f903767d5e82c7fb2df4c05922f5e5488ef834f2
jbpenrath <jb.penrath@gmail.com>
2021-04-01 17:21:12 +0200

    ⚡️(lti_consumer) api endpoint response can be cached
    To optimize db accesses, api endpoint response can be cached into a mem cached
    named `memory_cache` in `CACHES` settings.

--- src/richie/plugins/lti_consumer/api.py
+++ src/richie/plugins/lti_consumer/api.py
@@ -1,4 +1,6 @@
 """Declare API endpoints for LTI Consumer Plugin"""
+from django.core.cache import caches
+from django.core.cache.backends.base import InvalidCacheBackendError
 from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets
@@ -26,14 +28,27 @@ class LTIConsumerViewsSet(viewsets.GenericViewSet):
             - automatic_resizing: boolean to control automatic resizing
 
         """
-        plugin = get_object_or_404(models.LTIConsumer, pk=pk)
-
         edit = request.toolbar and request.toolbar.edit_mode_active
+        cache_key = f"lti_consumer_plugin__pk_{pk}__edit_{edit}"
 
-        return Response(
-            {
-                "url": plugin.url,
-                "content_parameters": plugin.get_content_parameters(edit=edit),
-                "automatic_resizing": plugin.automatic_resizing,
-            }
-        )
+        try:
+            cache = caches["memory_cache"]
+            response = cache.get(cache_key)
+            if response is not None:
+                return Response(response)
+        except InvalidCacheBackendError:
+            cache = None
+
+        plugin = get_object_or_404(models.LTIConsumer, pk=pk)
+        response = {
+            "automatic_resizing": plugin.automatic_resizing,
+            "content_parameters": plugin.get_content_parameters(edit=edit),
+            "url": plugin.url,
+        }
+
+        if cache is not None:
+            # Cache the response for 9 minutes 30 seconds,
+            # lti oauth credentials are stale after 10 minutes
+            cache.set(cache_key, response, 9.5 * 60)
+
+        return Response(response)
--- tests/plugins/lti_consumer/test_api.py
+++ tests/plugins/lti_consumer/test_api.py
@@ -5,11 +5,15 @@ Tests for LTI Consumer plugin api
 import json
 from unittest import mock
 
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.test import RequestFactory, TestCase
+from django.test.utils import override_settings
 
 from cms.api import add_plugin
 from cms.models import Placeholder
+from cms.toolbar.toolbar import CMSToolbar
 
+from richie.plugins.lti_consumer.api import LTIConsumerViewsSet
 from richie.plugins.lti_consumer.cms_plugins import LTIConsumerPlugin
 from richie.plugins.lti_consumer.factories import LTIConsumerFactory
 from richie.plugins.lti_consumer.models import LTIConsumer
@@ -57,3 +61,52 @@ class LtiConsumerPluginApiTestCase(TestCase):
             "/api/v1.0/plugins/lti-consumer/15003/context/", follow=True
         )
         self.assertEqual(response.status_code, 404)
+
+    @override_settings(
+        CACHES={
+            "default": {
+                "BACKEND": "django_redis.cache.RedisCache",
+                "LOCATION": "mymaster/redis-sentinel:26379,redis-sentinel:26379/0",
+                "OPTIONS": {"CLIENT_CLASS": "richie.apps.core.cache.SentinelClient"},
+            },
+            "memory_cache": {
+                "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            },
+        }
+    )
+    @mock.patch.object(
+        LTIConsumer, "get_content_parameters", return_value="test_content"
+    )
+    def test_lti_consumer_view_set_get_context_method_is_cached(self, mock_params):
+        """
+        If "memory_cache" is defined, get_context method is cached
+        for 9 minutes and 30 seconds to optimize db accesses without return
+        stale oauth credentials.
+        """
+        placeholder = Placeholder.objects.create(slot="test")
+
+        lti_consumer = LTIConsumerFactory()
+        model_instance = add_plugin(
+            placeholder,
+            LTIConsumerPlugin,
+            "en",
+            url=lti_consumer.url,
+            lti_provider_id=lti_consumer.lti_provider_id,
+        )
+        request = RequestFactory().get("/")
+        request.user = AnonymousUser()
+        request.session = {}
+        request.toolbar = CMSToolbar(request)
+        view_set = LTIConsumerViewsSet()
+
+        with self.assertNumQueries(1):
+            view_set.get_context(request, "v1.0", model_instance.pk)
+
+        mock_params.assert_called_once()
+
+        mock_params.reset_mock()
+
+        with self.assertNumQueries(0):
+            view_set.get_context(request, "v1.0", model_instance.pk)
+
+        mock_params.assert_not_called()
