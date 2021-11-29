efafa0d13251d8443065b78c8dd7c08fa3d5c8c7
Mehdi Benadda <me@mbenadda.com>
2021-11-29 11:05:17 +0100

    ✨(frontend) add a hook to use IntersectionObserver
    As we implement an infinite scroll loading list for facet search
    results, we need to make use of IntersectionObserver to detect
    scrolling and automatically fetch the results.
    
    To make this easier, we're adding a hook that delegates to
    IntersectionObserver and respects React component lifecycle.

---
+++ src/frontend/js/utils/useIntersectionObserver.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { Nullable } from 'types/utils';
+
+interface UseIntersectionObserverProps {
+  root?: React.MutableRefObject<Nullable<Element>>;
+  target: React.MutableRefObject<Nullable<Element>>;
+  onIntersect: Function;
+  threshold?: number;
+  rootMargin?: string;
+  enabled?: boolean;
+}
+
+export const useIntersectionObserver = ({
+  root,
+  target,
+  onIntersect,
+  threshold = 0.99,
+  rootMargin = '0px',
+  enabled = true,
+}: UseIntersectionObserverProps) => {
+  useEffect(() => {
+    if (!enabled || !IntersectionObserver) {
+      return;
+    }
+
+    // eslint-disable-next-line compat/compat
+    const observer = new IntersectionObserver(
+      (entries) => entries.forEach((entry) => entry.isIntersecting && onIntersect()),
+      {
+        root: root?.current,
+        rootMargin,
+        threshold,
+      },
+    );
+
+    const el = target?.current;
+
+    if (!el) {
+      return;
+    }
+
+    observer.observe(el);
+
+    return () => {
+      observer.unobserve(el);
+    };
+  }, [target.current, enabled]);
+};
