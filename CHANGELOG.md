a19964c882ef56b869dbd115bb7580e82d3af323
jbpenrath <jb.penrath@gmail.com>
2021-11-29 17:37:01 +0100

    💄(frontend) add theme modifier to spinner
    Spinner component accepts now a theme property. Right now, this property can set
    to `light` otherwise it should be undefined.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ Versioning](https://semver.org/spec/v2.0.0.html).
 
 ### Added
 
+- Add theme property to `Spinner` component
 - Add setting to limit the number of archived course runs displayed by
   default on a course detail page.
 - Add timing function variables
--- src/frontend/js/components/Spinner/index.tsx
+++ src/frontend/js/components/Spinner/index.tsx
@@ -1,20 +1,30 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 
 interface SpinnerProps {
   'aria-labelledby'?: string;
   size?: 'small' | 'large';
+  theme?: 'light';
 }
 
 /** Component. Displays a rotating CSS loader. */
 export const Spinner: FC<SpinnerProps> = (props) => {
-  const { children, size } = props;
+  const { children, size = 'small', theme } = props;
   const spinnerProps = props['aria-labelledby']
     ? { 'aria-labelledby': props['aria-labelledby'] }
     : {};
 
+  const spinnerClassList = useMemo(() => {
+    const classList = ['spinner', `spinner--${size}`];
+    if (theme) {
+      classList.push(`spinner--${theme}`);
+    }
+
+    return classList.join(' ');
+  }, [size, theme]);
+
   return (
     <div className="spinner-container" role="status" {...spinnerProps}>
-      <div className={`spinner spinner--${size || 'small'}`} />
+      <div className={spinnerClassList} />
       <div className="offscreen">{children}</div>
     </div>
   );
