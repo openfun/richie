e94cae898080913c65214ce6db2c80a28910de2f
jbpenrath <jb.penrath@gmail.com>
2022-03-18 17:22:08 +0100

    ✨(frontend) add a PaymentInterface component
    PaymentInterface is in charge to lazy load the right payment component according
    to the provided payment provider. The first payment provider supported is
    Payplug.

---
+++ src/frontend/js/components/PaymentInterfaces/__mocks__/index.tsx
@@ -0,0 +1,25 @@
+import { PaymentInterfaceProps } from 'components/PaymentInterfaces/index';
+import { PaymentErrorMessageId } from 'components/PaymentButton';
+
+const PaymentInterface = ({ onError, onSuccess }: PaymentInterfaceProps) => (
+  <p>
+    Payment interface component
+    <button
+      data-testid="payment-failure"
+      onClick={() => onError(PaymentErrorMessageId.ERROR_DEFAULT)}
+    >
+      Simulate payment failure
+    </button>
+    <button
+      data-testid="payment-abort"
+      onClick={() => onError(PaymentErrorMessageId.ERROR_ABORTING)}
+    >
+      Simulate payment abort
+    </button>
+    <button data-testid="payment-success" onClick={onSuccess}>
+      Simulate payment success
+    </button>
+  </p>
+);
+
+export default PaymentInterface;
