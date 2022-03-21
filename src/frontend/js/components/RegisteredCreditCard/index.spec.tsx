11159a8bd8d049f51fc26c5b7ca72201c584300e
jbpenrath <jb.penrath@gmail.com>
2022-03-21 14:37:20 +0100

    ✨(frontend) add a RegisteredCreditCard component
    Create a component to display a credit cards owned by the user. It accepts a
    method triggered when an user clicks on a credit card to select it.

---
+++ src/frontend/js/components/RegisteredCreditCard/index.spec.tsx
@@ -0,0 +1,96 @@
+import { fireEvent, getByRole, getByText, render } from '@testing-library/react';
+import { PropsWithChildren } from 'react';
+import { IntlProvider } from 'react-intl';
+import { CreditCard } from 'types/Joanie';
+import { CreditCardFactory } from 'utils/test/factories';
+import { RegisteredCreditCard } from '.';
+
+describe('RegisteredCreditCard', () => {
+  const dateFormatter = Intl.DateTimeFormat('en', {
+    month: '2-digit',
+    year: 'numeric',
+  });
+
+  const Wrapper = ({ children }: PropsWithChildren<{}>) => (
+    <IntlProvider locale="en">{children}</IntlProvider>
+  );
+
+  afterEach(() => {
+    CreditCardFactory.afterGenerate((c: CreditCard) => c);
+  });
+
+  it('should render credit card information', () => {
+    const creditCard: CreditCard = CreditCardFactory.generate();
+
+    const { container } = render(
+      <Wrapper>
+        <RegisteredCreditCard {...creditCard} selected={false} handleSelect={() => undefined} />
+      </Wrapper>,
+    );
+
+    // - Its title
+    getByText(container, creditCard.title!, { exact: true });
+
+    // - Its last numbers
+    getByText(container, creditCard.last_numbers);
+
+    // - Then its expiration date
+    getByText(
+      container,
+      `Expiration date: ${dateFormatter.format(
+        new Date(creditCard.expiration_year, creditCard.expiration_month - 1, 1),
+      )}`,
+    );
+
+    // A checkbox input
+    getByRole(container, 'checkbox', {
+      name: `Select ${creditCard.title}'s card`,
+    });
+  });
+
+  it('should display the credit card brand if it has not title', () => {
+    const creditCard: CreditCard = CreditCardFactory.afterGenerate(
+      ({ title, ...rest }: CreditCard) => rest,
+    ).generate();
+
+    const { container } = render(
+      <Wrapper>
+        <RegisteredCreditCard {...creditCard} selected={false} handleSelect={() => undefined} />
+      </Wrapper>,
+    );
+
+    getByText(container, creditCard.brand);
+  });
+
+  it('should have a controlled checkbox to select/unselect the current credit card', () => {
+    const creditCard: CreditCard = CreditCardFactory.generate();
+    let selected = false;
+    const handleSelect = jest.fn();
+
+    const { container, rerender } = render(
+      <Wrapper>
+        <RegisteredCreditCard {...creditCard} selected={selected} handleSelect={handleSelect} />
+      </Wrapper>,
+    );
+
+    const $checkbox = getByRole(container, 'checkbox', {
+      name: `Select ${creditCard.title}'s card`,
+    });
+
+    fireEvent.click($checkbox);
+
+    expect(handleSelect).toHaveBeenNthCalledWith(1, expect.objectContaining({ target: $checkbox }));
+
+    // - If we update selected value, the input checkbox state should be updated accordingly
+    selected = true;
+    rerender(
+      <Wrapper>
+        <RegisteredCreditCard {...creditCard} selected={selected} handleSelect={handleSelect} />
+      </Wrapper>,
+    );
+
+    getByRole(container, 'checkbox', {
+      name: `Unselect ${creditCard.title}'s card`,
+    });
+  });
+});
---
+++ src/frontend/js/components/RegisteredCreditCard/index.tsx
@@ -0,0 +1,70 @@
+import { useMemo } from 'react';
+import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+import type * as Joanie from 'types/Joanie';
+
+const messages = defineMessages({
+  inputAriaLabel: {
+    defaultMessage: "{selected, select, true {Unselect} other {Select}} {title}'s card",
+    description: 'ARIA Label read by screen reader to inform which card is focused',
+    id: 'components.RegisteredCreditCard.inputAriaLabel',
+  },
+  expirationDate: {
+    defaultMessage: 'Expiration date: {expirationDate}',
+    description: 'Credit card expiration date label',
+    id: 'components.RegisteredCreditCard.expirationDate',
+  },
+});
+
+export interface Props extends Joanie.CreditCard {
+  selected: boolean;
+  handleSelect: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const RegisteredCreditCard = ({ selected, handleSelect, ...creditCard }: Props) => {
+  const intl = useIntl();
+
+  const expirationDate = useMemo(() => {
+    const { expiration_month: month, expiration_year: year } = creditCard;
+    return intl.formatDate(new Date(year, month - 1, 1), {
+      month: '2-digit',
+      year: 'numeric',
+    });
+  }, [creditCard.expiration_month, creditCard.expiration_year]);
+
+  const inputId = useMemo(() => `registered-credit-card-${creditCard.id}`, [creditCard.id]);
+
+  return (
+    <label className="registered-credit-card">
+      <p className="form-field">
+        <input
+          aria-label={intl.formatMessage(messages.inputAriaLabel, {
+            title: creditCard.title,
+            selected,
+          })}
+          aria-describedby={`credit-card-${creditCard.id}-infos`}
+          checked={selected}
+          className="form-field__checkbox-input"
+          onChange={handleSelect}
+          type="checkbox"
+          id={inputId}
+        />
+        <label htmlFor={inputId} className="form-field__label">
+          <span className="form-field__checkbox-control">
+            <svg role="img" aria-hidden="true">
+              <use href="#icon-check" />
+            </svg>
+          </span>
+        </label>
+      </p>
+      <div id={`credit-card-${creditCard.id}-infos`} className="registered-credit-card__infos">
+        <strong className="h6 registered-credit-card__name">
+          {creditCard.title || creditCard.brand}
+        </strong>
+        <p className="registered-credit-card__number">{creditCard.last_numbers}</p>
+        <p className="registered-credit-card__validity">
+          <FormattedMessage {...messages.expirationDate} values={{ expirationDate }} />
+        </p>
+      </div>
+    </label>
+  );
+};
