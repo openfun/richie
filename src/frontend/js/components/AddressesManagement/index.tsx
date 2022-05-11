2359238fcac1ebce8cead5cc781aa9c3e6d27320
Emmanuel Pelletier <manu@habite.la>
2022-05-11 18:48:49 +0200

    🎨(frontend) run prettier after changes that shifted tabs on a file
    This is related to the change in the last commit, but I didn't want to
    have the whole prettier-diff in it, it's just noise.

--- src/frontend/js/components/AddressesManagement/index.tsx
+++ src/frontend/js/components/AddressesManagement/index.tsx
@@ -132,177 +132,185 @@ interface AddressesManagementProps {
   selectAddress: (address: Joanie.Address) => void;
 }
 
-const AddressesManagement = forwardRef<HTMLDivElement, AddressesManagementProps>(({ handleClose, selectAddress }, ref) => {
-  const intl = useIntl();
-  const addresses = useAddresses();
-  const [editedAddress, setEditedAddress] = useState<Maybe<Joanie.Address>>();
-  const [error, setError] = useState<Maybe<string>>();
+const AddressesManagement = forwardRef<HTMLDivElement, AddressesManagementProps>(
+  ({ handleClose, selectAddress }, ref) => {
+    const intl = useIntl();
+    const addresses = useAddresses();
+    const [editedAddress, setEditedAddress] = useState<Maybe<Joanie.Address>>();
+    const [error, setError] = useState<Maybe<string>>();
 
-  /**
-   * Sort addresses ascending by title according to the locale
-   *
-   * @param {Joanie.Address} a
-   * @param {Joanie.Address} b
-   * @returns {Joanie.Address[]} Sorted addresses ascending by title
-   */
-  const sortAddressByTitleAsc = (a: Joanie.Address, b: Joanie.Address) => {
-    return a.title.localeCompare(b.title, [intl.locale, intl.defaultLocale]);
-  };
+    /**
+     * Sort addresses ascending by title according to the locale
+     *
+     * @param {Joanie.Address} a
+     * @param {Joanie.Address} b
+     * @returns {Joanie.Address[]} Sorted addresses ascending by title
+     */
+    const sortAddressByTitleAsc = (a: Joanie.Address, b: Joanie.Address) => {
+      return a.title.localeCompare(b.title, [intl.locale, intl.defaultLocale]);
+    };
 
-  /**
-   * update `selectedAddress` state with the address provided
-   * then close the address management form
-   *
-   * @param {Joanie.Address} address
-   */
-  const handleSelect = (address: Joanie.Address) => {
-    setError(undefined);
-    selectAddress(address);
-    handleClose();
-  };
+    /**
+     * update `selectedAddress` state with the address provided
+     * then close the address management form
+     *
+     * @param {Joanie.Address} address
+     */
+    const handleSelect = (address: Joanie.Address) => {
+      setError(undefined);
+      selectAddress(address);
+      handleClose();
+    };
 
-  /**
-   * Ask the user to confirm his intention
-   * then make the request to delete the provided address
-   *
-   * @param {Joanie.Address} address
-   */
-  const handleDelete = (address: Joanie.Address) => {
-    setError(undefined);
-    // eslint-disable-next-line no-alert, no-restricted-globals
-    const sure = confirm(
-      intl.formatMessage(messages.deletionConfirmation, { title: address.title }),
-    );
-    if (!address.is_main && sure) {
-      addresses.methods.delete(address.id, {
-        onError: () => setError(intl.formatMessage(messages.actionDeletion)),
-      });
-    }
-  };
+    /**
+     * Ask the user to confirm his intention
+     * then make the request to delete the provided address
+     *
+     * @param {Joanie.Address} address
+     */
+    const handleDelete = (address: Joanie.Address) => {
+      setError(undefined);
+      // eslint-disable-next-line no-alert, no-restricted-globals
+      const sure = confirm(
+        intl.formatMessage(messages.deletionConfirmation, { title: address.title }),
+      );
+      if (!address.is_main && sure) {
+        addresses.methods.delete(address.id, {
+          onError: () => setError(intl.formatMessage(messages.actionDeletion)),
+        });
+      }
+    };
 
-  /**
-   * Create a new address according to form values
-   * then update `selectedAddress` state with this new one.
-   * If `save` checkbox input is checked, the address is persisted
-   * otherwise it is only stored through the `selectedAddress` state.
-   *
-   * @param {AddressFormValues} formValues address fields to update
-   */
-  const handleCreate = async ({ save, ...address }: AddressFormValues) => {
-    setError(undefined);
-    if (save) {
-      await addresses.methods.create(address, {
-        onSuccess: handleSelect,
-        onError: () => setError(intl.formatMessage(messages.actionCreation)),
-      });
-    } else {
-      handleSelect({
-        id: LOCAL_BILLING_ADDRESS_ID,
-        is_main: false,
-        ...address,
-      });
-    }
-  };
+    /**
+     * Create a new address according to form values
+     * then update `selectedAddress` state with this new one.
+     * If `save` checkbox input is checked, the address is persisted
+     * otherwise it is only stored through the `selectedAddress` state.
+     *
+     * @param {AddressFormValues} formValues address fields to update
+     */
+    const handleCreate = async ({ save, ...address }: AddressFormValues) => {
+      setError(undefined);
+      if (save) {
+        await addresses.methods.create(address, {
+          onSuccess: handleSelect,
+          onError: () => setError(intl.formatMessage(messages.actionCreation)),
+        });
+      } else {
+        handleSelect({
+          id: LOCAL_BILLING_ADDRESS_ID,
+          is_main: false,
+          ...address,
+        });
+      }
+    };
 
-  /**
-   * Update the `editedAddress` with new values provided as argument
-   * then clear `editedAddress` state if request succeeded.
-   *
-   * @param {AddressFormValues} formValues address fields to update
-   */
-  const handleUpdate = async ({ save, ...newAddress }: AddressFormValues) => {
-    setError(undefined);
-    addresses.methods.update(
-      {
-        ...editedAddress!,
-        ...newAddress,
-      },
-      {
-        onSuccess: () => setEditedAddress(undefined),
-        onError: () => setError(intl.formatMessage(messages.actionUpdate)),
-      },
-    );
-  };
-
-  /**
-   * Update the provided address to promote it as main
-   *
-   * @param {Joanie.Address} address
-   */
-  const promoteAddress = (address: Joanie.Address) => {
-    if (!address.is_main) {
+    /**
+     * Update the `editedAddress` with new values provided as argument
+     * then clear `editedAddress` state if request succeeded.
+     *
+     * @param {AddressFormValues} formValues address fields to update
+     */
+    const handleUpdate = async ({ save, ...newAddress }: AddressFormValues) => {
       setError(undefined);
       addresses.methods.update(
         {
-          ...address,
-          is_main: true,
+          ...editedAddress!,
+          ...newAddress,
         },
         {
-          onError: () => setError(intl.formatMessage(messages.actionPromotion)),
+          onSuccess: () => setEditedAddress(undefined),
+          onError: () => setError(intl.formatMessage(messages.actionUpdate)),
         },
       );
-    }
-  };
+    };
 
-  useEffect(() => {
-    setError(undefined);
-  }, [editedAddress]);
+    /**
+     * Update the provided address to promote it as main
+     *
+     * @param {Joanie.Address} address
+     */
+    const promoteAddress = (address: Joanie.Address) => {
+      if (!address.is_main) {
+        setError(undefined);
+        addresses.methods.update(
+          {
+            ...address,
+            is_main: true,
+          },
+          {
+            onError: () => setError(intl.formatMessage(messages.actionPromotion)),
+          },
+        );
+      }
+    };
 
-  return (
-    <div className="AddressesManagement" ref={ref}>
-      <button className="AddressesManagement__closeButton button button-sale--tertiary" onClick={handleClose}>
-        <Icon name="icon-chevron-down" className="button__icon" />
-        <FormattedMessage {...messages.closeButton} />
-      </button>
-      {error && (
-        <Banner
-          message={intl.formatMessage(messages.error, { action: error })}
-          type={BannerType.ERROR}
-          rounded
-        />
-      )}
-      {addresses.items.length > 0 ? (
-        <section className="address-registered">
+    useEffect(() => {
+      setError(undefined);
+    }, [editedAddress]);
+
+    return (
+      <div className="AddressesManagement" ref={ref}>
+        <button
+          className="AddressesManagement__closeButton button button-sale--tertiary"
+          onClick={handleClose}
+        >
+          <Icon name="icon-chevron-down" className="button__icon" />
+          <FormattedMessage {...messages.closeButton} />
+        </button>
+        {error && (
+          <Banner
+            message={intl.formatMessage(messages.error, { action: error })}
+            type={BannerType.ERROR}
+            rounded
+          />
+        )}
+        {addresses.items.length > 0 ? (
+          <section className="address-registered">
+            <header>
+              <h5>
+                <FormattedMessage {...messages.registeredAddresses} />
+              </h5>
+            </header>
+            <ul className="registered-addresses-list">
+              {Children.toArray(
+                addresses.items
+                  .sort(sortAddressByTitleAsc)
+                  .map((address) => (
+                    <RegisteredAddress
+                      address={address}
+                      edit={setEditedAddress}
+                      promote={promoteAddress}
+                      remove={handleDelete}
+                      select={handleSelect}
+                    />
+                  )),
+              )}
+            </ul>
+          </section>
+        ) : null}
+        <section className={`address-form ${editedAddress ? 'address-form--highlighted' : ''}`}>
           <header>
             <h5>
-              <FormattedMessage {...messages.registeredAddresses} />
+              {editedAddress ? (
+                <FormattedMessage
+                  {...messages.editAddress}
+                  values={{ title: editedAddress.title }}
+                />
+              ) : (
+                <FormattedMessage {...messages.addAddress} />
+              )}
             </h5>
           </header>
-          <ul className="registered-addresses-list">
-            {Children.toArray(
-              addresses.items
-                .sort(sortAddressByTitleAsc)
-                .map((address) => (
-                  <RegisteredAddress
-                    address={address}
-                    edit={setEditedAddress}
-                    promote={promoteAddress}
-                    remove={handleDelete}
-                    select={handleSelect}
-                  />
-                )),
-            )}
-          </ul>
+          <AddressForm
+            address={editedAddress}
+            handleReset={() => setEditedAddress(undefined)}
+            onSubmit={editedAddress ? handleUpdate : handleCreate}
+          />
         </section>
-      ) : null}
-      <section className={`address-form ${editedAddress ? 'address-form--highlighted' : ''}`}>
-        <header>
-          <h5>
-            {editedAddress ? (
-              <FormattedMessage {...messages.editAddress} values={{ title: editedAddress.title }} />
-            ) : (
-              <FormattedMessage {...messages.addAddress} />
-            )}
-          </h5>
-        </header>
-        <AddressForm
-          address={editedAddress}
-          handleReset={() => setEditedAddress(undefined)}
-          onSubmit={editedAddress ? handleUpdate : handleCreate}
-        />
-      </section>
-    </div>
-  );
-});
+      </div>
+    );
+  },
+);
 
 export default AddressesManagement;
