9f34a99531f57a84ee4ba4f42ab69c05c685db41
jeremie <gegeturambar@gmail.com>
2022-06-08 17:52:34 +0200

    remove old files

--- src/frontend/js/components/DashBoard/Views/CertificateList.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CertificateList = () => {
-  return <div />;
-};
-
-export default CertificateList;
--- src/frontend/js/components/DashBoard/Views/CourseList.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CourseList = () => {
-  return <div>CourseList</div>;
-};
-
-export default CourseList;
