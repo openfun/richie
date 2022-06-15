6e70830f1817693198b782c1b79e12dd45284762
Mehdi Benadda <me@mbenadda.com>
2022-06-15 22:36:05 +0200

    ✨(api) add endpoint to search and autocomplete licences
    As we add licence-based filters to the course search engine, we
    need to create a licence index for internal use. As we do this,
    it is not very costly to add another feature by opening endpoints
    so the frontend can search and autocomplete licences.

---
+++ src/richie/apps/search/indexers/licences.py
@@ -0,0 +1,106 @@
+"""
+ElasticSearch licence document management utilities
+"""
+from django.conf import settings
+
+from ...courses.models import Licence
+from ..defaults import ES_INDICES_PREFIX
+from ..forms import LicenceSearchForm
+from ..text_indexing import MULTILINGUAL_TEXT
+from ..utils.i18n import get_best_field_language
+from ..utils.indexers import slice_string_for_completion
+
+
+class LicencesIndexer:
+    """
+    Makes available the parameters the indexer requires as well as a function to shape
+    objects into what we want to index in ElasticSearch
+    """
+
+    index_name = f"{ES_INDICES_PREFIX}_licences"
+    form = LicenceSearchForm
+    mapping = {
+        "dynamic_templates": MULTILINGUAL_TEXT,
+        "properties": {
+            # Searchable description & title are handled by `MULTILINGUAL_TEXT`
+            **{
+                f"complete.{lang:s}": {
+                    "type": "completion",
+                    "analyzer": "simple_diacritics_insensitive",
+                }
+                for lang, _ in settings.LANGUAGES
+            },
+            # Create a raw title field to enable alphabetical sorting
+            # We cannot use the default title field as the analysis prevents sorting on it
+            **{
+                f"title_raw.{lang}": {"type": "keyword"}
+                for lang, _ in settings.LANGUAGES
+            },
+        },
+    }
+    scripts = {}
+    display_fields = ["id", "title"]
+
+    @classmethod
+    def get_es_document_for_licence(cls, licence, index=None, action="index"):
+        """Build an Elasticsearch document from the licence instance."""
+        index = index or cls.index_name
+
+        translations = licence.translations.all()
+
+        titles = {
+            translation.language_code: translation.name for translation in translations
+        }
+
+        return {
+            "_id": licence.id,
+            "_index": index,
+            "_op_type": action,
+            "complete": {
+                language_code: slice_string_for_completion(name)
+                for language_code, name in titles.items()
+            },
+            "content": {
+                translation.language_code: translation.content
+                for translation in translations
+            },
+            "title": titles,
+            "title_raw": titles,
+        }
+
+    @classmethod
+    def get_es_documents(cls, index=None, action="index"):
+        """
+        Loop on all the liences in database and format them for the ElasticSearch index
+        """
+        index = index or cls.index_name
+
+        for licence in Licence.objects.prefetch_related("translations").iterator():
+            yield cls.get_es_document_for_licence(licence, index=index, action=action)
+
+    @staticmethod
+    def format_es_object_for_api(es_licence, best_language):
+        """
+        Format a licence stored in ES into a consistent and easy-to-consume record for
+        API consumers
+        """
+        return {
+            "id": es_licence["_id"],
+            "title": get_best_field_language(
+                es_licence["_source"]["title"], best_language
+            ),
+        }
+
+    @staticmethod
+    def format_es_document_for_autocomplete(es_document, language=None):
+        """
+        Format a document stored in ES into an easy-to-consume record for autocomplete consumers.
+        This method differs from the regular one as objects retrieved from query VS complete
+        queries can be formatted differently; and consumers of autocomplete do not need
+        full objects.
+        """
+        return {
+            "id": es_document["_id"],
+            "kind": "licences",
+            "title": get_best_field_language(es_document["_source"]["title"], language),
+        }
---
+++ tests/apps/search/test_autocomplete_licences.py
@@ -0,0 +1,124 @@
+"""
+Tests for the licences autocomplete endpoint.
+"""
+import json
+from unittest import mock
+
+from django.test import TestCase
+
+from richie.apps.search import ES_CLIENT, ES_INDICES_CLIENT
+from richie.apps.search.elasticsearch import bulk_compat
+from richie.apps.search.indexers.licences import LicencesIndexer
+from richie.apps.search.text_indexing import ANALYSIS_SETTINGS
+from richie.apps.search.utils.indexers import slice_string_for_completion
+
+LICENCES_INDEX = "test_licences"
+
+
+@mock.patch.object(  # Plug the test index we're filling on the indexer itself
+    LicencesIndexer,
+    "index_name",
+    new_callable=mock.PropertyMock,
+    return_value=LICENCES_INDEX,
+)
+class AutocompleteLicencesTestCase(TestCase):
+    """
+    Test licence autocomplete queries in a real-world situation with ElasticSearch.
+    """
+
+    def execute_query(self, querystring="", **extra):
+        """
+        Not a test.
+        Prepare the ElasticSearch index and execute the query in it.
+        """
+
+        licences = [
+            {"id": "1", "title": {"en": "CC-BY-SA"}},
+            {"id": "2", "title": {"en": "CC-BY-NC"}},
+            {"id": "3", "title": {"en": "All Rights Résërvés"}},
+        ]
+
+        # Delete any existing indices so we get a clean slate
+        ES_INDICES_CLIENT.delete(index="_all")
+        # Create an index we'll use to test the ES features
+        ES_INDICES_CLIENT.create(index=LICENCES_INDEX)
+
+        # The index needs to be closed before we set an analyzer
+        ES_INDICES_CLIENT.close(index=LICENCES_INDEX)
+        ES_INDICES_CLIENT.put_settings(body=ANALYSIS_SETTINGS, index=LICENCES_INDEX)
+        ES_INDICES_CLIENT.open(index=LICENCES_INDEX)
+
+        # Use the default licences mapping from the Indexer
+        ES_INDICES_CLIENT.put_mapping(
+            body=LicencesIndexer.mapping, index=LICENCES_INDEX
+        )
+
+        # Actually insert our licences in the index
+        actions = [
+            {
+                "_id": licence["id"],
+                "_index": LICENCES_INDEX,
+                "_op_type": "create",
+                "complete": {"en": slice_string_for_completion(licence["title"]["en"])},
+                **licence,
+            }
+            for licence in licences
+        ]
+        bulk_compat(actions=actions, chunk_size=500, client=ES_CLIENT)
+        ES_INDICES_CLIENT.refresh()
+
+        response = self.client.get(
+            f"/api/v1.0/licences/autocomplete/?{querystring:s}", **extra
+        )
+        self.assertEqual(response.status_code, 200)
+
+        return licences, json.loads(response.content)
+
+    def test_autocomplete_missing_query(self, *_):
+        """
+        When the query is missing, the API returns a 400 error with an appropriate error.
+        """
+        response = self.client.get("/api/v1.0/licences/autocomplete/?")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json(),
+            {"errors": ['Missing autocomplete "query" for request to test_licences.']},
+        )
+
+    def test_autocomplete_text(self, *_):
+        """
+        Make sure autocomplete is operational and returns the expected licences.
+        """
+        all_licences, response = self.execute_query(querystring="query=CC-BY-N")
+        self.assertEqual(
+            [all_licences[1]["id"]], [licence["id"] for licence in response]
+        )
+
+        all_licences, response = self.execute_query(querystring="query=CC-BY-S")
+        self.assertEqual(
+            [all_licences[0]["id"]], [licence["id"] for licence in response]
+        )
+
+    def test_autocomplete_diacritics_insensitive_query(self, *_):
+        """
+        Queries are diacritics insensitive.
+        """
+        all_licences, response = self.execute_query(querystring="query=rêser")
+        self.assertEqual(
+            [all_licences[2]["id"]], [licence["id"] for licence in response]
+        )
+
+    def test_autocomplete_diacritics_insensitive_index(self, *_):
+        """
+        Index is diacritics insensitive.
+        """
+        all_licences, response = self.execute_query(querystring="query=reser")
+        self.assertEqual(
+            [all_licences[2]["id"]], [licence["id"] for licence in response]
+        )
+
+        # Sanity check for original, accented version
+        all_licences, response = self.execute_query(querystring="query=Résërvés")
+        self.assertEqual(
+            [all_licences[2]["id"]], [licence["id"] for licence in response]
+        )
