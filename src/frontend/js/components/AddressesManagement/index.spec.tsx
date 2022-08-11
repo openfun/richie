9ddfe4132f49a7b4605431b5f278aad089d6d929
jbpenrath <jb.penrath@gmail.com>
2022-08-12 01:20:05 +0200

    ✅(frontend) update tests of AddressesManagement
    Now that we display error message in AddressForm when form values are invalid we
     have to test this new feature. In order to do that we need to mock the
     validationSchema that is not an easy task. To ease readability and thinking
     about this task, we moved validationSchema tests into a dedicated test module
     then we create an AddressForm test module to test features related to this
     component as error message display.

--- src/frontend/js/components/AddressesManagement/index.spec.tsx
+++ src/frontend/js/components/AddressesManagement/index.spec.tsx
@@ -1,20 +1,16 @@
 /**
  * Test suite for AddressesManagement component
  */
-import { yupResolver } from '@hookform/resolvers/yup';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { act, renderHook } from '@testing-library/react-hooks';
-import faker from 'faker';
+import { act } from '@testing-library/react-hooks';
 import fetchMock from 'fetch-mock';
 import * as mockFactories from 'utils/test/factories';
 import { IntlProvider } from 'react-intl';
 import { QueryClientProvider } from 'react-query';
-import { useForm } from 'react-hook-form';
 import { SessionProvider } from 'data/SessionProvider';
 import { REACT_QUERY_SETTINGS, RICHIE_USER_TOKEN } from 'settings';
 import type * as Joanie from 'types/Joanie';
 import createQueryClient from 'utils/react-query/createQueryClient';
-import validationSchema from './validationSchema';
 import AddressesManagement from '.';
 
 jest.mock('utils/context', () => ({
@@ -30,142 +26,6 @@ jest.mock('utils/context', () => ({
 jest.mock('utils/indirection/window', () => ({
   confirm: jest.fn(() => true),
 }));
-
-describe('validationSchema', () => {
-  // Creation and Update form validation relies on a schema resolves by Yup.
-  it('should not have error if values are valid', async () => {
-    const defaultValues = {
-      address: faker.address.streetAddress(),
-      city: faker.address.city(),
-      country: faker.address.countryCode(),
-      first_name: faker.name.firstName(),
-      last_name: faker.name.lastName(),
-      postcode: faker.address.zipCode(),
-      title: faker.random.word(),
-      save: false,
-    };
-
-    const { result } = renderHook(() =>
-      useForm({
-        defaultValues,
-        resolver: yupResolver(validationSchema),
-      }),
-    );
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    result.current.formState.errors;
-    result.current.register('address');
-    result.current.register('city');
-    result.current.register('country');
-    result.current.register('first_name');
-    result.current.register('last_name');
-    result.current.register('postcode');
-    result.current.register('title');
-    result.current.register('save');
-
-    await act(async () => {
-      result.current.trigger();
-    });
-
-    const { formState } = result.current;
-
-    expect(formState.errors.address).not.toBeDefined();
-    expect(formState.errors.city).not.toBeDefined();
-    expect(formState.errors.country).not.toBeDefined();
-    expect(formState.errors.first_name).not.toBeDefined();
-    expect(formState.errors.last_name).not.toBeDefined();
-    expect(formState.errors.postcode).not.toBeDefined();
-    expect(formState.errors.title).not.toBeDefined();
-    expect(formState.errors.save).not.toBeDefined();
-    expect(formState.isValid).toBe(true);
-  });
-
-  it('should have an error if values are invalid', async () => {
-    const { result } = renderHook(() =>
-      useForm({
-        resolver: yupResolver(validationSchema),
-      }),
-    );
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    result.current.formState.errors;
-    result.current.register('address');
-    result.current.register('city');
-    result.current.register('country');
-    result.current.register('first_name');
-    result.current.register('last_name');
-    result.current.register('postcode');
-    result.current.register('title');
-    result.current.register('save');
-
-    // - Trigger form validation with empty values
-    await act(async () => {
-      result.current.trigger();
-    });
-
-    let { formState } = result.current;
-    expect(formState.errors.address?.type).toEqual('required');
-    expect(formState.errors.address?.message).toEqual('address is a required field');
-    expect(formState.errors.city?.type).toEqual('required');
-    expect(formState.errors.city?.message).toEqual('city is a required field');
-    expect(formState.errors.country?.type).toEqual('required');
-    expect(formState.errors.country?.message).toEqual('country is a required field');
-    expect(formState.errors.first_name?.type).toEqual('required');
-    expect(formState.errors.first_name?.message).toEqual('first_name is a required field');
-    expect(formState.errors.last_name?.type).toEqual('required');
-    expect(formState.errors.last_name?.message).toEqual('last_name is a required field');
-    expect(formState.errors.postcode?.type).toEqual('required');
-    expect(formState.errors.postcode?.message).toEqual('postcode is a required field');
-    expect(formState.errors.title?.type).toEqual('required');
-    expect(formState.errors.title?.message).toEqual('title is a required field');
-    expect(formState.errors.save).not.toBeDefined();
-    expect(formState.isValid).toBe(false);
-
-    // - Set values for all field but with a wrong one for country field
-    await act(async () => {
-      result.current.setValue('address', faker.address.streetAddress());
-      result.current.setValue('city', faker.address.city());
-      // set country value with an invalid country code
-      result.current.setValue('country', 'AA');
-      result.current.setValue('first_name', faker.name.firstName());
-      result.current.setValue('last_name', faker.name.lastName());
-      result.current.setValue('postcode', faker.address.zipCode());
-      result.current.setValue('title', faker.random.word());
-      result.current.trigger();
-    });
-
-    formState = result.current.formState;
-    expect(formState.errors.address).not.toBeDefined();
-    expect(formState.errors.city).not.toBeDefined();
-    expect(formState.errors.country?.type).toEqual('oneOf');
-    expect(formState.errors.country?.message).toContain(
-      'country must be one of the following values:',
-    );
-    expect(formState.errors.first_name).not.toBeDefined();
-    expect(formState.errors.last_name).not.toBeDefined();
-    expect(formState.errors.postcode).not.toBeDefined();
-    expect(formState.errors.title).not.toBeDefined();
-    expect(formState.errors.save).not.toBeDefined();
-    expect(formState.isValid).toBe(false);
-
-    // - Set country value with a valid country code
-    await act(async () => {
-      result.current.setValue('country', 'FR');
-      result.current.trigger();
-    });
-
-    formState = result.current.formState;
-    expect(formState.errors.address).not.toBeDefined();
-    expect(formState.errors.city).not.toBeDefined();
-    expect(formState.errors.country).not.toBeDefined();
-    expect(formState.errors.first_name).not.toBeDefined();
-    expect(formState.errors.last_name).not.toBeDefined();
-    expect(formState.errors.postcode).not.toBeDefined();
-    expect(formState.errors.title).not.toBeDefined();
-    expect(formState.errors.save).not.toBeDefined();
-    expect(formState.isValid).toBe(true);
-  });
-});
 
 describe('AddressesManagement', () => {
   const initializeUser = () => {
@@ -219,6 +79,46 @@ describe('AddressesManagement', () => {
     expect(handleClose).toHaveBeenCalledTimes(0);
     fireEvent.click($closeButton);
     expect(handleClose).toHaveBeenCalledTimes(1);
+  });
+
+  it("renders the user's addresses", async () => {
+    initializeUser();
+    const addresses = mockFactories.AddressFactory.generate(Math.ceil(Math.random() * 5));
+    fetchMock.get('https://joanie.endpoint/api/addresses/', addresses);
+
+    let container: HTMLElement;
+
+    await act(async () => {
+      ({ container } = render(
+        <QueryClientProvider client={createQueryClient({ persistor: true })}>
+          <IntlProvider locale="en">
+            <SessionProvider>
+              <AddressesManagement handleClose={handleClose} selectAddress={selectAddress} />
+            </SessionProvider>
+          </IntlProvider>
+        </QueryClientProvider>,
+      ));
+    });
+
+    // All user's addresses should be displayed
+    const $addresses = container!.querySelectorAll('.registered-addresses-item');
+    expect($addresses).toHaveLength(addresses.length);
+
+    addresses.forEach((address: Joanie.Address) => {
+      const $address = screen.getByTestId(`address-${address.id}-title`);
+      expect($address.textContent).toEqual(address.title);
+    });
+
+    // - User selects one of its existing address
+    const address = addresses[0];
+    const $selectButton = screen.getByRole('button', {
+      name: `Select "${address.title}" address`,
+      exact: true,
+    });
+    await act(async () => {
+      fireEvent.click($selectButton);
+    });
+    expect(selectAddress).toHaveBeenNthCalledWith(1, address);
   });
 
   it('renders a form to create an address', async () => {
@@ -307,47 +207,7 @@ describe('AddressesManagement', () => {
     });
   });
 
-  it("renders the user's addresses", async () => {
-    initializeUser();
-    const addresses = mockFactories.AddressFactory.generate(Math.ceil(Math.random() * 5));
-    fetchMock.get('https://joanie.endpoint/api/addresses/', addresses);
-
-    let container: HTMLElement;
-
-    await act(async () => {
-      ({ container } = render(
-        <QueryClientProvider client={createQueryClient({ persistor: true })}>
-          <IntlProvider locale="en">
-            <SessionProvider>
-              <AddressesManagement handleClose={handleClose} selectAddress={selectAddress} />
-            </SessionProvider>
-          </IntlProvider>
-        </QueryClientProvider>,
-      ));
-    });
-
-    // All user's addresses should be displayed
-    const $addresses = container!.querySelectorAll('.registered-addresses-item');
-    expect($addresses).toHaveLength(addresses.length);
-
-    addresses.forEach((address: Joanie.Address) => {
-      const $address = screen.getByTestId(`address-${address.id}-title`);
-      expect($address.textContent).toEqual(address.title);
-    });
-
-    // - User selects one of its existing address
-    const address = addresses[0];
-    const $selectButton = screen.getByRole('button', {
-      name: `Select "${address.title}" address`,
-      exact: true,
-    });
-    await act(async () => {
-      fireEvent.click($selectButton);
-    });
-    expect(selectAddress).toHaveBeenNthCalledWith(1, address);
-  });
-
-  it('renders an updated form when user selects an address to edit', async () => {
+  it('renders a form to edit an address when user selects an address to edit', async () => {
     initializeUser();
     const address = mockFactories.AddressFactory.generate();
     fetchMock.get('https://joanie.endpoint/api/addresses/', [address]);
---
+++ src/frontend/js/components/AddressesManagement/validationSchema.spec.ts
@@ -0,0 +1,147 @@
+import faker from 'faker';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import validationSchema from './validationSchema';
+
+describe('validationSchema', () => {
+  // Creation and Update form validation relies on a schema resolves by Yup.
+  it('should not have error if values are valid', async () => {
+    const defaultValues = {
+      address: faker.address.streetAddress(),
+      city: faker.address.city(),
+      country: faker.address.countryCode(),
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      postcode: faker.address.zipCode(),
+      title: faker.random.word(),
+      save: false,
+    };
+
+    const { result } = renderHook(() =>
+      useForm({
+        defaultValues,
+        resolver: yupResolver(validationSchema),
+      }),
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    result.current.formState.errors;
+    result.current.register('address');
+    result.current.register('city');
+    result.current.register('country');
+    result.current.register('first_name');
+    result.current.register('last_name');
+    result.current.register('postcode');
+    result.current.register('title');
+    result.current.register('save');
+
+    await act(async () => {
+      result.current.trigger();
+    });
+
+    const { formState } = result.current;
+
+    expect(formState.errors.address).not.toBeDefined();
+    expect(formState.errors.city).not.toBeDefined();
+    expect(formState.errors.country).not.toBeDefined();
+    expect(formState.errors.first_name).not.toBeDefined();
+    expect(formState.errors.last_name).not.toBeDefined();
+    expect(formState.errors.postcode).not.toBeDefined();
+    expect(formState.errors.title).not.toBeDefined();
+    expect(formState.errors.save).not.toBeDefined();
+    expect(formState.isValid).toBe(true);
+  });
+
+  it('should have an error if values are invalid', async () => {
+    const { result } = renderHook(() =>
+      useForm({
+        resolver: yupResolver(validationSchema),
+      }),
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    result.current.formState.errors;
+    result.current.register('address');
+    result.current.register('city');
+    result.current.register('country');
+    result.current.register('first_name');
+    result.current.register('last_name');
+    result.current.register('postcode');
+    result.current.register('title');
+    result.current.register('save');
+
+    // - Trigger form validation with empty values
+    await act(async () => {
+      result.current.trigger();
+    });
+
+    let { formState } = result.current;
+    expect(formState.errors.address?.type).toEqual('required');
+    expect(formState.errors.address?.message?.key).toEqual('mixedRequired');
+    expect(formState.errors.address?.message?.values?.path).toEqual('address');
+    expect(formState.errors.city?.type).toEqual('required');
+    expect(formState.errors.city?.message?.key).toEqual('mixedRequired');
+    expect(formState.errors.city?.message?.values?.path).toEqual('city');
+    expect(formState.errors.country?.type).toEqual('required');
+    expect(formState.errors.country?.message?.key).toEqual('mixedRequired');
+    expect(formState.errors.country?.message?.values?.path).toEqual('country');
+    expect(formState.errors.first_name?.type).toEqual('required');
+    expect(formState.errors.first_name?.message?.key).toEqual('mixedRequired');
+    expect(formState.errors.first_name?.message?.values?.path).toEqual('first_name');
+    expect(formState.errors.last_name?.type).toEqual('required');
+    expect(formState.errors.last_name?.message?.key).toEqual('mixedRequired');
+    expect(formState.errors.last_name?.message?.values?.path).toEqual('last_name');
+    expect(formState.errors.postcode?.type).toEqual('required');
+    expect(formState.errors.postcode?.message?.key).toEqual('mixedRequired');
+    expect(formState.errors.postcode?.message?.values?.path).toEqual('postcode');
+    expect(formState.errors.title?.type).toEqual('required');
+    expect(formState.errors.title?.message?.key).toEqual('mixedRequired');
+    expect(formState.errors.title?.message?.values?.path).toEqual('title');
+    expect(formState.errors.save).not.toBeDefined();
+    expect(formState.isValid).toBe(false);
+
+    // - Set values for all field but with a wrong one for country field
+    await act(async () => {
+      result.current.setValue('address', faker.address.streetAddress());
+      result.current.setValue('city', faker.address.city());
+      // set country value with an invalid country code
+      result.current.setValue('country', 'AA');
+      result.current.setValue('first_name', faker.name.firstName());
+      result.current.setValue('last_name', faker.name.lastName());
+      result.current.setValue('postcode', faker.address.zipCode());
+      result.current.setValue('title', faker.random.word());
+      result.current.trigger();
+    });
+
+    formState = result.current.formState;
+    expect(formState.errors.address).not.toBeDefined();
+    expect(formState.errors.city).not.toBeDefined();
+    expect(formState.errors.country?.type).toEqual('oneOf');
+    expect(formState.errors.country?.message?.key).toEqual('mixedOneOf');
+    expect(formState.errors.country?.message?.values?.path).toEqual('country');
+    expect(formState.errors.first_name).not.toBeDefined();
+    expect(formState.errors.last_name).not.toBeDefined();
+    expect(formState.errors.postcode).not.toBeDefined();
+    expect(formState.errors.title).not.toBeDefined();
+    expect(formState.errors.save).not.toBeDefined();
+    expect(formState.isValid).toBe(false);
+
+    // - Set country value with a valid country code
+    await act(async () => {
+      result.current.setValue('country', 'FR');
+      result.current.trigger();
+    });
+
+    formState = result.current.formState;
+    expect(formState.errors.address).not.toBeDefined();
+    expect(formState.errors.city).not.toBeDefined();
+    expect(formState.errors.country).not.toBeDefined();
+    expect(formState.errors.first_name).not.toBeDefined();
+    expect(formState.errors.last_name).not.toBeDefined();
+    expect(formState.errors.postcode).not.toBeDefined();
+    expect(formState.errors.title).not.toBeDefined();
+    expect(formState.errors.save).not.toBeDefined();
+    expect(formState.isValid).toBe(true);
+  });
+});
