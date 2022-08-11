30460d375b14ce56c193c4c9d036389925bcca6c
jbpenrath <jb.penrath@gmail.com>
2022-08-12 01:14:20 +0200

    🔧(jest) protect some modules to the effect of jest.resetModules
    When we used `jest.resetModules` the module registry is reset, this is useful to
     isolate module in individual test but, it also causes some error. To prevent
     those error, a workaround is to exclude problematic modules to the scope of
     resetModules. We manage it through a little tweak in the jest.setup.ts.

--- src/frontend/jest/setup.ts
+++ src/frontend/jest/setup.ts
@@ -4,6 +4,23 @@ import '@testing-library/jest-dom/extend-expect';
 import { setLogger } from 'react-query';
 import { noop } from 'utils';
 
+/*
+ * A little trick to prevent so package to be reset when using `jest.resetModules()`.
+ * https://github.com/facebook/jest/issues/8987#issuecomment-584898030
+ */
+const RESET_MODULE_EXCEPTIONS = ['react', 'react-intl'];
+
+const mockActualRegistry: Record<PropertyKey, any> = {};
+
+RESET_MODULE_EXCEPTIONS.forEach((moduleName) => {
+  jest.doMock(moduleName, () => {
+    if (!mockActualRegistry[moduleName]) {
+      mockActualRegistry[moduleName] = jest.requireActual(moduleName);
+    }
+    return mockActualRegistry[moduleName];
+  });
+});
+
 /* Prevent log error during tests */
 setLogger({
   // eslint-disable-next-line no-console
