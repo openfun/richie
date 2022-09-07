02ee28da93ec6ef224c36e6a9fd0646ed303d04c
Nathan Vasse <nathan.vasse@gmail.com>
2022-09-07 10:13:31 +0200

    🔨(docker) rename node to frontend
    we decided that changing the name of the node service to frontend
    was more self explanatory about its real use

--- src/frontend/.storybook/main.js
+++ src/frontend/.storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../js/**/stories.@(js|jsx|ts|tsx)",
+    "../js/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
