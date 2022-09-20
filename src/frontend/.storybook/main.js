7e6e4b48741ccc01607b2e96d4d762f2657dc73d
Nathan Vasse <nathan.vasse@gmail.com>
2022-09-20 11:06:14 +0200

    🔧(frontend) allow storybook to use absolute paths
    it was causing crashes for component using absolute paths

--- src/frontend/.storybook/main.js
+++ src/frontend/.storybook/main.js
@@ -1,17 +1,22 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../js/**/*.stories.@(js|jsx|ts|tsx)",
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../js/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
   staticDirs: ['../../richie/static', '../../richie/apps/core/templates/richie'],
-}
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
+};
