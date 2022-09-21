ce739194afaaa93e2e71bbc85c071ad0d69d2dff
Nathan Vasse <nathan.vasse@gmail.com>
2022-09-21 15:09:54 +0200

    ✨(frontend) add capitalizeFirst method
    To prevent copy pasting the same code to capitalize first letters

--- src/frontend/js/utils/StringHelper/index.spec.tsx
+++ src/frontend/js/utils/StringHelper/index.spec.tsx
@@ -16,4 +16,14 @@ describe('StringHelper', () => {
   it('is zero', () => {
     expect(StringHelper.isString(0)).toBe(false);
   });
+  it('capitalizes first letter of a string', () => {
+    expect(StringHelper.capitalizeFirst('a')).toBe('A');
+    expect(StringHelper.capitalizeFirst('A')).toBe('A');
+    expect(StringHelper.capitalizeFirst('hi there')).toBe('Hi there');
+    expect(StringHelper.capitalizeFirst('1hi there')).toBe('1hi there');
+    expect(StringHelper.capitalizeFirst('_hi there')).toBe('_hi there');
+    expect(StringHelper.capitalizeFirst('')).toBe('');
+    expect(StringHelper.capitalizeFirst(null)).toBe(null);
+    expect(StringHelper.capitalizeFirst(undefined)).toBe(undefined);
+  });
 });
--- src/frontend/js/utils/StringHelper/index.ts
+++ src/frontend/js/utils/StringHelper/index.ts
@@ -1,5 +1,14 @@
+import { Maybe, Nullable } from 'types/utils';
+
 export class StringHelper {
   static isString(input: any): boolean {
     return typeof input === 'string' || input instanceof String;
+  }
+
+  static capitalizeFirst(str?: Nullable<string>): Maybe<Nullable<string>> {
+    if (!str) {
+      return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
 }
