6f6b0c6a12bd8b5611f580233d7f9eb0515f9276
Nathan Vasse <nathan.vasse@gmail.com>
2022-10-12 16:48:03 +0200

    ✨(frontend) add CreditCardHelper
    Add various helpers related to credit card management.

---
+++ src/frontend/js/utils/CreditCardHelper/index.spec.tsx
@@ -0,0 +1,47 @@
+import * as faker from 'faker';
+import { CreditCardExpirationStatus, CreditCardHelper } from 'utils/CreditCardHelper/index';
+import { CreditCardFactory } from 'utils/test/factories';
+
+describe('CreditCardHelper', () => {
+  it('handles falsy values', () => {
+    expect(
+      CreditCardHelper.isExpiredSoon({
+        ...CreditCardFactory.generate(),
+        expiration_month: null,
+        expiration_year: undefined,
+      }),
+    ).toBe(false);
+  });
+  it('is not soon expired', () => {
+    const limit = new Date();
+    limit.setMonth(limit.getMonth() + 4);
+    const date = faker.date.future(0.2, limit);
+    expect(
+      CreditCardHelper.getExpirationState({
+        ...CreditCardFactory.generate(),
+        expiration_month: date.getMonth() + 1,
+        expiration_year: date.getFullYear(),
+      }),
+    ).toBe(CreditCardExpirationStatus.FINE);
+  });
+  it('is soon expired', () => {
+    const date = faker.date.future(0.2);
+    expect(
+      CreditCardHelper.getExpirationState({
+        ...CreditCardFactory.generate(),
+        expiration_month: date.getMonth() + 1,
+        expiration_year: date.getFullYear(),
+      }),
+    ).toBe(CreditCardExpirationStatus.SOON);
+  });
+  it('is expired', () => {
+    const date = faker.date.past(0.2);
+    expect(
+      CreditCardHelper.getExpirationState({
+        ...CreditCardFactory.generate(),
+        expiration_month: date.getMonth() + 1,
+        expiration_year: date.getFullYear(),
+      }),
+    ).toBe(CreditCardExpirationStatus.EXPIRED);
+  });
+});
