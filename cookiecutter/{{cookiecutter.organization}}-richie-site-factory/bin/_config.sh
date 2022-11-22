d846c6072b169610102981712170d2d7e383514c
Samuel Paccoud - France Université Numérique <samuel.paccoud@fun-mooc.fr>
2022-11-22 21:03:06 +0100

    🔨(bin) stop using env variable to activate sites
    The DX was not good as we had to run bin/activate and then export
    a variable. Many users forgot to export the variable expecting the
    activate script to do what it claims to do...
    
    We now use a `.site` file to record the active site as it is done
    in terraform. This allows a much better DX as running `bin/activate`
    now effectively activates the site.

--- cookiecutter/{{cookiecutter.organization}}-richie-site-factory/bin/_config.sh
+++ cookiecutter/{{cookiecutter.organization}}-richie-site-factory/bin/_config.sh
@@ -3,9 +3,6 @@
 PROJECT_DIRECTORY=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/..
 SITES_DIRECTORY="sites"
 
-# Set default active site
-RICHIE_SITE="${RICHIE_SITE:-funmooc}"
-
 # console colors
 declare -r COLOR_INFO="\033[0;36m"
 declare -r COLOR_RESET="\033[0m"
@@ -14,4 +11,3 @@ export COLOR_INFO
 export COLOR_RESET
 export PROJECT_DIRECTORY
 export SITES_DIRECTORY
-export RICHIE_SITE
