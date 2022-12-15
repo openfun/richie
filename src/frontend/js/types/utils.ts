2e42114c09fd690ed09e0d75cabd34f7bdf3bdfc
Nathan Vasse <nathan.vasse@gmail.com>
2022-12-15 17:43:30 +0100

    🏷️(frontend) add AddParameters type
    This type is needed in order to provide a way to add a parameters
    to an existing ad-hoc function type.

--- src/frontend/js/types/utils.ts
+++ src/frontend/js/types/utils.ts
@@ -3,3 +3,8 @@ export type Maybe<T> = T | undefined;
 export type Nullable<T> = T | null;
 
 export type PropsWithTestId<Props> = Props & { 'data-testid'?: string };
+
+export type AddParameters<
+  TFunction extends (...args: readonly unknown[]) => unknown,
+  TParameters extends [...args: readonly unknown[]],
+> = (...args: [...Parameters<TFunction>, ...TParameters]) => ReturnType<TFunction>;
