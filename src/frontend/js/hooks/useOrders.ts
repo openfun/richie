03d9d7b75e35b23531b15e0be9e92c5daff66d2e
jbpenrath <jb.penrath@gmail.com>
2022-12-23 17:53:16 +0100

    🏷️(frontend) update types of useOrders hook
    We should be able to filter orders by product id and course code. The current
    type does not support that, so we have to replace the default ResourceQuery type
     by a custom OrderResourceQuery.

--- src/frontend/js/hooks/useOrders.ts
+++ src/frontend/js/hooks/useOrders.ts
@@ -1,6 +1,6 @@
+import { API, Order, Product, Course } from 'types/Joanie';
 import { useJoanieApi } from 'data/JoanieApiProvider';
 import { useSessionMutation } from 'utils/react-query/useSessionMutation';
-import { API, Order } from '../types/Joanie';
 import {
   QueryOptions,
   ResourcesQuery,
@@ -8,13 +8,19 @@ import {
   UseResourcesProps,
 } from './useResources';
 
-const props: UseResourcesProps<Order, ResourcesQuery, API['user']['orders']> = {
+type OrderResourcesQuery = ResourcesQuery & {
+  course?: Course['code'];
+  product?: Product['id'];
+};
+
+const props: UseResourcesProps<Order, OrderResourcesQuery, API['user']['orders']> = {
   queryKey: ['orders'],
   apiInterface: () => useJoanieApi().user.orders,
   omniscient: true,
   session: true,
 };
-export const useOrders = (filters?: ResourcesQuery, queryOptions?: QueryOptions<Order>) => {
+
+export const useOrders = (filters?: OrderResourcesQuery, queryOptions?: QueryOptions<Order>) => {
   const custom = useResourcesCustom({ ...props, filters, queryOptions });
   const abortHandler = useSessionMutation(useJoanieApi().user.orders.abort);
   return {
