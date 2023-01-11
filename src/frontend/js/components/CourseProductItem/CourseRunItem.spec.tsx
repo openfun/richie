740a3d8c9ade062a714f1927383a8a74b1c97b59
jbpenrath <jb.penrath@gmail.com>
2023-01-11 22:33:33 +0100

    🐛(CourseRunItem) prevent enrollment with invalid order
    About gradeo, the user should be able to enroll to product course runs
    only if it owns a valid order. Previously, once the user owns an order no
    matter its state, it was able to enroll.

---
+++ src/frontend/js/components/CourseProductItem/CourseRunItem.spec.tsx
@@ -0,0 +1,36 @@
+import faker from 'faker';
+import { waitFor, screen, getByText, render } from '@testing-library/react';
+import { OrderFactory } from 'utils/test/factories';
+import type { CourseRun, Order } from 'types/Joanie';
+import { OrderState } from 'types/Joanie';
+import CourseRunItem from './CourseRunItem';
+
+jest.mock('components/CourseProductCourseRuns', () => ({
+  CourseRunList: ({ courseRuns }: { courseRuns: CourseRun[] }) => (
+    <div data-testid={`CourseRunList-${courseRuns.map(({ id }) => id).join('-')}`} />
+  ),
+}));
+
+describe('CourseRunItem', () => {
+  it('does not allow user which purchase the product to enroll to course if order state is not validated', async () => {
+    const order: Order = OrderFactory.afterGenerate((o: Order) => ({
+      ...o,
+      state: faker.helpers.randomize([OrderState.CANCELED, OrderState.PENDING]),
+    })).generate();
+    const targetCourse = order.target_courses[0];
+
+    render(<CourseRunItem targetCourse={targetCourse} order={order} />);
+
+    // - It should render CourseRunList component
+    const $item = screen.getByTestId(`course-item-${targetCourse.code}`);
+    // the course title shouldn't be a heading to prevent misdirection for screen reader users,
+    // but we want to it to visually look like a h5
+    const $courseTitle = getByText($item, targetCourse.title);
+    expect($courseTitle.tagName).toBe('STRONG');
+    expect($courseTitle.classList.contains('h5')).toBe(true);
+    screen.getByTestId(`CourseRunList-${targetCourse.course_runs.map(({ id }) => id).join('-')}`);
+
+    // - Does not Render <SaleTunnel />
+    expect(screen.queryByTestId('SaleTunnel')).toBeNull();
+  });
+});
