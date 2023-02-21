b93a366fbac6157b4e522a8197a67ffc64915740
Romain Le Cellier <rlecellier@gmail.com>
2023-02-21 10:33:45 +0100

    ✨(backend) implement feature flags from .env (#1912)
    We need to activate / disable some features during development.
    This will let us push partial features (only front,only back for
    example).

---
+++ src/richie/apps/core/templatetags/feature_flags.py
@@ -0,0 +1,15 @@
+"""Custom template tags related to FEATURES settings."""
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.simple_tag()
+def is_feature_enabled(flag):
+    """
+    template tag to know if a feature flag is enable / disable
+    """
+    if hasattr(settings, "FEATURES"):
+        return settings.FEATURES.get(flag, False)
+    return False
