ecaf78b8a416279aefb1be6200956b49e74f507e
jbpenrath <jb.penrath@gmail.com>
2023-02-22 15:49:33 +0100

    ✅(frontend) prevent useCreditCard tests to fail
    useCreditCard test suite seems to be flaky. By inspecting the code it appears we
     did not wrap the resolution of a deferred promise into an act while this
     instruction may cause a rerender.
    
    Co-authored-by: NathanVss <nathan.vasse@gmail.com>

--- src/frontend/js/utils/test/expectSpinner.ts
+++ src/frontend/js/utils/test/expectSpinner.ts
@@ -1,7 +1,13 @@
-import { screen, waitFor } from '@testing-library/react';
+import { screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 
 export const expectSpinner = async (name?: string) => {
   await waitFor(() => {
     screen.getByRole('status', { name });
+  });
+};
+
+export const expectNoSpinner = async (name?: string) => {
+  await waitForElementToBeRemoved(() => {
+    return screen.getByRole('status', { name });
   });
 };
