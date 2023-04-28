7fee9be6200ed36f153137614435e11efa264314
Romain Le Cellier <rlecellier@gmail.com>
2023-04-28 11:41:44 +0200

    ✨(frontend) create homemade factory engine
    we decide to remove helix from this project because:
    * it doesn't use Typescript
    * it doesn't use the last version of faker

---
+++ src/frontend/js/utils/test/factories/factories.spec.ts
@@ -0,0 +1,157 @@
+import { faker } from '@faker-js/faker';
+import { factory } from './factories';
+
+interface DemoPointOfInterest {
+  name: string;
+}
+
+interface DemoAddress {
+  street: string;
+  city: string;
+  pointOfInterests: DemoPointOfInterest[];
+}
+
+interface DemoUser {
+  fullname: string;
+  username?: string;
+  mainAddress: DemoAddress;
+  addresses: DemoAddress[];
+}
+
+describe('factory', () => {
+  const DemoPointOfInterestFactory = factory<DemoPointOfInterest>(() => {
+    return {
+      name: faker.random.word(),
+    };
+  });
+  const DemoAddressFactory = factory<DemoAddress>(() => {
+    return {
+      city: faker.address.city(),
+      street: faker.address.street(),
+      pointOfInterests: DemoPointOfInterestFactory().many(3),
+    };
+  });
+  const DemoUserFactory = factory<DemoUser>((options) => {
+    let fullname = faker.name.fullName();
+    if (options?.generateIndex !== undefined) {
+      fullname += `_${options.generateIndex}`;
+    }
+    return {
+      fullname,
+      username: faker.internet.userName(),
+      mainAddress: DemoAddressFactory().one(),
+      addresses: DemoAddressFactory().many(3),
+      favoriteColors: Array(2).fill(faker.color.human()),
+    };
+  });
+
+  it('generates one entity', () => {
+    const generatedUser = DemoUserFactory({
+      fullname: 'toto',
+      mainAddress: { city: 'Paris' },
+    }).one();
+
+    expect(generatedUser.fullname).toEqual('toto');
+    expect(generatedUser.username).not.toEqual('toto');
+    expect(Object.keys(generatedUser)).toEqual([
+      'fullname',
+      'username',
+      'mainAddress',
+      'addresses',
+      'favoriteColors',
+    ]);
+    expect(typeof generatedUser.username).toEqual('string');
+    expect(typeof generatedUser.mainAddress.street).toEqual('string');
+    expect(generatedUser.mainAddress.city).toEqual('Paris');
+    expect(generatedUser.addresses.length).toEqual(3);
+  });
+
+  it('generates many entities', () => {
+    const generatedUserList = DemoUserFactory({
+      fullname: 'toto',
+      mainAddress: { city: 'Paris' },
+    }).many(2);
+
+    expect(generatedUserList.length).toEqual(2);
+    const [firstUser, secondUser] = generatedUserList;
+    expect(firstUser.fullname).toEqual('toto');
+    expect(firstUser.username).not.toEqual('toto');
+    expect(Object.keys(firstUser)).toEqual([
+      'fullname',
+      'username',
+      'mainAddress',
+      'addresses',
+      'favoriteColors',
+    ]);
+    expect(typeof firstUser.username).toEqual('string');
+    expect(typeof secondUser.mainAddress.street).toEqual('string');
+    expect(secondUser.mainAddress.city).toEqual('Paris');
+
+    expect(secondUser.fullname).toEqual('toto');
+    expect(secondUser.username).not.toEqual('toto');
+    expect(Object.keys(secondUser)).toEqual([
+      'fullname',
+      'username',
+      'mainAddress',
+      'addresses',
+      'favoriteColors',
+    ]);
+    expect(typeof secondUser.username).toEqual('string');
+    expect(typeof secondUser.mainAddress.street).toEqual('string');
+    expect(secondUser.mainAddress.city).toEqual('Paris');
+  });
+
+  // generatedIndex only work's on factory definition
+  // it's not working with override data.
+  it('allow usage of "generatedIndex" in factory\'s property', () => {
+    const generatedUserList = DemoUserFactory().many(2);
+    const [firstUser, secondUser] = generatedUserList;
+    expect(firstUser.fullname.slice(-2)).toEqual('_0');
+    expect(secondUser.fullname.slice(-2)).toEqual('_1');
+  });
+
+  it('generates override property with undefined', () => {
+    const generatedUser = DemoUserFactory({
+      username: undefined,
+    }).one();
+    expect(generatedUser.username).toBeUndefined();
+  });
+
+  it('generates override array property', () => {
+    const generatedUser = DemoUserFactory({
+      fullname: 'toto',
+      addresses: DemoAddressFactory().many(1),
+    }).one();
+    expect(generatedUser.addresses.length).toEqual(1);
+  });
+
+  it('generates override deep array property', () => {
+    const generatedUser = DemoUserFactory({
+      fullname: 'toto',
+      addresses: [
+        {
+          city: 'skyline',
+          street: 'angel street',
+          pointOfInterests: [{ name: 'some clouds' }],
+        },
+      ],
+    }).one();
+    expect(generatedUser.addresses.length).toEqual(1);
+    const [address] = generatedUser.addresses;
+    expect(address.pointOfInterests.length).toEqual(1);
+  });
+
+  it('generates override deep array property with sub factories', () => {
+    const generatedUser = DemoUserFactory({
+      fullname: 'toto',
+      addresses: DemoAddressFactory({
+        city: 'skyline',
+        street: 'angel street',
+        pointOfInterests: DemoPointOfInterestFactory({ name: 'some clouds' }).many(1),
+      }).many(1),
+    }).one();
+    expect(generatedUser.addresses.length).toEqual(1);
+    const [address] = generatedUser.addresses;
+    expect(address.pointOfInterests.length).toEqual(1);
+  });
+});
---
+++ src/frontend/js/utils/test/factories/factories.ts
@@ -0,0 +1,93 @@
+import { RecordKey } from '@faker-js/faker/modules/helpers/unique';
+import mergeWith from 'lodash-es/mergeWith';
+import isPlainObject from 'lodash-es/isPlainObject';
+
+const UNDEFINED_PLACEHOLDER = '%%UNDEFINED%%';
+
+type Subset<K> = {
+  [attr in keyof K]?: K[attr] extends object
+    ? Subset<K[attr]>
+    : K[attr] extends object | null
+    ? Subset<K[attr]> | null
+    : K[attr] extends object | null | undefined
+    ? Subset<K[attr]> | null | undefined
+    : K[attr];
+};
+
+interface FactoryBuilderOptions {
+  generateIndex?: number;
+}
+type FactoryBuilder<TData> = (options?: FactoryBuilderOptions) => TData;
+interface FactoryInterface<TData> {
+  one(): TData;
+  many(number: number): TData[];
+}
+
+const setUndefinedValues = (obj: any) => {
+  if (isPlainObject(obj)) {
+    return Object.entries(obj).reduce((acc: any, [key, value]: [PropertyKey, any]) => {
+      let parsedValue: any;
+      if (isPlainObject(value)) {
+        parsedValue = setUndefinedValues(value);
+      } else {
+        parsedValue = value;
+      }
+      return {
+        ...acc,
+        [key]: parsedValue === UNDEFINED_PLACEHOLDER ? undefined : parsedValue,
+      };
+    }, {});
+  }
+};
+
+type Factory<TData> = (override?: Subset<TData>) => FactoryInterface<TData>;
+export const factory = <TData>(builder: FactoryBuilder<TData>): Factory<TData> => {
+  const mergeData = (data: TData, override: Subset<TData>): TData => {
+    const customizer = (_objValue: any, srcValue: any) => {
+      if (Array.isArray(srcValue)) {
+        return srcValue;
+      }
+      // when we return undefined lodash.mergeWith use lodash.merge strategy
+      if (srcValue === undefined) {
+        return UNDEFINED_PLACEHOLDER;
+      }
+    };
+    const mergedData = mergeWith(data, override, customizer);
+
+    return setUndefinedValues(mergedData) as TData;
+  };
+  return (override: Subset<TData> = {}) => ({
+    one: (): TData => mergeData(builder(), override),
+    many: (amount: number): TData[] => {
+      return Array.from(Array(amount), (_, i) =>
+        mergeData(builder({ generateIndex: i }), override),
+      );
+    },
+  });
+};
+
+export class FactoryConfig {
+  static #isInternalConstructing = false;
+  static #instance: FactoryConfig;
+  static GLOBAL_UNIQUE_STORE: Record<RecordKey, RecordKey> = {};
+
+  constructor() {
+    if (!FactoryConfig.#isInternalConstructing) {
+      throw new TypeError('PrivateConstructor is not constructable');
+    }
+    FactoryConfig.#isInternalConstructing = false;
+  }
+
+  static getInstance(): FactoryConfig {
+    if (!FactoryConfig.#instance) {
+      FactoryConfig.#isInternalConstructing = true;
+      FactoryConfig.#instance = new FactoryConfig();
+    }
+
+    return FactoryConfig.#instance;
+  }
+
+  static resetUniqueStore(): void {
+    FactoryConfig.GLOBAL_UNIQUE_STORE = {};
+  }
+}
