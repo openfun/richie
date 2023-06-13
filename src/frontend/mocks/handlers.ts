0bd47bc53cf5ef745b94c3c82eabbcb7b2f00dfc
Romain Le Cellier <rlecellier@gmail.com>
2023-06-13 15:43:20 +0200

    ⚰️(frontend) remove unused mws mock
    Now that we've our course, product and organization endpoint in joanie, we no
    longer need theses mocked endpoints.

--- src/frontend/mocks/handlers.ts
+++ src/frontend/mocks/handlers.ts
@@ -1,4 +1,1 @@
-import { listOrganizations } from '../js/api/mocks/joanie/organizations';
-import { listCourses, getCourse } from '../js/api/mocks/joanie/courses';
-
-export const handlers = [listOrganizations, listCourses, getCourse];
+export const handlers = [];
