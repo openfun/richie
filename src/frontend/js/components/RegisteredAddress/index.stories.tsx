5a4387b72065b13d5d5ea46eea228fd68817d1eb
Romain Le Cellier <rlecellier@gmail.com>
2023-06-28 17:19:26 +0200

    🧑‍💻(frontend) add story for RegisteredAddress
    This component had no story, it's needed to implement cunningham Button

---
+++ src/frontend/js/components/RegisteredAddress/index.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { StorybookHelper } from 'utils/StorybookHelper';
+import { AddressFactory } from 'utils/test/factories/joanie';
+import RegisteredAddress from '.';
+
+export default {
+  component: RegisteredAddress,
+  parameters: {
+    docs: {
+      source: {
+        code: 'Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554',
+      },
+    },
+  },
+  args: {
+    address: AddressFactory().one(),
+  },
+  render: (args) => {
+    return StorybookHelper.wrapInApp(
+      <div className="SaleTunnel__modal modal" style={{ width: '600px' }}>
+        <div className="AddressesManagement">
+          <div className="address-form">
+            <RegisteredAddress {...args} />
+          </div>
+        </div>
+      </div>,
+    );
+  },
+} as Meta<typeof RegisteredAddress>;
+
+type Story = StoryObj<typeof RegisteredAddress>;
+
+export const Default: Story = {};
+export const MainAddress: Story = {
+  args: {
+    address: AddressFactory({ is_main: true }).one(),
+  },
+};
