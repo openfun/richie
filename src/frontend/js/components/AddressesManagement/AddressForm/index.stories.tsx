56df36064070e1754aae5a4e3d322f6bb4c6cf76
Romain Le Cellier <rlecellier@gmail.com>
2023-06-28 16:59:19 +0200

    🧑‍💻(frontend) add story for AddressForm
    AddressForm had no story, it's needed to implement cunningham Button

---
+++ src/frontend/js/components/AddressesManagement/AddressForm/index.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { StorybookHelper } from 'utils/StorybookHelper';
+import AddressForm from '.';
+
+export default {
+  component: AddressForm,
+  parameters: {
+    docs: {
+      source: {
+        code: 'Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554',
+      },
+    },
+  },
+  render: (args) => {
+    return StorybookHelper.wrapInApp(
+      <div className="SaleTunnel__modal">
+        <div className="AddressesManagement" style={{ width: '600px' }}>
+          <div className="address-form">
+            <AddressForm {...args} />
+          </div>
+        </div>
+      </div>,
+    );
+  },
+} as Meta<typeof AddressForm>;
+
+type Story = StoryObj<typeof AddressForm>;
+
+export const Default: Story = {};
+//   export const RichieCourse: Story = {
+//     args: {
+//       context: RichieContextFactory().one(),
+//       course: getCourseGlimpseProps(CourseLightFactory().one()),
+//     },
+//   };
