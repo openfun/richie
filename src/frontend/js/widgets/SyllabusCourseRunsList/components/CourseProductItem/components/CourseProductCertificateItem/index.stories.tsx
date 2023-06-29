b14b359939ecb12317ccf96bcccc477fd083c644
Romain Le Cellier <rlecellier@gmail.com>
2023-06-29 12:07:35 +0200

    🧑‍💻(frontend) add story for CertificateItem
    This component had no story, it's needed to implement cunningham Button.

---
+++ src/frontend/js/widgets/SyllabusCourseRunsList/components/CourseProductItem/components/CourseProductCertificateItem/index.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { CertificationDefinitionFactory, OrderLiteFactory } from 'utils/test/factories/joanie';
+import JoanieApiProvider from 'contexts/JoanieApiContext';
+import { StorybookHelper } from 'utils/StorybookHelper';
+import CertificateItem from '.';
+
+export default {
+  component: CertificateItem,
+  parameters: {
+    docs: {
+      source: {
+        code: 'Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554',
+      },
+    },
+  },
+  args: {
+    certificateDefinition: CertificationDefinitionFactory().one(),
+    order: OrderLiteFactory({ certificate: "Demo User's certificate for cours Demo Course" }).one(),
+  },
+  render: (args) => {
+    return StorybookHelper.wrapInApp(
+      <JoanieApiProvider>
+        <CertificateItem {...args} />
+      </JoanieApiProvider>,
+    );
+  },
+} as Meta<typeof CertificateItem>;
+
+type Story = StoryObj<typeof CertificateItem>;
+
+export const Default: Story = {};
