97b096608b1f1a52ab552a6dbbe49e8b52b25aa2
Romain Le Cellier <rlecellier@gmail.com>
2023-06-29 14:51:08 +0200

    🧑‍💻(frontend) add story for CourseRunItem
    This component had no story, it's needed to implement cunningham Button

---
+++ src/frontend/js/widgets/SyllabusCourseRunsList/components/CourseProductItem/components/CourseRunItem/index.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { OrderFactory, TargetCourseFactory } from 'utils/test/factories/joanie';
+import { StorybookHelper } from 'utils/StorybookHelper';
+import { createTestQueryClient } from 'utils/test/createTestQueryClient';
+import { UserFactory } from 'utils/test/factories/richie';
+import CourseRunItem from '.';
+
+export default {
+  component: CourseRunItem,
+  parameters: {
+    docs: {
+      source: {
+        code: 'Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554',
+      },
+    },
+  },
+  render: (args) => {
+    return StorybookHelper.wrapInApp(
+      <QueryClientProvider client={createTestQueryClient({ user: UserFactory().one() })}>
+        <CourseRunItem {...args} />
+      </QueryClientProvider>,
+    );
+  },
+} as Meta<typeof CourseRunItem>;
+
+type Story = StoryObj<typeof CourseRunItem>;
+
+export const Default: Story = {
+  args: {
+    targetCourse: TargetCourseFactory().one(),
+    order: OrderFactory({ certificate: "Demo User's certificate for cours Demo Course" }).one(),
+  },
+};
