db7f02ed68259d1314414cc710d486120d06893e
Romain Le Cellier <rlecellier@gmail.com>
2023-07-21 15:24:57 +0200

    🧑‍💻(frontend) update DashboardBox story to cunningham
    This story needed a update after implemation of cunningham Button.

--- src/frontend/js/widgets/Dashboard/components/DashboardBox/index.stories.tsx
+++ src/frontend/js/widgets/Dashboard/components/DashboardBox/index.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Button } from 'components/Button';
+import { Button } from '@openfun/cunningham-react';
 import { DashboardBox } from './index';
 
 export default {
