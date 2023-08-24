1d396e5b56e5fe9cd8bbee111b27c442aa0c08eb
jbpenrath <jb.penrath@gmail.com>
2023-08-24 22:58:07 +0200

    ✨(frontend) add a ProductHelper util
    Create an helper class for product to easily get product date range, aggregated
    distinct languages and human readable languages from product target course runs.

---
+++ src/frontend/js/utils/ProductHelper/index.ts
@@ -0,0 +1,44 @@
+import { IntlShape } from 'react-intl';
+import { Product, TargetCourse } from 'types/Joanie';
+import { Maybe } from 'types/utils';
+import { IntlHelper } from 'utils/IntlHelper';
+
+/**
+ * Helper class for products
+ *
+ * It provides following util methods :
+ * - getDateRange: Return the product date range processed from course runs dates
+ * - getLanguages: Return the product languages from course runs, optionally humanized
+ */
+export class ProductHelper {
+  // Private methods
+  static #getFlatTargetCourseRuns = (product: Product) => {
+    return product.target_courses.flatMap((course: TargetCourse) => course.course_runs);
+  };
+
+  // Public methods
+  static getDateRange(product: Product) {
+    const courseRuns = this.#getFlatTargetCourseRuns(product);
+
+    const minRawDate = Math.min(...courseRuns.map(({ start }) => new Date(start).getTime()));
+    const maxRawDate = Math.max(...courseRuns.map(({ end }) => new Date(end).getTime()));
+
+    const minDate = Number.isNaN(minRawDate) ? undefined : new Date(minRawDate);
+    const maxDate = Number.isNaN(maxRawDate) ? undefined : new Date(maxRawDate);
+
+    return [minDate, maxDate];
+  }
+
+  static getLanguages(product: Product, humanized?: false): string[];
+  static getLanguages(product: Product, humanized: true, intl: IntlShape): string;
+  static getLanguages(product: Product, humanized = false, intl: Maybe<IntlShape> = undefined) {
+    const courseRuns = this.#getFlatTargetCourseRuns(product);
+    const languages = courseRuns.flatMap((courseRun) => courseRun.languages);
+
+    const uniqueLanguages = [...new Set(languages)];
+
+    if (!humanized || !intl) return uniqueLanguages;
+
+    return IntlHelper.getLocalizedLanguages(uniqueLanguages, intl);
+  }
+}
