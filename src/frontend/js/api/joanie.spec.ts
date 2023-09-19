a506c265ea9187f65ea8266d3b630eaf5cc65f8a
Romain Le Cellier <rlecellier@gmail.com>
2023-09-19 16:06:18 +0200

    ♻️(frontend) api.joanie: add buildApiUrl
    we create a function to handle url build that was repeat over and over.

--- src/frontend/js/api/joanie.spec.ts
+++ src/frontend/js/api/joanie.spec.ts
@@ -1,5 +1,6 @@
 import fetchMock from 'fetch-mock';
-import { getResponseBody } from './joanie';
+import { ResourcesQuery } from 'hooks/useResources';
+import { buildApiUrl, getResponseBody } from './joanie';
 
 describe('api/joanie', () => {
   it('getResponse should handle empty response body', async () => {
@@ -9,5 +10,22 @@ describe('api/joanie', () => {
     const responseBody = await getResponseBody(res);
     expect(responseBody).toBe('');
     fetchMock.restore();
+  });
+  it('buildApiUrl should build api url', () => {
+    interface TestApiFilters extends ResourcesQuery {
+      firstname: string;
+      lastname: string;
+      gender: string[];
+    }
+    const apiFilters: TestApiFilters = {
+      id: 'DUMMY_TEST_API_ID',
+      firstname: 'John',
+      lastname: 'Do',
+      gender: ['male', 'robot'],
+    };
+    const url = buildApiUrl('test/:id/', apiFilters);
+    expect(url).toEqual(
+      'test/DUMMY_TEST_API_ID/?firstname=John&gender=male&gender=robot&lastname=Do',
+    );
   });
 });
