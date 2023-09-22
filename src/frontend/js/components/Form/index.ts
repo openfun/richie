e1cddc16ab07a5f9ad07f6b71c8e8c2a0b35df63
Romain Le Cellier <rlecellier@gmail.com>
2023-09-22 16:37:08 +0200

    ⚰️(frontend) remove old form inputs
    These inputs aren't used anymore. All of our forms use cunningham
    components

--- src/frontend/js/components/Form/index.ts
+++ src/frontend/js/components/Form/index.ts
@@ -1,1 +1,5 @@
-export * from './Inputs';
+export { default as Select } from './Select';
+export { CountrySelectField } from './CountrySelectField';
+export { messages } from './messages';
+export * from './utils';
+export * from './ValidationErrors';
