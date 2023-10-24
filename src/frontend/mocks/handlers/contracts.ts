66daf25395359c8f8c995055e941fb7cabe0a069
jbpenrath <jb.penrath@gmail.com>
2023-10-24 17:07:54 +0200

    ⬆️(frontend) migrate to msw 2.0
    MSW 2.0 has been released and it introduces a new API so we have to update our
    codebase accordingly. Furthermore this version only supports node>=18.
    
    https://mswjs.io/docs/migrations/1.x-to-2.x

--- src/frontend/mocks/handlers/contracts.ts
+++ src/frontend/mocks/handlers/contracts.ts
@@ -1,19 +1,16 @@
-import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 import { getAPIEndpoint } from 'api/joanie';
 import { Contract, PaginatedResponse } from 'types/Joanie';
 import { ContractFactory } from 'utils/test/factories/joanie';
 import { PER_PAGE } from 'settings';
 
 export default [
-  rest.get<PaginatedResponse<Contract>>(`${getAPIEndpoint()}/contracts/`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        count: 250,
-        results: ContractFactory().many(PER_PAGE.teacherContractList),
-        next: null,
-        previous: null,
-      }),
-    );
+  http.get(`${getAPIEndpoint()}/contracts/`, () => {
+    return HttpResponse.json<PaginatedResponse<Contract>>({
+      count: 250,
+      results: ContractFactory().many(PER_PAGE.teacherContractList),
+      next: null,
+      previous: null,
+    });
   }),
 ];
