ebfe2a843c43aace9161dd11abf69250d66091be
Nathan Vasse <nathan.vasse@gmail.com>
2023-11-03 17:11:38 +0100

    ♻️(frontend) create a generic SuccessIcon
    This icon will be re-used in the signing tunnel, so we need it to be
    generic.

---
+++ src/frontend/js/components/SuccessIcon/index.tsx
@@ -0,0 +1,10 @@
+export const SuccessIcon = () => {
+  return (
+    <svg className="success-icon" role="img" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <circle className="circle back" cx="16" cy="16" r="14" />
+      <circle className="circle" cx="16" cy="16" r="14" />
+      <polyline className="line back" points="7.5 15.5 14 22.5 24 10.5" />
+      <polyline className="line" points="7.5 15.5 14 22.5 24 10.5" />
+    </svg>
+  );
+};
