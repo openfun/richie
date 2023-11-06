7764ac681febde5ba928eadee32e15925bcb609a
Romain Le Cellier <rlecellier@gmail.com>
2023-11-06 15:17:52 +0100

    ♻️(frontend) adapt CoursesHelper with order type
    CoursesHelper are restricted to CredentialOrder

--- src/frontend/js/utils/CoursesHelper/index.ts
+++ src/frontend/js/utils/CoursesHelper/index.ts
@@ -1,17 +1,16 @@
-import * as Joanie from 'types/Joanie';
-import { Enrollment } from 'types/Joanie';
+import { TargetCourse, AbstractCourse, Enrollment, CredentialOrder } from 'types/Joanie';
 
 export class CoursesHelper {
-  static findActiveCourseEnrollmentInOrder(targetCourse: Joanie.TargetCourse, order: Joanie.Order) {
+  static findActiveCourseEnrollmentInOrder(targetCourse: TargetCourse, order: CredentialOrder) {
     return CoursesHelper.findActiveEnrollment(targetCourse, order.target_enrollments);
   }
-  static findActiveEnrollment(targetCourse: Joanie.AbstractCourse, enrollments: Enrollment[]) {
+  static findActiveEnrollment(targetCourse: AbstractCourse, enrollments: Enrollment[]) {
     const courseRunIds = targetCourse.course_runs.map(({ id }) => id);
     return enrollments.find(({ is_active, course_run }) => {
       return is_active && courseRunIds.includes(course_run.id);
     });
   }
-  static findCourseEnrollmentsInOrder(targetCourse: Joanie.AbstractCourse, order: Joanie.Order) {
+  static findCourseEnrollmentsInOrder(targetCourse: AbstractCourse, order: CredentialOrder) {
     const courseRunIds = targetCourse.course_runs.map(({ id }) => id);
     return order.target_enrollments.filter(({ course_run }) => {
       return courseRunIds.includes(course_run.id);
