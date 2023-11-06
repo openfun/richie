5509536e0ee8675d608456fd654b8bbd3e99205d
Romain Le Cellier <rlecellier@gmail.com>
2023-11-06 15:14:05 +0100

    ♻️(frontend) useOrdersEnrollments with order type
    useOrderEnrollments should only return CredentialOrder

--- src/frontend/js/pages/DashboardCourses/useOrdersEnrollments.tsx
+++ src/frontend/js/pages/DashboardCourses/useOrdersEnrollments.tsx
@@ -1,14 +1,33 @@
 import { defineMessages } from 'react-intl';
 import { useJoanieApi } from 'contexts/JoanieApiContext';
-import { Enrollment, Order, PaginatedResourceQuery } from 'types/Joanie';
+import {
+  Enrollment,
+  PaginatedResourceQuery,
+  CredentialOrder,
+  CertificateOrder,
+} from 'types/Joanie';
 import useUnionResource, { ResourceUnionPaginationProps } from 'hooks/useUnionResource';
 import { PER_PAGE } from 'settings';
 import { OrderResourcesQuery } from 'hooks/useOrders';
 
-export const isOrder = (obj: Order | Enrollment): obj is Order => {
+export const isOrder = (
+  obj: CredentialOrder | CertificateOrder | Enrollment,
+): obj is CredentialOrder | CertificateOrder => {
   return 'total' in obj && 'target_enrollments' in obj;
 };
-export const isEnrollement = (obj: Order | Enrollment): obj is Enrollment => {
+export const isCertificateOrder = (
+  obj: CredentialOrder | CertificateOrder | Enrollment,
+): obj is CertificateOrder => {
+  return isOrder(obj) && !!obj.enrollment;
+};
+export const isCredentialOrder = (
+  obj: CredentialOrder | CertificateOrder | Enrollment,
+): obj is CredentialOrder => {
+  return isOrder(obj) && !!obj.course;
+};
+export const isEnrollment = (
+  obj: CredentialOrder | CertificateOrder | Enrollment,
+): obj is Enrollment => {
   return 'was_created_by_order' in obj && 'course_run' in obj;
 };
 
@@ -30,7 +49,7 @@ export const useOrdersEnrollments = ({
 }: UseOrdersEnrollmentsProps = {}) => {
   const api = useJoanieApi();
   return useUnionResource<
-    Order,
+    CredentialOrder | CertificateOrder,
     Enrollment,
     PaginatedResourceQuery,
     { was_created_by_order: boolean } & PaginatedResourceQuery
