c9e0ab42c160e02b69b34b365a802ebe94abe043
Manuel Raynaud <manu@raynaud.io>
2023-11-16 14:56:06 +0100

    👷(circleci) publish python package on every merge on master
    We use setuptools_scm to automatically generate the release version of
    richie. When the current commit is a tag, the version tag will be used.
    Otherwise it guess the next version number with unique info like the
    current commit hash. The CI is refactor to take advantage of this
    feature.

--- src/richie/__init__.py
+++ src/richie/__init__.py
@@ -1,26 +1,7 @@
 """
 Richie: a FUN portal for Open edX
 """
-from __future__ import absolute_import, unicode_literals
-
-import os
-
-import pkg_resources
-from setuptools.config import read_configuration
-
-PROJECT_DIR = os.path.join(os.path.dirname(__file__), "../..")
+from importlib_metadata import version
 
 
-def _extract_version(package_name):
-    """
-    Get package version from installed distribution or configuration file if not
-    installed
-    """
-    try:
-        return pkg_resources.get_distribution(package_name).version
-    except pkg_resources.DistributionNotFound:
-        _conf = read_configuration(os.path.join(PROJECT_DIR, "pyproject.toml"))
-    return _conf["project"]["version"]
-
-
-__version__ = _extract_version("richie")
+__version__ = version("richie")
