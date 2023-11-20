cc586f0aa00d60e066929c004a7d825140d9bfbb
Romain Le Cellier <rlecellier@gmail.com>
2023-11-20 16:16:32 +0100

    ♻️(frontend) update DashboardItem.Order.Contract
    Use new SignContractButton in our DashboardItemOrder

--- src/frontend/js/widgets/Dashboard/components/DashboardItem/Order/Contract/index.tsx
+++ src/frontend/js/widgets/Dashboard/components/DashboardItem/Order/Contract/index.tsx
@@ -1,87 +1,19 @@
-import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
-import { Button, Loader } from '@openfun/cunningham-react';
 import { DashboardItem } from 'widgets/Dashboard/components/DashboardItem/index';
 import { Contract, CredentialOrder, Product } from 'types/Joanie';
-import { RouterButton } from 'widgets/Dashboard/components/RouterButton';
-import { LearnerDashboardPaths } from 'widgets/Dashboard/utils/learnerRouteMessages';
-import { getDashboardRoutePath } from 'widgets/Dashboard/utils/dashboardRoutes';
-import { useJoanieApi } from 'contexts/JoanieApiContext';
-import { alert } from 'utils/indirection/window';
-import { handle } from 'utils/errors/handle';
 import ContractStatus from 'components/ContractStatus';
+import SignContractButton from 'components/SignContractButton';
 
-const messages = defineMessages({
-  contractSignActionLabel: {
-    id: 'components.DashboardItemOrder.contractSignActionLabel',
-    description: 'Label of "sign contract" action.',
-    defaultMessage: 'Sign',
-  },
-  contractDownloadActionLabel: {
-    id: 'components.DashboardItemOrder.contractDownloadActionLabel',
-    description: 'Label of "download contract" action.',
-    defaultMessage: 'Download',
-  },
-  contractDownloadError: {
-    id: 'components.DashboardItemOrder.contractDownloadError',
-    description: "Message displayed when the order's contract download fails.",
-    defaultMessage: 'An error happened while downloading the contract. Please try again later.',
-  },
-});
+interface DashboardItemOrderContractFooterProps {
+  order: CredentialOrder;
+  contract?: Contract;
+  writable: boolean;
+}
 
 export const DashboardItemOrderContractFooter = ({
   order,
   contract,
-  onSign,
-  loading,
   writable,
-}: {
-  order: CredentialOrder;
-  contract?: Contract;
-  mode?: 'default' | 'compact';
-  onSign?: () => void;
-  loading?: boolean;
-  writable: boolean;
-}) => {
-  const getRoutePath = getDashboardRoutePath(useIntl());
-  const api = useJoanieApi();
-  const intl = useIntl();
-
-  const downloadContract = async () => {
-    try {
-      const blob = await api.user.contracts.download(contract!.id);
-      // eslint-disable-next-line compat/compat
-      const file = window.URL.createObjectURL(blob);
-      window.open(file);
-
-      // eslint-disable-next-line compat/compat
-      URL.revokeObjectURL(file);
-    } catch (e) {
-      handle(e);
-      alert(intl.formatMessage(messages.contractDownloadError));
-    }
-  };
-
-  const renderSignButton = () => {
-    if (writable) {
-      return (
-        <Button className="dashboard-item__button" size="small" onClick={onSign} disabled={loading}>
-          <FormattedMessage {...messages.contractSignActionLabel} />
-        </Button>
-      );
-    }
-    return (
-      <RouterButton
-        size="small"
-        href={getRoutePath(LearnerDashboardPaths.ORDER, {
-          orderId: order.id,
-        })}
-        className="dashboard-item__button"
-      >
-        <FormattedMessage {...messages.contractSignActionLabel} />
-      </RouterButton>
-    );
-  };
-
+}: DashboardItemOrderContractFooterProps) => {
   return (
     <div
       className="dashboard-item-order__footer"
@@ -90,13 +22,7 @@ export const DashboardItemOrderContractFooter = ({
       <div className="dashboard-item__block__status">
         <ContractStatus contract={contract} />
       </div>
-      {contract?.signed_on ? (
-        <Button className="dashboard-item__button" color="secondary" onClick={downloadContract}>
-          <FormattedMessage {...messages.contractDownloadActionLabel} />
-        </Button>
-      ) : (
-        renderSignButton()
-      )}
+      <SignContractButton order={order} writable={writable} className="dashboard-item__button" />
     </div>
   );
 };
@@ -104,32 +30,21 @@ export const DashboardItemOrderContractFooter = ({
 export const DashboardItemOrderContract = ({
   order,
   product,
-  onSign,
-  loading,
   writable,
 }: {
   order: CredentialOrder;
   product: Product;
   writable: boolean;
-  onSign?: () => void;
-  loading?: boolean;
 }) => {
   return (
     <div className="dashboard__contract-item" data-testid="dashboard-item-order-contract">
       <DashboardItem
-        title={
-          <>
-            {loading && <Loader size="small" />}
-            {product.contract_definition!.title}
-          </>
-        }
+        title={product.contract_definition!.title}
         code=""
         footer={
           <DashboardItemOrderContractFooter
             order={order}
             contract={order.contract}
-            onSign={onSign}
-            loading={loading}
             writable={writable}
           />
         }
