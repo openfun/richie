258f38a23ffb480b71c975bda7f16214abb6b810
Romain Le Cellier <rlecellier@gmail.com>
2023-11-22 14:22:41 +0100

    ✨(frontend) hide teacher dashboard link
    The teacher dashboard access must be limited to user that are linked to
    a course or an organization.
    
    remove joanie user from session

--- src/frontend/js/widgets/UserLogin/index.not.isJoanieEnabled.spec.tsx
+++ src/frontend/js/widgets/UserLogin/index.not.isJoanieEnabled.spec.tsx
@@ -12,11 +12,10 @@ import {
 import { Deferred } from 'utils/test/deferred';
 import context from 'utils/context';
 import { createTestQueryClient } from 'utils/test/createTestQueryClient';
-import { JoanieUserApiAbilityActions, User } from 'types/User';
+import { User } from 'types/User';
 import { HttpStatusCode } from 'utils/errors/HttpError';
-import JoanieSessionProvider from 'contexts/SessionContext/JoanieSessionProvider';
-import { JoanieUserProfileFactory } from 'utils/test/factories/joanie';
 import { Nullable } from 'types/utils';
+import BaseSessionProvider from 'contexts/SessionContext/BaseSessionProvider';
 import UserLogin from '.';
 
 jest.mock('utils/errors/handle', () => ({
@@ -38,9 +37,6 @@ jest.mock('utils/context', () => ({
       backend: 'fonzie',
       endpoint: 'https://auth.local.test',
     },
-    joanie_backend: {
-      endpoint: 'https://endpoint.test',
-    },
   }).one(),
 }));
 
@@ -51,24 +47,14 @@ describe('<UserLogin />', () => {
   }: PropsWithChildren & { user?: Nullable<User | boolean> }) => (
     <QueryClientProvider client={createTestQueryClient({ user })}>
       <IntlProvider locale="en">
-        <JoanieSessionProvider>{children}</JoanieSessionProvider>
+        <BaseSessionProvider>{children}</BaseSessionProvider>
       </IntlProvider>
     </QueryClientProvider>
   );
-  beforeEach(() => {
-    fetchMock
-      .get('https://endpoint.test/api/v1.0/orders/', [])
-      .get('https://endpoint.test/api/v1.0/addresses/', [])
-      .get('https://endpoint.test/api/v1.0/credit-cards/', []);
-  });
-  afterEach(() => {
-    fetchMock.restore();
-  });
 
   it('gets and renders the user name and a dropdown containing a logout link', async () => {
     const user: User = UserFactory().one();
 
-    fetchMock.get('https://endpoint.test/api/v1.0/users/me/', JoanieUserProfileFactory().one());
     render(
       <Wrapper user={user}>
         <UserLogin context={context} />
@@ -90,7 +76,6 @@ describe('<UserLogin />', () => {
     const loginDeferred = new Deferred();
     fetchMock.get('https://auth.test/api/user/v1/me', loginDeferred.promise);
 
-    fetchMock.get('https://endpoint.test/api/v1.0/users/me/', JoanieUserProfileFactory().one());
     render(
       <Wrapper user={null}>
         <UserLogin context={context} />
@@ -107,23 +92,13 @@ describe('<UserLogin />', () => {
     expect(screen.queryByText('Loading login status...')).not.toBeInTheDocument();
   });
 
-  it('should renders profile urls and bind user info if needed, for user without abilities', async () => {
+  it('should renders profile urls and bind user info if needed', async () => {
     const user: User = UserFactory().one();
     const profileUrls = {
       settings: { label: 'Settings', action: 'https://auth.local.test/settings' },
       account: { label: 'Account', action: 'https://auth.local.test/u/(username)' },
-      dashboard_teacher: { label: 'Teacher dashboard', action: 'https://dashboard.teacher.url' },
     };
 
-    fetchMock.get(
-      'https://endpoint.test/api/v1.0/users/me/',
-      JoanieUserProfileFactory({
-        abilities: {
-          [JoanieUserApiAbilityActions.HAS_ORGANIZATION_ACCESS]: false,
-          [JoanieUserApiAbilityActions.HAS_COURSE_ACCESS]: false,
-        },
-      }).one(),
-    );
     render(
       <Wrapper user={user}>
         <UserLogin context={context} profileUrls={profileUrls} />
@@ -141,45 +116,5 @@ describe('<UserLogin />', () => {
     const accountLink = screen.getByRole('link', { name: 'Account' });
     expect(settingsLink.getAttribute('href')).toEqual('https://auth.local.test/settings');
     expect(accountLink.getAttribute('href')).toEqual(`https://auth.local.test/u/${user.username}`);
-    expect(screen.queryByRole('link', { name: 'Teacher dashboard' })).not.toBeInTheDocument();
-  });
-
-  it('should renders teacher dashboard menu item for user with organization access', async () => {
-    const user: User = UserFactory().one();
-    const profileUrls = {
-      settings: { label: 'Settings', action: 'https://auth.local.test/settings' },
-      account: { label: 'Account', action: 'https://auth.local.test/u/(username)' },
-      dashboard_teacher: { label: 'Teacher dashboard', action: 'https://dashboard.teacher.url' },
-    };
-
-    fetchMock.get(
-      'https://endpoint.test/api/v1.0/users/me/',
-      JoanieUserProfileFactory({
-        abilities: {
-          [JoanieUserApiAbilityActions.HAS_ORGANIZATION_ACCESS]: true,
-        },
-      }).one(),
-    );
-    render(
-      <Wrapper user={user}>
-        <UserLogin context={context} profileUrls={profileUrls} />
-      </Wrapper>,
-    );
-
-    const button = await screen.findByLabelText(`Access to your profile settings`, {
-      selector: 'button',
-    });
-
-    await userEvent.click(button);
-
-    screen.getByText(user.username);
-    const settingsLink = screen.getByRole('link', { name: 'Settings' });
-    const accountLink = screen.getByRole('link', { name: 'Account' });
-    expect(settingsLink.getAttribute('href')).toEqual('https://auth.local.test/settings');
-    expect(accountLink.getAttribute('href')).toEqual(`https://auth.local.test/u/${user.username}`);
-
-    const teacherDashboardLink = screen.getByRole('link', { name: 'Teacher dashboard' });
-    expect(teacherDashboardLink).toBeInTheDocument();
-    expect(teacherDashboardLink.getAttribute('href')).toEqual('https://dashboard.teacher.url');
   });
 });
