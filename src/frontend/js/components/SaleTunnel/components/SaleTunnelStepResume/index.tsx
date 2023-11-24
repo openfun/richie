d3cb928c54e4f6990c1a443aab08a5464de7e545
Nathan Vasse <nathan.vasse@gmail.com>
2023-11-24 15:07:09 +0100

    ✨(frontend) add sign button to sale tunnel
    If a product has a contract definition we display a slightly different
    final step inside the sale tunnel, we tell the user to sign its contract
    before being able to enroll to any course run.

--- src/frontend/js/components/SaleTunnel/components/SaleTunnelStepResume/index.tsx
+++ src/frontend/js/components/SaleTunnel/components/SaleTunnelStepResume/index.tsx
@@ -1,6 +1,9 @@
 import { Button } from '@openfun/cunningham-react';
-import { defineMessages, FormattedMessage } from 'react-intl';
+import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 import { SuccessIcon } from 'components/SuccessIcon';
+import { LearnerDashboardPaths } from 'widgets/Dashboard/utils/learnerRouteMessages';
+import { getDashboardBasename } from 'widgets/Dashboard/hooks/useDashboardRouter/getDashboardBasename';
+import { useSaleTunnelContext } from 'components/SaleTunnel/context';
 
 const messages = defineMessages({
   congratulations: {
@@ -18,10 +21,21 @@ const messages = defineMessages({
     description: "Text to remind that order's invoice will be send by email soon",
     id: 'components.SaleTunnelStepResume.successDetailMessage',
   },
+  successDetailSignatureMessage: {
+    defaultMessage:
+      'In order to enroll to course runs you first need to sign the training contract.',
+    description: 'Text to remind that order needs to be signed',
+    id: 'components.SaleTunnelStepResume.successDetailSignatureMessage',
+  },
   cta: {
     defaultMessage: 'Start this course now!',
     description: 'Label to the call to action to close sale tunnel',
     id: 'components.SaleTunnelStepResume.cta',
+  },
+  ctaSignature: {
+    defaultMessage: 'Sign the training contract',
+    description: 'Label to the call to action to close sale tunnel if there is a pending signature',
+    id: 'components.SaleTunnelStepResume.ctaSignature',
   },
 });
 
@@ -29,23 +43,45 @@ interface SaleTunnelStepResumeProps {
   next: () => void;
 }
 
-export const SaleTunnelStepResume = ({ next }: SaleTunnelStepResumeProps) => (
-  <section className="SaleTunnelStepResume">
-    <header className="SaleTunnelStepResume__header">
-      <SuccessIcon />
-      <h3 className="SaleTunnelStepResume__title">
-        <FormattedMessage {...messages.congratulations} />
-      </h3>
-    </header>
-    <p className="SaleTunnelStepResume__content">
-      <FormattedMessage {...messages.successMessage} />
-      <br />
-      <FormattedMessage {...messages.successDetailMessage} />
-    </p>
-    <footer className="SaleTunnelStepResume__footer">
-      <Button onClick={next}>
-        <FormattedMessage {...messages.cta} />
-      </Button>
-    </footer>
-  </section>
-);
+export const SaleTunnelStepResume = ({ next }: SaleTunnelStepResumeProps) => {
+  const intl = useIntl();
+  const { order, product } = useSaleTunnelContext();
+
+  return (
+    <section className="SaleTunnelStepResume">
+      <header className="SaleTunnelStepResume__header">
+        <SuccessIcon />
+        <h3 className="SaleTunnelStepResume__title">
+          <FormattedMessage {...messages.congratulations} />
+        </h3>
+      </header>
+      <p className="SaleTunnelStepResume__content">
+        <FormattedMessage {...messages.successMessage} />
+        <br />
+        <FormattedMessage {...messages.successDetailMessage} />
+        {product.contract_definition && (
+          <>
+            <br />
+            <FormattedMessage {...messages.successDetailSignatureMessage} />
+          </>
+        )}
+      </p>
+      <footer className="SaleTunnelStepResume__footer">
+        {product.contract_definition ? (
+          <Button
+            href={
+              getDashboardBasename(intl.locale) +
+              LearnerDashboardPaths.ORDER.replace(':orderId', order!.id)
+            }
+          >
+            <FormattedMessage {...messages.ctaSignature} />
+          </Button>
+        ) : (
+          <Button onClick={next}>
+            <FormattedMessage {...messages.cta} />
+          </Button>
+        )}
+      </footer>
+    </section>
+  );
+};
