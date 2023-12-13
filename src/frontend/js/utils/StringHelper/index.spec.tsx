efaec3cea91fe5bfaf4e5cf6a5022c0ff0230bc2
jbpenrath <jb.penrath@gmail.com>
2023-12-13 14:17:17 +0100

    ✨(frontend) add abbreviate method to StringHelper
    Add an abbreviate method into StringHelper in charge of returning abbreviation
    of a provided sentence.

--- src/frontend/js/utils/StringHelper/index.spec.tsx
+++ src/frontend/js/utils/StringHelper/index.spec.tsx
@@ -4,18 +4,23 @@ describe('StringHelper', () => {
   it('is a string', () => {
     expect(StringHelper.isString('Hello')).toBe(true);
   });
+
   it('is a ReactNode', () => {
     expect(StringHelper.isString(<div>Hello</div>)).toBe(false);
   });
+
   it('is null', () => {
     expect(StringHelper.isString(null)).toBe(false);
   });
+
   it('is a number', () => {
     expect(StringHelper.isString(10)).toBe(false);
   });
+
   it('is zero', () => {
     expect(StringHelper.isString(0)).toBe(false);
   });
+
   it('capitalizes first letter of a string', () => {
     expect(StringHelper.capitalizeFirst('a')).toBe('A');
     expect(StringHelper.capitalizeFirst('A')).toBe('A');
@@ -25,5 +30,11 @@ describe('StringHelper', () => {
     expect(StringHelper.capitalizeFirst('')).toBe('');
     expect(StringHelper.capitalizeFirst(null)).toBe(null);
     expect(StringHelper.capitalizeFirst(undefined)).toBe(undefined);
+  });
+
+  it('abbreviates a string', () => {
+    expect(StringHelper.abbreviate('John Doe')).toBe('JD');
+    expect(StringHelper.abbreviate('John doe')).toBe('JD');
+    expect(StringHelper.abbreviate('John Doe', 1)).toBe('J');
   });
 });
