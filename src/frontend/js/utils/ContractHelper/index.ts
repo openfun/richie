6a057036b28cb8906b5aa696380c32c8fd6eb080
jbpenrath <jb.penrath@gmail.com>
2023-12-17 23:03:18 +0100

    ✨(frontend) add ContractHelper to compute state
    A contract expects to be signed by both learner and organization, in some of our
     views we need to get the signature state. That's the purpose of this
     ContractHelper util. It is able to return the raw state or a human readable
     label. Furthermore, according to point of view of the user (learner or
     organization), label should be adapted.

---
+++ src/frontend/js/utils/ContractHelper/index.ts
@@ -0,0 +1,77 @@
+import { defineMessages, IntlShape } from 'react-intl';
+import { Contract, ContractState } from 'types/Joanie';
+import { StringHelper } from 'utils/StringHelper';
+import { Maybe, Nullable } from 'types/utils';
+
+const messages = defineMessages({
+  organizationUnsigned: {
+    id: 'utils.ContractHelper.organizationUnsigned',
+    description: 'Label for unsigned contract status in organization point of view',
+    defaultMessage: 'Pending for learner signature',
+  },
+  organizationHalfSigned: {
+    id: 'utils.ContractHelper.organizationHalfSigned',
+    description: 'Label for half signed contract status in organization point of view',
+    defaultMessage: 'Pending for signature',
+  },
+  organizationSigned: {
+    id: 'utils.ContractHelper.organizationSigned',
+    description: 'Label for signed contract status in organization point of view',
+    defaultMessage: 'Signed',
+  },
+  learnerUnsigned: {
+    id: 'utils.ContractHelper.learnerUnsigned',
+    description: 'Label for unsigned contract status in learner point of view',
+    defaultMessage: 'Pending for signature',
+  },
+  learnerHalfSigned: {
+    id: 'utils.ContractHelper.learnerHalfSigned',
+    description: 'Label for unsigned contract status in learner point of view',
+    defaultMessage: 'Pending for organization signature',
+  },
+  learnerSigned: {
+    id: 'utils.ContractHelper.learnerSigned',
+    description: 'Label for signed contract status in learner point of view',
+    defaultMessage: 'Signed',
+  },
+});
+
+export enum ContractStatePoV {
+  LEARNER = 'learner',
+  ORGANIZATION = 'organization',
+}
+
+export class ContractHelper {
+  static getState(contract: null | undefined): null;
+  static getState(contract: Contract): ContractState;
+  static getState(contract: Maybe<Contract>): Nullable<ContractState>;
+  static getState(contract: Maybe<Nullable<Contract>>): Nullable<ContractState> {
+    if (!contract) return null;
+
+    if (contract.student_signed_on && contract.organization_signed_on) {
+      return ContractState.SIGNED;
+    }
+
+    if (contract?.student_signed_on) {
+      return ContractState.LEARNER_SIGNED;
+    }
+
+    return ContractState.UNSIGNED;
+  }
+
+  static getStateLabel(state: ContractState, pov: ContractStatePoV, intl: IntlShape) {
+    const camelCasedState = state
+      .toLowerCase()
+      .split('_')
+      .map(StringHelper.capitalizeFirst)
+      .join('');
+    const messageKey = `${pov}${camelCasedState}` as keyof typeof messages;
+    const message = messages[messageKey];
+
+    return intl.formatMessage(message);
+  }
+  static getHumanReadableState(contract: Contract, pov: ContractStatePoV, intl: IntlShape) {
+    const state = this.getState(contract);
+    return this.getStateLabel(state, pov, intl);
+  }
+}
