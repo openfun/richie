8bf76bbbd00349df4018afe15176f5a36c518a9a
jbpenrath <jb.penrath@gmail.com>
2023-12-17 22:01:24 +0100

    🗑️(frontend) hide contracts menu in course views
    Currently in teacher dashboard, we are displaying contract in courses and
    organizations views. For now, we decide to show this menu only on an
    organization page. Indeed, display contracts within a "simple" course (not a
    training) has no meaning. About trainings, we have to rework to manner to get
    contracts with right filters and ensure that we are able to add signature
    feature on those views. That's why we decide to hide those menus for now and
    once we will have setup download and signature on organization view we will work
     to make it work on course views.

--- src/frontend/js/widgets/Dashboard/components/TeacherDashboardCourseSidebar/utils.ts
+++ src/frontend/js/widgets/Dashboard/components/TeacherDashboardCourseSidebar/utils.ts
@@ -7,19 +7,13 @@ interface GetMenuRoutesArgs {
 export const getMenuRoutes = ({ courseProductRelationId, organizationId }: GetMenuRoutesArgs) => {
   if (organizationId) {
     if (courseProductRelationId) {
-      return [
-        TeacherDashboardPaths.ORGANIZATION_PRODUCT,
-        TeacherDashboardPaths.ORGANIZATION_PRODUCT_CONTRACTS,
-      ];
+      return [TeacherDashboardPaths.ORGANIZATION_PRODUCT];
     }
-    return [
-      TeacherDashboardPaths.ORGANIZATION_COURSE_GENERAL_INFORMATION,
-      TeacherDashboardPaths.ORGANIZATION_COURSE_CONTRACTS,
-    ];
+    return [TeacherDashboardPaths.ORGANIZATION_COURSE_GENERAL_INFORMATION];
   }
 
   if (courseProductRelationId) {
-    return [TeacherDashboardPaths.COURSE_PRODUCT, TeacherDashboardPaths.COURSE_PRODUCT_CONTRACTS];
+    return [TeacherDashboardPaths.COURSE_PRODUCT];
   }
-  return [TeacherDashboardPaths.COURSE_GENERAL_INFORMATION, TeacherDashboardPaths.COURSE_CONTRACTS];
+  return [TeacherDashboardPaths.COURSE_GENERAL_INFORMATION];
 };
