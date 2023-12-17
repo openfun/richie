abe6c4e5d1fa820c284580dae167cb8f566dfb33
jbpenrath <jb.penrath@gmail.com>
2023-12-17 22:10:39 +0100

    ✨(frontend) add omit method to ObjectHelper
    The omit method aims to easily remove given properties of an object and infer
    the right returned type.

--- src/frontend/js/utils/ObjectHelper/index.spec.ts
+++ src/frontend/js/utils/ObjectHelper/index.spec.ts
@@ -1,19 +1,27 @@
 import { ObjectHelper } from 'utils/ObjectHelper/index';
 
 describe('ObjectHelper', () => {
-  it('should return true if object is empty', () => {
-    expect(ObjectHelper.isEmpty({})).toBe(true);
+  describe('isEmpty', () => {
+    it('should return true if object is empty', () => {
+      expect(ObjectHelper.isEmpty({})).toBe(true);
+    });
+
+    it('should return false if object is not empty', () => {
+      expect(ObjectHelper.isEmpty({ foo: 'bar' })).toBe(false);
+    });
+
+    it('should return true if array is empty', () => {
+      expect(ObjectHelper.isEmpty([])).toBe(true);
+    });
+
+    it('should return true if array is not empty', () => {
+      expect(ObjectHelper.isEmpty(['foo'])).toBe(false);
+    });
   });
 
-  it('should return false if object is not empty', () => {
-    expect(ObjectHelper.isEmpty({ foo: 'bar' })).toBe(false);
-  });
-
-  it('should return true if array is empty', () => {
-    expect(ObjectHelper.isEmpty([])).toBe(true);
-  });
-
-  it('should return true if array is not empty', () => {
-    expect(ObjectHelper.isEmpty(['foo'])).toBe(false);
+  describe('omit', () => {
+    it('should return an object without the given keys', () => {
+      expect(ObjectHelper.omit({ foo: 1, bar: 2, baz: 3 }, 'foo', 'baz')).toEqual({ bar: 2 });
+    });
   });
 });
--- src/frontend/js/utils/ObjectHelper/index.ts
+++ src/frontend/js/utils/ObjectHelper/index.ts
@@ -2,4 +2,13 @@ export class ObjectHelper {
   static isEmpty(obj: object): boolean {
     return !Object.entries(obj).length;
   }
+
+  static omit<Obj, Keys extends Array<keyof Obj>>(
+    obj: Obj,
+    ...keys: Keys
+  ): Omit<Obj, (typeof keys)[number]> {
+    const cleanedObject = { ...obj };
+    keys.forEach((key) => delete cleanedObject[key]);
+    return cleanedObject;
+  }
 }
