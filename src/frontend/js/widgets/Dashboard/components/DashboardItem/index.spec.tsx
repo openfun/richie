7c441c9046c6e7b3959d81985fbd2ec33081566a
Romain Le Cellier <rlecellier@gmail.com>
2023-12-18 15:49:32 +0100

    ♻️(frontend) add display mode to DashboardItem
    We have place where css are used in order to show/hide a DashboardItem header.
    It should work just fine but with new usage, some test fails when receiving
    hidden elements.
    This full/compact mode allows to formalize the two usage we've to a
    dashboard element:
    * as listing item
    * as sub-DashboardItem, typicaly in a detail page

--- src/frontend/js/widgets/Dashboard/components/DashboardItem/index.spec.tsx
+++ src/frontend/js/widgets/Dashboard/components/DashboardItem/index.spec.tsx
@@ -4,16 +4,43 @@ import { DashboardSubItem } from './DashboardSubItem';
 import { DashboardItem, DEMO_IMAGE_URL } from '.';
 
 describe('<DashboardItem />', () => {
-  it('renders a basic DashboardItem', () => {
-    render(<DashboardItem title="Become a React pro" code="Ref. 123" imageUrl={DEMO_IMAGE_URL} />);
+  it('renders a DashboardItem in "full" mode', () => {
+    render(
+      <DashboardItem
+        title="Become a React pro"
+        code="Ref. 123"
+        imageUrl={DEMO_IMAGE_URL}
+        footer="Hi, i'm the footer"
+      />,
+    );
 
-    screen.getByText('Become a React pro');
-    screen.getByText('Ref. 123');
+    expect(screen.getByText('Become a React pro')).toBeInTheDocument();
+    expect(screen.getByText('Ref. 123')).toBeInTheDocument();
+    expect(screen.getByText("Hi, i'm the footer")).toBeInTheDocument();
     expect(screen.getByTestId('dashboard-item__block__head__thumbnail')).toHaveAttribute(
       'src',
       DEMO_IMAGE_URL,
     );
   });
+
+  it('renders a DashboardItem in "compact" mode', () => {
+    render(
+      <DashboardItem
+        title="Become a React pro"
+        code="Ref. 123"
+        imageUrl={DEMO_IMAGE_URL}
+        footer="Hi, i'm the footer"
+        mode="compact"
+      />,
+    );
+
+    expect(screen.getByText("Hi, i'm the footer")).toBeInTheDocument();
+
+    expect(screen.queryByText('Become a React pro')).not.toBeInTheDocument();
+    expect(screen.queryByText('Ref. 123')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('dashboard-item__block__head__thumbnail')).not.toBeInTheDocument();
+  });
+
   it('renders a DashboardItem with children', () => {
     render(
       <DashboardItem title="Become a React pro" code="Ref. 123" imageUrl={DEMO_IMAGE_URL}>
--- src/frontend/js/widgets/Dashboard/components/DashboardItem/index.tsx
+++ src/frontend/js/widgets/Dashboard/components/DashboardItem/index.tsx
@@ -2,55 +2,67 @@ import { PropsWithChildren, ReactNode } from 'react';
 import { JoanieFile } from 'types/Joanie';
 import { Nullable, PropsWithTestId } from 'types/utils';
 
-type Props = PropsWithTestId<{
+export type DashboardItemProps = PropsWithTestId<{
   title: ReactNode;
   code?: string;
   imageUrl?: string;
   imageFile?: Nullable<JoanieFile>;
   footer?: ReactNode;
+  mode?: 'full' | 'compact';
 }>;
 
 // This is temporary due to the fact that backend doesn't give this attribute yet.
 export const DEMO_IMAGE_URL =
   'https://dz03nossv7tsm.cloudfront.net/media/filer_public_thumbnails/filer_public/12/32/123267a1-bc28-49ca-bfca-679568c34043/logo_upsaclay-2020.jpg__200x113_q85_replace_alpha-%23FFFFFF_subsampling-2_upscale.jpg';
 
-export const DashboardItem = (props: PropsWithChildren<Props>) => {
+export const DashboardItem = ({
+  title,
+  code,
+  imageUrl,
+  imageFile,
+  footer,
+  mode = 'full',
+  children,
+  ...props
+}: PropsWithChildren<DashboardItemProps>) => {
   return (
     <div className="dashboard-item" data-testid={props['data-testid']}>
       <div className="dashboard-item__block">
-        <header
-          className={[
-            'dashboard-item__block__head',
-            props.imageUrl ? 'dashboard-item__block__head--with-image' : '',
-          ].join(' ')}
-        >
-          {props.imageFile ? (
-            <img
-              data-testid="dashboard-item__block__head__thumbnail"
-              className="dashboard-item__block__head__thumbnail"
-              sizes={props.imageFile.size + ''}
-              src={props.imageFile.src}
-              srcSet={props.imageFile.srcset}
-              alt=""
-            />
-          ) : (
-            props.imageUrl && (
+        {mode === 'full' && (
+          <header
+            className={[
+              'dashboard-item__block__head',
+              imageUrl ? 'dashboard-item__block__head--with-image' : '',
+            ].join(' ')}
+          >
+            {imageFile ? (
               <img
                 data-testid="dashboard-item__block__head__thumbnail"
                 className="dashboard-item__block__head__thumbnail"
-                src={props.imageUrl}
+                sizes={imageFile.size + ''}
+                src={imageFile.src}
+                srcSet={imageFile.srcset}
                 alt=""
               />
-            )
-          )}
-          <div className="dashboard-item__block__head__captions">
-            <h5 className="dashboard-item__block__head__title">{props.title}</h5>
-            <div className="dashboard-item__block__head__code">{props.code}</div>
-          </div>
-        </header>
-        <footer className="dashboard-item__block__footer">{props.footer}</footer>
+            ) : (
+              imageUrl && (
+                <img
+                  data-testid="dashboard-item__block__head__thumbnail"
+                  className="dashboard-item__block__head__thumbnail"
+                  src={imageUrl}
+                  alt=""
+                />
+              )
+            )}
+            <div className="dashboard-item__block__head__captions">
+              <h5 className="dashboard-item__block__head__title">{title}</h5>
+              <div className="dashboard-item__block__head__code">{code}</div>
+            </div>
+          </header>
+        )}
+        <footer className="dashboard-item__block__footer">{footer}</footer>
       </div>
-      {props.children}
+      {children}
     </div>
   );
 };
