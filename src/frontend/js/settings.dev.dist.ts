5efb2d0e1d95c36ec4fb8d72075b09ae96208374
Romain Le Cellier <rlecellier@gmail.com>
2024-01-23 16:10:30 +0100

    🧑‍💻(frontend) ignore dev user selection
    We've to change dev user during development and it's common to forget
    about it on commit.
    To avoid redundant task of switching dev user on each new branch, we
    moved this choice in an ignored file `settings.dev.ts`

---
+++ src/frontend/js/settings.dev.dist.ts
@@ -0,0 +1,14 @@
+export default {
+  /**
+   * Available users:
+   *  * admin
+   *  * user0
+   *  * user1
+   *  * user2
+   *  * user3
+   *  * user4
+   *  * organization_owner
+   *  * student_user
+   */
+  CURRENT_JOANIE_DEV_DEMO_USER: 'admin',
+};
