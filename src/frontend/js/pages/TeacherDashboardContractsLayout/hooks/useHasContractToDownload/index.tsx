2499c6f21c9437fb55738ad472e7c50897fc1e89
Romain Le Cellier <rlecellier@gmail.com>
2024-01-31 17:36:16 +0100

    🐛(frontend) fix hasContractToDownload filters
    useHasContractToDownload should filter by CourseProductRelationId when
    this information is in the route params

--- src/frontend/js/pages/TeacherDashboardContractsLayout/hooks/useHasContractToDownload/index.tsx
+++ src/frontend/js/pages/TeacherDashboardContractsLayout/hooks/useHasContractToDownload/index.tsx
@@ -1,13 +1,17 @@
 import { useOrganizationContracts } from 'hooks/useContracts';
 import { PER_PAGE } from 'settings';
-import { ContractState, Organization } from 'types/Joanie';
+import { ContractState, CourseProductRelation, Organization } from 'types/Joanie';
 
-const useHasContractToDownload = (organizationId: Organization['id']) => {
+const useHasContractToDownload = (
+  organizationId: Organization['id'],
+  courseProductRelationId?: CourseProductRelation['id'],
+) => {
   const {
     items: contracts,
     states: { isFetched },
   } = useOrganizationContracts({
     organization_id: organizationId,
+    course_product_relation_id: courseProductRelationId,
     signature_state: ContractState.SIGNED,
     page: 1,
     page_size: PER_PAGE.teacherContractList,
