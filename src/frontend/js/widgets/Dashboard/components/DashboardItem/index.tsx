be17545f1b20a1953d96728922560a20f53f8363
Nathan Vasse <nathan.vasse@gmail.com>
2024-02-01 15:56:39 +0100

    ✨(frontend) add more button to DashboardItem
    We need a generic "more" button to DashboardItem in order to display
    various advanced options.

--- src/frontend/js/widgets/Dashboard/components/DashboardItem/index.tsx
+++ src/frontend/js/widgets/Dashboard/components/DashboardItem/index.tsx
@@ -1,6 +1,18 @@
 import { PropsWithChildren, ReactNode } from 'react';
+import { Button } from '@openfun/cunningham-react';
+import { useSelect } from 'downshift';
+import { defineMessages, useIntl } from 'react-intl';
 import { JoanieFile } from 'types/Joanie';
 import { Nullable, PropsWithTestId } from 'types/utils';
+import { Icon, IconTypeEnum } from 'components/Icon';
+
+const messages = defineMessages({
+  moreLabel: {
+    id: 'components.DashboardItem.more_label',
+    description: 'Accessible label for the more button on the dashboard item',
+    defaultMessage: 'See additional options',
+  },
+});
 
 export type DashboardItemProps = PropsWithTestId<{
   title: ReactNode;
@@ -9,6 +21,7 @@ export type DashboardItemProps = PropsWithTestId<{
   imageFile?: Nullable<JoanieFile>;
   footer?: ReactNode;
   mode?: 'full' | 'compact';
+  more?: ReactNode;
 }>;
 
 // This is temporary due to the fact that backend doesn't give this attribute yet.
@@ -23,6 +36,7 @@ export const DashboardItem = ({
   footer,
   mode = 'full',
   children,
+  more,
   ...props
 }: PropsWithChildren<DashboardItemProps>) => {
   return (
@@ -58,11 +72,37 @@ export const DashboardItem = ({
               <h5 className="dashboard-item__block__head__title">{title}</h5>
               <div className="dashboard-item__block__head__code">{code}</div>
             </div>
+            {more && <DashboardItemMore more={more} />}
           </header>
         )}
         <footer className="dashboard-item__block__footer">{footer}</footer>
       </div>
       {children}
+    </div>
+  );
+};
+
+const DashboardItemMore = ({ more }: { more: ReactNode }) => {
+  const intl = useIntl();
+  const { isOpen, getToggleButtonProps, getMenuProps } = useSelect({
+    items: [],
+  });
+
+  return (
+    <div className="selector dashboard-item__block__head__more">
+      <Button
+        icon={<Icon name={IconTypeEnum.MORE} />}
+        color="tertiary"
+        size="small"
+        aria-label={intl.formatMessage(messages.moreLabel)}
+        {...getToggleButtonProps()}
+      />
+      <ul
+        {...getMenuProps()}
+        className={`selector__list ${isOpen ? '' : 'selector__list--is-closed'}`}
+      >
+        {isOpen && more}
+      </ul>
     </div>
   );
 };
