5086368e2bc3e8dee49eb52a06e1eecb8161532e
Romain Le Cellier <rlecellier@gmail.com>
2024-02-01 14:27:22 +0100

    ✨(frontend) handle training in contract frame
    OrganizationContractFrame will be used in training page and needs to
    handle signing contracts for a single courseProductRelationId.

--- src/frontend/js/components/ContractFrame/OrganizationContractFrame.spec.tsx
+++ src/frontend/js/components/ContractFrame/OrganizationContractFrame.spec.tsx
@@ -6,9 +6,14 @@ import { IntlProvider } from 'react-intl';
 import fetchMock from 'fetch-mock';
 import { QueryStateFactory } from 'utils/test/factories/reactQuery';
 import { RichieContextFactory as mockRichieContextFactory } from 'utils/test/factories/richie';
-import { ContractFactory, OrganizationFactory } from 'utils/test/factories/joanie';
+import {
+  ContractFactory,
+  CourseProductRelationFactory,
+  OrganizationFactory,
+} from 'utils/test/factories/joanie';
 import { createTestQueryClient } from 'utils/test/createTestQueryClient';
 import JoanieSessionProvider from 'contexts/SessionContext/JoanieSessionProvider';
+import { isCourseProductRelation } from 'types/Joanie';
 import { Props } from './AbstractContractFrame';
 import { OrganizationContractFrame } from '.';
 
@@ -70,132 +75,93 @@ describe('OrganizationContractFrame', () => {
     fetchMock.restore();
   });
 
-  it('should implement AbstractContractFrame for organization', async () => {
-    const organization = OrganizationFactory().one();
-    const contract = ContractFactory().one();
-    const isOpen = faker.datatype.boolean();
+  it.each([
+    {
+      label: 'contractList: undefined, courseProductRelation: undefined',
+      contractList: undefined,
+      courseProductRelation: undefined,
+    },
+    {
+      label: 'contractList: 2 Contract, courseProductRelation: undefined',
+      contractList: ContractFactory().many(2),
+      courseProductRelation: undefined,
+    },
+    {
+      label: 'contractList: undefined, courseProductRelation: one CourseProductRelation',
+      contractList: undefined,
+      courseProductRelation: CourseProductRelationFactory().one(),
+    },
+  ])(
+    'should implement AbstractContractFrame for organization and $label',
+    async ({ contractList, courseProductRelation }) => {
+      const organization = OrganizationFactory().one();
+      const contracts = contractList || ContractFactory().many(2);
+      const isOpen = faker.datatype.boolean();
 
-    const expectedUrls = {
-      getInvitationLink: `https://joanie.endpoint/api/v1.0/organizations/${organization.id}/contracts-signature-link/`,
-      checkSignature: `https://joanie.endpoint/api/v1.0/organizations/${organization.id}/contracts/?id=${contract.id}`,
-    };
+      const invitationLinkQueryString =
+        courseProductRelation && isCourseProductRelation(courseProductRelation)
+          ? `?course_product_relation_ids=${courseProductRelation.id}`
+          : '';
+      const expectedUrls = {
+        getInvitationLink: `https://joanie.endpoint/api/v1.0/organizations/${organization.id}/contracts-signature-link/${invitationLinkQueryString}`,
+        checkSignature: `https://joanie.endpoint/api/v1.0/organizations/${organization.id}/contracts/?id=${contracts[0].id}&id=${contracts[1].id}`,
+      };
 
-    fetchMock
-      .get(expectedUrls.getInvitationLink, {
-        invitation_link: faker.internet.url(),
-        contract_ids: [contract.id],
-      })
-      .get(expectedUrls.checkSignature, { results: [contract] });
+      fetchMock
+        .get(expectedUrls.getInvitationLink, {
+          invitation_link: faker.internet.url(),
+          contract_ids: contracts.map((contract) => contract.id),
+        })
+        .get(expectedUrls.checkSignature, { results: [contracts] });
 
-    const handleDone = jest.fn();
-    const handleClose = jest.fn();
+      const handleDone = jest.fn();
+      const handleClose = jest.fn();
 
-    const client = createTestQueryClient({
-      user: true,
-      queriesCallback: (queries) => {
-        // Push contract and orders queries
-        queries.push(QueryStateFactory(['user', 'organization_contracts'], { data: [] }));
-      },
-    });
+      const client = createTestQueryClient({
+        user: true,
+        queriesCallback: (queries) => {
+          // Push contract and orders queries
+          queries.push(QueryStateFactory(['user', 'organization_contracts'], { data: [] }));
+        },
+      });
 
-    let contractsQueryState = client.getQueryState(['user', 'organization_contracts']);
-    expect(contractsQueryState?.isInvalidated).toBe(false);
+      let contractsQueryState = client.getQueryState(['user', 'organization_contracts']);
+      expect(contractsQueryState?.isInvalidated).toBe(false);
 
-    await act(async () => {
-      render(
-        <Wrapper client={client}>
-          <OrganizationContractFrame
-            organizationId={organization.id}
-            isOpen={isOpen}
-            onDone={handleDone}
-            onClose={handleClose}
-          />
-        </Wrapper>,
-      );
-    });
+      await act(async () => {
+        render(
+          <Wrapper client={client}>
+            <OrganizationContractFrame
+              organizationId={organization.id}
+              courseProductRelationIds={
+                courseProductRelation ? [courseProductRelation.id] : undefined
+              }
+              isOpen={isOpen}
+              onDone={handleDone}
+              onClose={handleClose}
+            />
+          </Wrapper>,
+        );
+      });
 
-    // isOpen should be passed down to AbstractContractFrame
-    const contractFrame = await screen.getByTestId('AbstractContractFrame');
-    expect(contractFrame).toHaveAttribute('data-is-open', String(isOpen));
+      // isOpen should be passed down to AbstractContractFrame
+      const contractFrame = screen.getByTestId('AbstractContractFrame');
+      expect(contractFrame).toHaveAttribute('data-is-open', String(isOpen));
 
-    // getInvitationLink should post on order/submit-for-signature endpoint
-    expect(fetchMock.called(expectedUrls.getInvitationLink)).toBe(true);
+      // getInvitationLink should post on order/submit-for-signature endpoint
+      expect(fetchMock.called(expectedUrls.getInvitationLink)).toBe(true);
 
-    // checkSignature should get on order endpoint
-    expect(fetchMock.called(expectedUrls.checkSignature)).toBe(true);
+      // checkSignature should get on order endpoint
+      expect(fetchMock.called(expectedUrls.checkSignature)).toBe(true);
 
-    // onDone should be tweaked to invalidate the user orders and contracts queries
-    // and passed down to AbstractContractFrame
-    contractsQueryState = client.getQueryState(['user', 'organization_contracts']);
-    expect(contractsQueryState?.isInvalidated).toBe(true);
-    expect(handleDone).toHaveBeenCalledTimes(1);
+      // onDone should be tweaked to invalidate the user orders and contracts queries
+      // and passed down to AbstractContractFrame
+      contractsQueryState = client.getQueryState(['user', 'organization_contracts']);
+      expect(contractsQueryState?.isInvalidated).toBe(true);
+      expect(handleDone).toHaveBeenCalledTimes(1);
 
-    // onClose should be passed down to AbstractContractFrame
-    expect(handleClose).toHaveBeenCalledTimes(1);
-  });
-
-  it('should implement AbstractContractFrame for organization with a list of contract ids', async () => {
-    const organization = OrganizationFactory().one();
-    const contracts = ContractFactory().many(2);
-    const isOpen = faker.datatype.boolean();
-
-    const expectedUrls = {
-      getInvitationLink: `https://joanie.endpoint/api/v1.0/organizations/${organization.id}/contracts-signature-link/?contracts_ids=${contracts[0].id}&contracts_ids=${contracts[1].id}`,
-      checkSignature: `https://joanie.endpoint/api/v1.0/organizations/${organization.id}/contracts/?id=${contracts[0].id}&id=${contracts[1].id}`,
-    };
-
-    fetchMock
-      .get(expectedUrls.getInvitationLink, {
-        invitation_link: faker.internet.url(),
-        contract_ids: contracts.map((contract) => contract.id),
-      })
-      .get(expectedUrls.checkSignature, { results: [contracts] });
-
-    const handleDone = jest.fn();
-    const handleClose = jest.fn();
-
-    const client = createTestQueryClient({
-      user: true,
-      queriesCallback: (queries) => {
-        // Push contract and orders queries
-        queries.push(QueryStateFactory(['user', 'organization_contracts'], { data: [] }));
-      },
-    });
-
-    let contractsQueryState = client.getQueryState(['user', 'organization_contracts']);
-    expect(contractsQueryState?.isInvalidated).toBe(false);
-
-    await act(async () => {
-      render(
-        <Wrapper client={client}>
-          <OrganizationContractFrame
-            organizationId={organization.id}
-            contractIds={contracts.map((contract) => contract.id)}
-            isOpen={isOpen}
-            onDone={handleDone}
-            onClose={handleClose}
-          />
-        </Wrapper>,
-      );
-    });
-
-    // isOpen should be passed down to AbstractContractFrame
-    const contractFrame = await screen.getByTestId('AbstractContractFrame');
-    expect(contractFrame).toHaveAttribute('data-is-open', String(isOpen));
-
-    // getInvitationLink should post on order/submit-for-signature endpoint
-    expect(fetchMock.called(expectedUrls.getInvitationLink)).toBe(true);
-
-    // checkSignature should get on order endpoint
-    expect(fetchMock.called(expectedUrls.checkSignature)).toBe(true);
-
-    // onDone should be tweaked to invalidate the user orders and contracts queries
-    // and passed down to AbstractContractFrame
-    contractsQueryState = client.getQueryState(['user', 'organization_contracts']);
-    expect(contractsQueryState?.isInvalidated).toBe(true);
-    expect(handleDone).toHaveBeenCalledTimes(1);
-
-    // onClose should be passed down to AbstractContractFrame
-    expect(handleClose).toHaveBeenCalledTimes(1);
-  });
+      // onClose should be passed down to AbstractContractFrame
+      expect(handleClose).toHaveBeenCalledTimes(1);
+    },
+  );
 });
