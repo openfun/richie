7b53a2721545bb60044cbc76e3564094beea61a9
Romain Le Cellier <rlecellier@gmail.com>
2024-03-11 14:20:04 +0100

    ✅(frontend) async expectBanner helpers
    our expectBanner helpers weren't properly async.
    by returning waitFor, these functions can now be await.

--- src/frontend/js/utils/test/expectBanner.ts
+++ src/frontend/js/utils/test/expectBanner.ts
@@ -1,34 +1,34 @@
 import { screen, waitFor } from '@testing-library/react';
 import { BannerType, getBannerTestId } from 'components/Banner';
 
-export const expectBannerError = async (message: string, rootElement: ParentNode = document) => {
+export const expectBannerError = (message: string, rootElement: ParentNode = document) => {
   return expectBanner(BannerType.ERROR, message, rootElement);
 };
-export const expectBannerInfo = async (message: string, rootElement: ParentNode = document) => {
+export const expectBannerInfo = (message: string, rootElement: ParentNode = document) => {
   return expectBanner(BannerType.INFO, message, rootElement);
 };
 
-export const expectBanner = async (
+export const expectBanner = (
   type: BannerType,
   message: string,
   rootElement: ParentNode = document,
 ) => {
-  await waitFor(async () => {
+  return waitFor(async () => {
     const banner = rootElement.querySelector('.banner--' + type) as HTMLElement;
     expect(banner).not.toBeNull();
     expect(banner).toHaveTextContent(message);
   });
 };
 
-export const expectNoBannerError = async (message: string) => {
+export const expectNoBannerError = (message: string) => {
   return expectNoBanner(BannerType.ERROR, message);
 };
-export const expectNoBannerInfo = async (message: string) => {
+export const expectNoBannerInfo = (message: string) => {
   return expectNoBanner(BannerType.INFO, message);
 };
 
-export const expectNoBanner = async (type: BannerType, message: string) => {
-  await waitFor(() => {
+export const expectNoBanner = (type: BannerType, message: string) => {
+  return waitFor(() => {
     expect(screen.queryByTestId(getBannerTestId(message, type))).toBeNull();
   });
 };
