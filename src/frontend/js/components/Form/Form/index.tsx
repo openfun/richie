20048cd3599e1c490e4fc7314974545d3643860f
Romain Le Cellier <rlecellier@gmail.com>
2024-04-02 17:29:05 +0200

    💄(frontend) add Form
    To easly reuse form layout, we add a Form component that centralize
    form's blocks

---
+++ src/frontend/js/components/Form/Form/index.tsx
@@ -0,0 +1,19 @@
+import c from 'classnames';
+import { PropsWithChildren } from 'react';
+
+interface FormProps
+  extends React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement> {}
+
+const Form = ({ children, onSubmit, className, name, noValidate = true }: FormProps) => {
+  return (
+    <form className={c('form', className)} name={name} onSubmit={onSubmit} noValidate={noValidate}>
+      {children}
+    </form>
+  );
+};
+
+Form.Row = ({ children, className }: PropsWithChildren<{ className?: string }>) => {
+  return <div className={c('form-row', className)}>{children}</div>;
+};
+
+export default Form;
--- src/frontend/js/components/Form/index.ts
+++ src/frontend/js/components/Form/index.ts
@@ -1,5 +1,8 @@
+import Form from './Form';
+
 export { default as Select } from './Select';
 export { CountrySelectField } from './CountrySelectField';
 export { messages } from './messages';
 export * from './utils';
 export * from './ValidationErrors';
+export default Form;
