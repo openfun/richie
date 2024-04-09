6f57e9ea0d70736314d91806aec14dd167552a38
Romain Le Cellier <rlecellier@gmail.com>
2024-04-09 17:24:32 +0200

    🧑‍💻(frontend) add SaleTunnel story
    add a story for the sale tunnel. It make it easy to update it.

---
+++ src/frontend/js/components/SaleTunnel/index.stories.tsx
@@ -0,0 +1,32 @@
+import { StoryObj, Meta } from '@storybook/react';
+import { BaseJoanieAppWrapper } from 'utils/test/wrappers/BaseJoanieAppWrapper';
+import { CourseFactory, ProductFactory } from 'utils/test/factories/joanie';
+import SaleTunnel, { SaleTunnelProps } from './index';
+
+export default {
+  // component: SaleTunnel,
+  render: (props?: SaleTunnelProps) => {
+    const defaultProps: SaleTunnelProps = {
+      isOpen: true,
+      product: ProductFactory().one(),
+      onClose: () => {},
+      course: CourseFactory().one(),
+      // enrollment?: Enrollment;
+      // product: CredentialProduct | CertificateProduct;
+      // orderGroup?: OrderGroup;
+      // onFinish?: (order: Order) => void;
+    };
+    return (
+      <BaseJoanieAppWrapper>
+        <SaleTunnel {...{ ...defaultProps, ...props }} />
+      </BaseJoanieAppWrapper>
+    );
+  },
+  // argTypes: {},
+} as Meta<typeof SaleTunnel>;
+
+type Story = StoryObj<typeof SaleTunnel>;
+
+export const Default: Story = {
+  args: {},
+};
