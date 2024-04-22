829daacc4427080626fe335a5fed0b030f0a0494
Romain Le Cellier <rlecellier@gmail.com>
2024-04-22 16:55:41 +0200

    ⚰️(frontend) remove comment
    remove debuging comment

--- src/frontend/js/utils/test/expectUrlMatchLocationDisplayed.ts
+++ src/frontend/js/utils/test/expectUrlMatchLocationDisplayed.ts
@@ -1,6 +1,5 @@
 import { screen } from '@testing-library/react';
 
 export const expectUrlMatchLocationDisplayed = (url: string) => {
-  // expect(screen.getByTestId('location-display').textContent).toMatch(new RegExp(url + '$'));
   screen.getByTestId('location-display-' + url);
 };
