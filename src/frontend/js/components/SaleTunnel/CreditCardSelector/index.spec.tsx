41f08e1e3336ab84c0ef06893520db4de5b7426e
Nathan Vasse <nathan.vasse@gmail.com>
2024-04-22 16:27:40 +0200

    ✨(front) add CreditCardSelector component
    This component is used to select an existing credit card or to
    choose to register a new one during payment.

---
+++ src/frontend/js/components/SaleTunnel/CreditCardSelector/index.spec.tsx
@@ -0,0 +1,321 @@
+import { screen, within } from '@testing-library/react';
+import { useMemo, useState } from 'react';
+import fetchMock from 'fetch-mock';
+import { faker } from '@faker-js/faker';
+import userEvent from '@testing-library/user-event';
+import { RichieContextFactory as mockRichieContextFactory } from 'utils/test/factories/richie';
+import { CreditCardSelector } from 'components/SaleTunnel/CreditCardSelector/index';
+import { render } from 'utils/test/render';
+import { CreditCard } from 'types/Joanie';
+import {
+  CredentialOrderFactory,
+  CreditCardFactory,
+  ProductFactory,
+} from 'utils/test/factories/joanie';
+import { SaleTunnelProps } from 'components/SaleTunnel/index';
+import { setupJoanieSession } from 'utils/test/wrappers/JoanieAppWrapper';
+import { expectNoSpinner, expectSpinner } from 'utils/test/expectSpinner';
+import { SaleTunnelStep, SaleTunnelContext, SaleTunnelContextType } from '../GenericSaleTunnel';
+
+jest.mock('utils/context', () => ({
+  __esModule: true,
+  default: mockRichieContextFactory({
+    authentication: { backend: 'fonzie', endpoint: 'https://auth.test' },
+    joanie_backend: { endpoint: 'https://joanie.endpoint' },
+  }).one(),
+}));
+
+jest.mock('utils/indirection/window', () => ({
+  matchMedia: () => ({
+    matches: true,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  }),
+}));
+
+describe('CreditCardSelector', () => {
+  setupJoanieSession();
+
+  const buildWrapper = () => {
+    const contextRef = {
+      current: {} as SaleTunnelContextType,
+    };
+
+    const Wrapper = () => {
+      const [creditCard, setCreditCard] = useState<CreditCard>();
+      const context: SaleTunnelContextType = useMemo(
+        () => ({
+          webAnalyticsEventKey: 'eventKey',
+          order: CredentialOrderFactory().one(),
+          product: ProductFactory().one(),
+          props: {} as SaleTunnelProps,
+          setBillingAddress: jest.fn(),
+          creditCard,
+          setCreditCard,
+          onPaymentSuccess: jest.fn(),
+          step: SaleTunnelStep.PAYMENT,
+          registerSubmitCallback: jest.fn(),
+          unregisterSubmitCallback: jest.fn(),
+          runSubmitCallbacks: jest.fn(),
+        }),
+        [creditCard],
+      );
+      contextRef.current = context;
+
+      return (
+        <SaleTunnelContext.Provider value={context}>
+          <CreditCardSelector />
+        </SaleTunnelContext.Provider>
+      );
+    };
+
+    return { contextRef, Wrapper };
+  };
+
+  // Make sure no edit button exists
+  it('renders component when no credit card exists, with no edit button', async () => {
+    const { Wrapper } = buildWrapper();
+    render(<Wrapper />);
+
+    screen.getByRole('heading', {
+      name: 'Payment method',
+    });
+    screen.getByText('Choose your payment method or add a new one during the payment.');
+
+    // During loading state, the spinner should be displayed and the current selected card should not be displayed.
+    expect(screen.queryByText('Add new credit card during payment')).not.toBeInTheDocument();
+    await expectSpinner();
+    await expectNoSpinner();
+
+    screen.getByText('Use another credit card during payment');
+
+    // As the user has no credit card, the edit button should not be displayed.
+    expect(
+      screen.queryByRole('button', {
+        name: 'Change credit card',
+      }),
+    ).not.toBeInTheDocument();
+  });
+
+  it('uses the main credit card as default', async () => {
+    const override = {
+      expiration_year: faker.date
+        .future({ refDate: new Date().setFullYear(new Date().getFullYear() + 2) })
+        .getFullYear(),
+    };
+    const mainCreditCard = CreditCardFactory({
+      is_main: true,
+      ...override,
+    }).one();
+
+    const creditCards = [CreditCardFactory().one(), CreditCardFactory().one(), mainCreditCard];
+    fetchMock.get('https://joanie.endpoint/api/v1.0/credit-cards/', creditCards, {
+      overwriteRoutes: true,
+    });
+
+    const { contextRef, Wrapper } = buildWrapper();
+    render(<Wrapper />);
+    await expectNoSpinner();
+
+    screen.getByTestId('credit-card-' + mainCreditCard.id);
+    screen.getByText(mainCreditCard.title!);
+    screen.getByText('Ends with •••• ' + mainCreditCard.last_numbers);
+    screen.getByText(
+      'Expires on ' +
+        mainCreditCard.expiration_month.toLocaleString(undefined, {
+          minimumIntegerDigits: 2,
+        }) +
+        '/' +
+        mainCreditCard.expiration_year,
+    );
+
+    expect(contextRef.current.creditCard!.id).toEqual(mainCreditCard.id);
+  });
+
+  it('is possible to change the selected credit card', async () => {
+    const override = {
+      expiration_year: faker.date
+        .future({ refDate: new Date().setFullYear(new Date().getFullYear() + 2) })
+        .getFullYear(),
+    };
+
+    const mainCreditCard = CreditCardFactory({
+      is_main: true,
+      ...override,
+    }).one();
+
+    const creditCards = [...CreditCardFactory(override).many(2), mainCreditCard];
+    fetchMock.get('https://joanie.endpoint/api/v1.0/credit-cards/', creditCards, {
+      overwriteRoutes: true,
+    });
+
+    const { contextRef, Wrapper } = buildWrapper();
+    render(<Wrapper />);
+    await expectNoSpinner();
+
+    screen.getByTestId('credit-card-' + mainCreditCard.id);
+    screen.getByText(mainCreditCard.title!);
+    screen.getByText('Ends with •••• ' + mainCreditCard.last_numbers);
+
+    expect(contextRef.current.creditCard!.id).toEqual(mainCreditCard.id);
+
+    const editButton = screen.getByRole('button', {
+      name: 'Change credit card',
+    });
+    const user = userEvent.setup();
+
+    await user.click(editButton);
+
+    await screen.findByTestId('credit-card-selector-modal');
+
+    await screen.findAllByText('Choose credit card');
+    creditCards.forEach((creditCard) => {
+      const container = screen.getAllByTestId('credit-card-' + creditCard.id)[0];
+      within(container).getByText(creditCard.title!);
+      within(container).getByText('Ends with •••• ' + creditCard.last_numbers);
+      within(container).getByText(
+        'Expires on ' +
+          creditCard.expiration_month.toLocaleString(undefined, {
+            minimumIntegerDigits: 2,
+          }) +
+          '/' +
+          creditCard.expiration_year,
+      );
+    });
+
+    const radio = screen.getByRole('radio', {
+      name: new RegExp(creditCards[0].title!),
+    });
+    await user.click(radio);
+
+    const submitButton = screen.getByRole('button', {
+      name: 'Choose credit card',
+    });
+    await user.click(submitButton);
+
+    expect(screen.queryByTestId('credit-card-selector-modal')).not.toBeInTheDocument();
+
+    expect(contextRef.current.creditCard!.id).toEqual(creditCards[0].id);
+    screen.getByTestId('credit-card-' + creditCards[0].id);
+    screen.getByText(creditCards[0].title!);
+    screen.getByText('Ends with •••• ' + creditCards[0].last_numbers);
+  });
+
+  it('is possible to unselect credit card to define a new one during payment via modal', async () => {
+    const override = {
+      expiration_year: faker.date
+        .future({ refDate: new Date().setFullYear(new Date().getFullYear() + 2) })
+        .getFullYear(),
+    };
+
+    const mainCreditCard = CreditCardFactory({
+      is_main: true,
+      ...override,
+    }).one();
+
+    const creditCards = [...CreditCardFactory(override).many(2), mainCreditCard];
+    fetchMock.get('https://joanie.endpoint/api/v1.0/credit-cards/', creditCards, {
+      overwriteRoutes: true,
+    });
+
+    const { contextRef, Wrapper } = buildWrapper();
+    render(<Wrapper />);
+    await expectNoSpinner();
+
+    screen.getByTestId('credit-card-' + mainCreditCard.id);
+    screen.getByText(mainCreditCard.title!);
+    screen.getByText('Ends with •••• ' + mainCreditCard.last_numbers);
+    expect(contextRef.current.creditCard!.id).toEqual(mainCreditCard.id);
+
+    expect(contextRef.current.creditCard!.id).toEqual(mainCreditCard.id);
+
+    const editButton = screen.getByRole('button', {
+      name: 'Change credit card',
+    });
+    const user = userEvent.setup();
+    await user.click(editButton);
+
+    const radio = screen.getByRole('radio', {
+      name: /Use another credit card during payment/i,
+    });
+    await user.click(radio);
+
+    const submitButton = screen.getByRole('button', {
+      name: 'Choose credit card',
+    });
+    await user.click(submitButton);
+
+    expect(screen.queryByTestId('credit-card-selector-modal')).not.toBeInTheDocument();
+    screen.getByText('Use another credit card during payment');
+    expect(contextRef.current.creditCard).toBeUndefined();
+  });
+
+  it('is possible to unselect credit card to define a new one during payment via shortcut', async () => {
+    const override = {
+      expiration_year: faker.date
+        .future({ refDate: new Date().setFullYear(new Date().getFullYear() + 2) })
+        .getFullYear(),
+    };
+
+    const mainCreditCard = CreditCardFactory({
+      is_main: true,
+      ...override,
+    }).one();
+
+    const creditCards = [...CreditCardFactory(override).many(2), mainCreditCard];
+    fetchMock.get('https://joanie.endpoint/api/v1.0/credit-cards/', creditCards, {
+      overwriteRoutes: true,
+    });
+
+    const { contextRef, Wrapper } = buildWrapper();
+    render(<Wrapper />);
+    await expectNoSpinner();
+
+    screen.getByTestId('credit-card-' + mainCreditCard.id);
+    screen.getByText(mainCreditCard.title!);
+    screen.getByText('Ends with •••• ' + mainCreditCard.last_numbers);
+    expect(screen.queryByText('Add new credit card during payment')).not.toBeInTheDocument();
+    expect(contextRef.current.creditCard!.id).toEqual(mainCreditCard.id);
+
+    const user = userEvent.setup();
+    const button = screen.getByRole('button', { name: /use another credit card during payment/i });
+    await user.click(button);
+
+    expect(screen.queryByTestId('credit-card-selector-modal')).not.toBeInTheDocument();
+    screen.getByText('Use another credit card during payment');
+    expect(contextRef.current.creditCard).toBeUndefined();
+  });
+
+  it('renders the correct label for expired date', async () => {
+    const now = new Date();
+    const date = faker.date.past({
+      refDate: new Date(now.getFullYear(), now.getMonth(), 0, 23, 59, 59),
+    });
+    const mainCreditCard = CreditCardFactory({
+      is_main: true,
+      expiration_month: date.getMonth() + 1,
+      expiration_year: date.getFullYear(),
+    }).one();
+
+    const creditCards = [mainCreditCard];
+    fetchMock.get('https://joanie.endpoint/api/v1.0/credit-cards/', creditCards, {
+      overwriteRoutes: true,
+    });
+
+    const { Wrapper } = buildWrapper();
+    render(<Wrapper />);
+    await expectNoSpinner();
+
+    screen.getByTestId('credit-card-' + mainCreditCard.id);
+    screen.getByText(mainCreditCard.title!);
+    screen.getByText('Ends with •••• ' + mainCreditCard.last_numbers);
+    screen.getByText(
+      'Expired since ' +
+        mainCreditCard.expiration_month.toLocaleString(undefined, {
+          minimumIntegerDigits: 2,
+        }) +
+        '/' +
+        mainCreditCard.expiration_year,
+    );
+  });
+});
---
+++ src/frontend/js/components/SaleTunnel/CreditCardSelector/index.tsx
@@ -0,0 +1,264 @@
+import {
+  Button,
+  Modal,
+  ModalProps,
+  ModalSize,
+  Radio,
+  RadioGroup,
+  useModal,
+} from '@openfun/cunningham-react';
+import { useEffect, useMemo, useState } from 'react';
+import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+import { CreditCardBrandLogo } from 'pages/DashboardCreditCardsManagement/CreditCardBrandLogo';
+import { CreditCardFactory } from 'utils/test/factories/joanie';
+import { CreditCard, CreditCardBrand } from 'types/Joanie';
+import { useCreditCardsManagement } from 'hooks/useCreditCardsManagement';
+import { Spinner } from 'components/Spinner';
+import { useSaleTunnelContext } from 'components/SaleTunnel/GenericSaleTunnel';
+import { CreditCardExpirationStatus, CreditCardHelper } from 'utils/CreditCardHelper';
+import { useMatchMediaLg } from 'hooks/useMatchMedia';
+
+const messages = defineMessages({
+  endsWith: {
+    id: 'components.SaleTunnel.CreditCardSelector.endsWith',
+    description: 'Text to show the credit card code',
+    defaultMessage: 'Ends with •••• {code}',
+  },
+  expiration: {
+    id: 'components.SaleTunnel.CreditCardSelector.expiration',
+    description: 'Text to show the credit card expiration date',
+    defaultMessage: 'Expires on {month}/{year}',
+  },
+  expired: {
+    id: 'components.SaleTunnel.CreditCardSelector.expired',
+    description: 'Text to show the credit card expired date',
+    defaultMessage: 'Expired since {month}/{year}',
+  },
+  title: {
+    id: 'components.SaleTunnel.CreditCardSelector.title',
+    description: 'Title for the credit card section',
+    defaultMessage: 'Payment method',
+  },
+  description: {
+    id: 'components.SaleTunnel.CreditCardSelector.description',
+    description: 'Description for the credit card section',
+    defaultMessage: 'Choose your payment method or add a new one during the payment.',
+  },
+  creditCardEmptyInlineDescription: {
+    id: 'components.SaleTunnel.CreditCardSelector.creditCardEmptyInlineDescription',
+    description: 'Description for the empty credit card inline',
+    defaultMessage: 'Use another credit card during payment',
+  },
+  modalTitle: {
+    id: 'components.SaleTunnel.CreditCardSelector.modalTitle',
+    description: 'Title for the credit card modal',
+    defaultMessage: 'Choose credit card',
+  },
+  modalValidate: {
+    id: 'components.SaleTunnel.CreditCardSelector.modalValidate',
+    description: 'Validate button for the credit card modal',
+    defaultMessage: 'Validate',
+  },
+  modalDescription: {
+    id: 'components.SaleTunnel.CreditCardSelector.modalDescription',
+    description: 'Description for the credit card modal',
+    defaultMessage:
+      'Choose the default payment method you want to use for this order and the upcoming payments.',
+  },
+  editCreditCardAriaLabel: {
+    id: 'components.SaleTunnel.CreditCardSelector.editCreditCardAriaLabel',
+    description: 'Aria label for the edit credit card button',
+    defaultMessage: 'Change credit card',
+  },
+});
+
+export const CreditCardSelector = () => {
+  const intl = useIntl();
+  const modal = useModal();
+  const isMobile = useMatchMediaLg();
+
+  const {
+    states: { fetching },
+    items: creditCards,
+  } = useCreditCardsManagement();
+
+  const { creditCard, setCreditCard } = useSaleTunnelContext();
+
+  const getDefaultCreditCard = () => {
+    if (creditCards.length === 0) {
+      return;
+    }
+    const mainCreditCard = creditCards.find((_creditCard) => _creditCard.is_main);
+    if (mainCreditCard) {
+      return mainCreditCard;
+    }
+    return creditCards[0];
+  };
+
+  useEffect(() => {
+    if (!creditCard) {
+      setCreditCard(getDefaultCreditCard());
+    }
+  }, [creditCards]);
+
+  return (
+    <div className="credit-card-selector">
+      <h4 className="block-title mb-t">
+        <FormattedMessage {...messages.title} />
+      </h4>
+      <div className="description mb-s">
+        <FormattedMessage {...messages.description} />
+      </div>
+      {fetching ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="credit-card-selector__content">
+            {creditCard ? <CreditCardInline creditCard={creditCard} /> : <CreditCardEmptyInline />}
+
+            {creditCards?.length > 0 && (
+              <Button
+                icon={<span className="material-icons">edit</span>}
+                color="tertiary-text"
+                size="medium"
+                onClick={modal.open}
+                aria-label={intl.formatMessage(messages.editCreditCardAriaLabel)}
+              />
+            )}
+          </div>
+          {creditCard && (
+            <Button
+              onClick={() => setCreditCard(undefined)}
+              size="small"
+              color="tertiary"
+              className="mt-t"
+              fullWidth={isMobile}
+            >
+              <FormattedMessage {...messages.creditCardEmptyInlineDescription} />
+            </Button>
+          )}
+          {/* This way we make sure the internal state of the modal is reset each time */}
+          {modal.isOpen && (
+            <CreditCardSelectorModal
+              {...modal}
+              defaultCreditCard={creditCard!}
+              onChange={(newCreditCard) => {
+                setCreditCard(newCreditCard);
+                modal.close();
+              }}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+const CreditCardInline = ({ creditCard }: { creditCard: CreditCard }) => {
+  const expirationState = useMemo(
+    () => CreditCardHelper.getExpirationState(creditCard),
+    [creditCard],
+  );
+  const expirationMessage =
+    expirationState === CreditCardExpirationStatus.EXPIRED ? messages.expired : messages.expiration;
+  return (
+    <div className="credit-card-selector__card" data-testid={`credit-card-${creditCard.id}`}>
+      <CreditCardBrandLogo
+        creditCard={{ ...CreditCardFactory().one(), brand: CreditCardBrand.MASTERCARD }}
+        variant="inline"
+      />
+      <div className="credit-card-selector__card__info">
+        <div className="credit-card-selector__card__info__title">{creditCard.title}</div>
+        <div className="credit-card-selector__card__info__meta">
+          <div>
+            <FormattedMessage {...messages.endsWith} values={{ code: creditCard.last_numbers }} />
+          </div>
+          <div>|</div>
+          <div>
+            {' '}
+            <FormattedMessage
+              {...expirationMessage}
+              values={{
+                month: creditCard.expiration_month.toLocaleString(undefined, {
+                  minimumIntegerDigits: 2,
+                }),
+                year: creditCard.expiration_year,
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CreditCardEmptyInline = () => {
+  return (
+    <div className="credit-card-selector__card">
+      <div className="credit-card-brand-logo--inline credit-card-selector__card__add-logo">
+        <span className="material-icons">add_card</span>
+      </div>
+      <div className="credit-card-selector__card__info">
+        <div className="credit-card-selector__card__info__meta">
+          <div>
+            <FormattedMessage {...messages.creditCardEmptyInlineDescription} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export interface CreditCardSelectorModalProps extends Pick<ModalProps, 'isOpen' | 'onClose'> {
+  defaultCreditCard?: CreditCard;
+  onChange: (creditCard?: CreditCard) => void;
+}
+
+const CreditCardSelectorModal = ({
+  defaultCreditCard,
+  onChange,
+  ...props
+}: CreditCardSelectorModalProps) => {
+  const intl = useIntl();
+  const { items: creditCards } = useCreditCardsManagement();
+  const [selected, setSelected] = useState(defaultCreditCard);
+  return (
+    <Modal
+      {...props}
+      size={ModalSize.MEDIUM}
+      title={intl.formatMessage(messages.modalTitle)}
+      actions={
+        <Button color="primary" size="small" fullWidth={true} onClick={() => onChange(selected)}>
+          <FormattedMessage {...messages.modalTitle} />
+        </Button>
+      }
+    >
+      <div className="credit-card-selector__modal" data-testid="CreditCardSelectorModal">
+        <div className="description mb-s">
+          <FormattedMessage {...messages.modalDescription} />
+        </div>
+        <RadioGroup fullWidth={true}>
+          {creditCards.map((creditCard) => (
+            <Radio
+              key={creditCard.id}
+              label={<CreditCardInline creditCard={creditCard} />}
+              name="credit_card"
+              value={creditCard.id}
+              fullWidth={true}
+              onChange={() => setSelected(creditCard)}
+              checked={selected?.id === creditCard.id}
+            />
+          ))}
+          <Radio
+            label={<CreditCardEmptyInline />}
+            name="credit_card"
+            value={undefined}
+            fullWidth={true}
+            onChange={() => setSelected(undefined)}
+            checked={!selected}
+          />
+        </RadioGroup>
+      </div>
+    </Modal>
+  );
+};
