d4f95df30be47edc273dd20dfe8ac2a9838db391
jbpenrath <jb.penrath@gmail.com>
2024-06-12 16:31:57 +0200

    💄(frontend) move SaleTunnelSponsors into the left column of SaleTunnel
    We are preparing the SaleTunnel to display payment schedule. Currently we are
    displaying training organizations below the purchase button. With the payment
    schedule, the course plan column looks a little bit empty and the layout looks
    unbalanced. So in order to improve layout, we decide to move SaleTunnelSponsors
    component into the left column

--- src/frontend/js/components/SaleTunnel/Sponsors/SaleTunnelSponsors.tsx
+++ src/frontend/js/components/SaleTunnel/Sponsors/SaleTunnelSponsors.tsx
@@ -1,20 +1,43 @@
+import { defineMessages, FormattedMessage } from 'react-intl';
 import { useSaleTunnelContext } from 'components/SaleTunnel/GenericSaleTunnel';
-import { DashboardAvatar } from 'widgets/Dashboard/components/DashboardAvatar';
+import {
+  DashboardAvatar,
+  DashboardAvatarVariantEnum,
+} from 'widgets/Dashboard/components/DashboardAvatar';
+import { Organization } from 'types/Joanie';
+
+const messages = defineMessages({
+  blockTitle: {
+    id: 'components.SaleTunnel.Sponsors.SaleTunnelSponsors.blockTitle',
+    defaultMessage: 'University',
+    description: 'Title for the universities section in the sale tunnel',
+  },
+});
 
 export const SaleTunnelSponsors = () => {
   const {
     props: { organizations },
   } = useSaleTunnelContext();
   return (
-    <div className="sale-tunnel__sponsors">
-      {organizations?.map((organization) => {
-        if (organization.logo) {
-          return (
-            <img key={organization.id} src={organization.logo!.src} alt={organization.title} />
-          );
-        }
-        return <DashboardAvatar key={organization.id} title={organization.title} />;
-      })}
-    </div>
+    <>
+      <h3 className="block-title">
+        <FormattedMessage {...messages.blockTitle} />
+      </h3>
+      <div className="sale-tunnel__sponsors">{organizations?.map(OrganizationLogo)}</div>
+    </>
+  );
+};
+
+const OrganizationLogo = (organization: Organization) => {
+  if (organization.logo) {
+    return <img key={organization.id} src={organization.logo!.src} alt={organization.title} />;
+  }
+
+  return (
+    <DashboardAvatar
+      key={organization.id}
+      title={organization.title}
+      variant={DashboardAvatarVariantEnum.SQUARE}
+    />
   );
 };
