1b356f0c13b5c7eaa57d3516a6eb02f6bfd19277
jbpenrath <jb.penrath@gmail.com>
2024-07-03 10:47:37 +0200

    ✨(frontend) manage tokenization with credit card query hook
    We recently add an api endpoint to tokenize credit cards so we update the Joanie
     API Interface and useCreditCards hook to manage this endpoint.

--- src/frontend/js/hooks/useCreditCards/index.ts
+++ src/frontend/js/hooks/useCreditCards/index.ts
@@ -1,8 +1,15 @@
-import { defineMessages } from 'react-intl';
-import { CreditCard } from 'types/Joanie';
-
+import { defineMessages, useIntl } from 'react-intl';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { API, CreditCard } from 'types/Joanie';
 import { useJoanieApi } from 'contexts/JoanieApiContext';
-import { useResource, useResources, UseResourcesProps } from '../useResources';
+import { useSessionMutation } from 'utils/react-query/useSessionMutation';
+import {
+  QueryOptions,
+  ResourcesQuery,
+  useResource,
+  useResourcesCustom,
+  UseResourcesProps,
+} from '../useResources';
 
 const messages = defineMessages({
   errorUpdate: {
@@ -20,10 +27,10 @@ const messages = defineMessages({
     description: 'Error message shown to the user when credit card deletion request fails.',
     defaultMessage: 'An error occurred while deleting the credit card. Please retry later.',
   },
-  errorCreate: {
-    id: 'hooks.useCreditCards.errorCreate',
-    description: 'Error message shown to the user when credit card creation request fails.',
-    defaultMessage: 'An error occurred while creating the credit card. Please retry later.',
+  errorTokenize: {
+    id: 'hooks.useCreditCards.errorTokenize',
+    description: 'Error message shown to the user when credit card tokenize request fails.',
+    defaultMessage: 'An error occurred while adding a credit card. Please retry later.',
   },
   errorNotFound: {
     id: 'hooks.useCreditCards.errorNotFound',
@@ -32,16 +39,47 @@ const messages = defineMessages({
   },
 });
 
+const useCreditCardResources =
+  (props: UseResourcesProps<CreditCard, ResourcesQuery, API['user']['creditCards']>) =>
+  (filters?: ResourcesQuery, queryOptions?: QueryOptions<CreditCard>) => {
+    const custom = useResourcesCustom({ ...props, filters, queryOptions });
+    const queryClient = useQueryClient();
+    const intl = useIntl();
+    const api = props.apiInterface();
+    const mutation = (props.session ? useSessionMutation : useMutation) as typeof useMutation;
+    const tokenizeHandler = mutation({
+      mutationFn: api.tokenize,
+      onSuccess: async () => {
+        custom.methods.setError(undefined);
+        props.onMutationSuccess?.(queryClient);
+      },
+      onError: () => custom.methods.setError(intl.formatMessage(messages.errorTokenize)),
+    });
+
+    return {
+      ...custom,
+      methods: {
+        ...custom.methods,
+        tokenize: tokenizeHandler.mutateAsync,
+      },
+      states: {
+        ...custom.states,
+        isPending: [tokenizeHandler, custom.states].some((value) => value?.isPending),
+        tokenizing: tokenizeHandler.isPending,
+      },
+    };
+  };
+
 /**
  * Joanie Api hook to retrieve/create/update/delete credit cards
  * owned by the authenticated user.
  */
-const props: UseResourcesProps<CreditCard> = {
+const props: UseResourcesProps<CreditCard, ResourcesQuery, API['user']['creditCards']> = {
   queryKey: ['creditCards'],
   apiInterface: () => useJoanieApi().user.creditCards,
   omniscient: true,
   session: true,
   messages,
 };
-export const useCreditCards = useResources<CreditCard>(props);
-export const useCreditCard = useResource<CreditCard>(props);
+export const useCreditCards = useCreditCardResources(props);
+export const useCreditCard = useResource(props);
