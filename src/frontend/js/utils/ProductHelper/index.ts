a7ea7289b2abcb0ec0dc228980719a351e0ca664
jbpenrath <jb.penrath@gmail.com>
2024-08-21 10:57:54 +0200

    ✨(frontend) add utils into ProductHelper
    Factorize some utils method by gathering them within the ProductHelper utils.
    hasRemainingSeats, hasOpenedTargetCourse, isPurchasable

--- src/frontend/js/utils/ProductHelper/index.ts
+++ src/frontend/js/utils/ProductHelper/index.ts
@@ -2,6 +2,8 @@ import { IntlShape } from 'react-intl';
 import { CourseProductRelation, Product, TargetCourse } from 'types/Joanie';
 import { Maybe } from 'types/utils';
 import { IntlHelper } from 'utils/IntlHelper';
+import * as Joanie from 'types/Joanie';
+import { isOpenedCourseRunCertificate, isOpenedCourseRunCredential } from 'utils/CourseRuns';
 
 /**
  * Helper class for products
@@ -44,5 +46,35 @@ export class ProductHelper {
 
   static getActiveOrderGroups(courseProductRelation: CourseProductRelation) {
     return courseProductRelation.order_groups?.filter((orderGroup) => orderGroup.is_active);
+  }
+
+  static hasRemainingSeats(product: Maybe<Product>) {
+    if (!product) return false;
+    return typeof product?.remaining_order_count !== 'number' || product.remaining_order_count > 0;
+  }
+
+  static hasOpenedTargetCourse(product: Maybe<Product>, enrollment?: Maybe<Joanie.Enrollment>) {
+    if (!product) return false;
+
+    if (product.type === Joanie.ProductType.CERTIFICATE) {
+      if (!enrollment?.course_run) {
+        throw new Error(
+          'Unable to check if the certificate product relies on an opened course run without enrollment.',
+        );
+      }
+
+      return isOpenedCourseRunCertificate(enrollment.course_run.state);
+    }
+
+    return (
+      product.target_courses.length > 0 &&
+      product.target_courses.every(({ course_runs }) =>
+        course_runs.some((targetCourseRun) => isOpenedCourseRunCredential(targetCourseRun.state)),
+      )
+    );
+  }
+
+  static isPurchasable(product: Maybe<Product>, enrollment?: Maybe<Joanie.Enrollment>) {
+    return this.hasOpenedTargetCourse(product, enrollment) && this.hasRemainingSeats(product);
   }
 }
