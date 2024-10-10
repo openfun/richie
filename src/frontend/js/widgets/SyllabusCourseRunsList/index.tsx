9b3578077d70f4e5736c3aed0841a829790adbb7
Tiago-Salles <tiagosalles20@gmail.com>
2024-10-10 16:05:49 +0000

    🐛(frontend) fix enroll action validation
    - adjust the condition to show a course as available
    - enroll now action component from link to button

--- src/frontend/js/widgets/SyllabusCourseRunsList/index.tsx
+++ src/frontend/js/widgets/SyllabusCourseRunsList/index.tsx
@@ -85,7 +85,7 @@ const SyllabusCourseRunsList = ({
         </div>
       )}
       {openedRuns.length === 1 &&
-        (course.is_self_paced && openedRuns[0].state.priority !== Priority.ARCHIVED_OPEN ? (
+        (course.is_self_paced && openedRuns[0].state.priority === Priority.ONGOING_OPEN ? (
           <div className="course-detail__row course-detail__runs course-detail__runs--open">
             <SyllabusCourseRunCompacted
               courseRun={openedRuns[0]}
