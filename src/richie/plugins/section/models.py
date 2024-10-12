33fe0eff49a6f779441c1bb8b126fe116a39d11d
David THENON <sveetch@gmail.com>
2024-10-12 00:28:18 +0000

    ✨(plugins) added grid columns and gutter option to section plugin
    The plugin 'section' now has grid options to apply grid column rules
    to its children contents. The grid define the column sizes rules
    and a gutter.
    
    Also this bring two new Makefile tasks to help with pending
    migrations.

--- src/richie/plugins/section/models.py
+++ src/richie/plugins/section/models.py
@@ -9,7 +9,7 @@ from django.utils.translation import gettext_lazy as _
 from cms.models.pluginmodel import CMSPlugin
 from djangocms_attributes_field.fields import AttributesField
 
-from .defaults import SECTION_TEMPLATES
+from .defaults import SECTION_GRID_COLUMNS, SECTION_TEMPLATES
 
 
 class Section(CMSPlugin):
@@ -33,6 +33,23 @@ class Section(CMSPlugin):
         verbose_name=_("Attributes"),
         blank=True,
         excluded_keys=["href", "target"],
+    )
+    grid_columns = models.CharField(
+        _("Grid"),
+        max_length=50,
+        blank=True,
+        choices=SECTION_GRID_COLUMNS,
+        default="",
+        help_text=_(
+            "Define a Grid to use for contents, when no Grid is enable each content "
+            "can define its own width."
+        ),
+    )
+    grid_gutter = models.BooleanField(
+        verbose_name=_("Grid gutter"),
+        default=True,
+        blank=True,
+        help_text=_("Enable a gutter between content items when Grid is enabled."),
     )
 
     def __str__(self):
