168d0db3e0671a8f7b190fc1c7e3eef2b267e729
jbpenrath <jb.penrath@gmail.com>
2024-11-04 16:33:06 +0100

    🏷️(frontend) update return type of CourseProductRelation endpoint
    Now the CourseProductRelation joanie endpoint return a light course product
    relation representation when we request a resource list and a detailed
    representation when we request a single resource. We have to update our types to
     reflect this api change.

--- src/frontend/js/components/CourseGlimpseList/utils.ts
+++ src/frontend/js/components/CourseGlimpseList/utils.ts
@@ -1,10 +1,10 @@
 import { IntlShape } from 'react-intl';
-import { CourseProductRelation, CourseListItem as JoanieCourse } from 'types/Joanie';
+import { CourseProductRelationLight, CourseListItem as JoanieCourse } from 'types/Joanie';
 import { Course as RichieCourse } from 'types/Course';
 import { CourseGlimpseCourse, getCourseGlimpseProps } from 'components/CourseGlimpse';
 
 export const getCourseGlimpseListProps = (
-  courses: RichieCourse[] | (JoanieCourse | CourseProductRelation)[],
+  courses: RichieCourse[] | (JoanieCourse | CourseProductRelationLight)[],
   intl?: IntlShape,
   organizationId?: string,
 ): CourseGlimpseCourse[] => {
