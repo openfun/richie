657adb12a5ad862b223d772d0b7b9a5327881e6d
Sandro Costa <sandro.costa@fccn.pt>
2025-01-23 15:07:45 +0000

    ♿️(search) add label to the search input
    Add a label to the `SearchInput` component to improve accessibility.

--- src/frontend/js/components/SearchInput/index.spec.tsx
+++ src/frontend/js/components/SearchInput/index.spec.tsx
@@ -10,14 +10,15 @@ describe('<SearchInput />', () => {
 
   const inputProps = {};
 
-  it('renders with the input field and button', () => {
-    const { container, getByText } = render(
+  it('renders with the input field, label and button', () => {
+    const { container, getByRole, getByLabelText } = render(
       <IntlProvider locale="en">
         <SearchInput context={contextProps} inputProps={inputProps} />
       </IntlProvider>,
     );
 
-    getByText('Search');
+    getByRole('button', { name: 'Search' });
+    getByLabelText('Search');
     // NB: we're searching the DOM as labelling and a11y for this input are handled by `react-autosuggest`
     expect(container.querySelector('input')).not.toBeNull();
     expect(container.innerHTML).toContain('icon-magnifying-glass');
@@ -26,13 +27,13 @@ describe('<SearchInput />', () => {
   it('triggers the passed callback on click', () => {
     const callback = jest.fn();
 
-    const { getByText } = render(
+    const { getByRole } = render(
       <IntlProvider locale="en">
         <SearchInput context={contextProps} inputProps={inputProps} onClick={callback} />
       </IntlProvider>,
     );
 
-    fireEvent.click(getByText('Search'));
+    fireEvent.click(getByRole('button', { name: 'Search' }));
     expect(callback).toHaveBeenCalledTimes(1);
   });
 });
