af09ab077d8c929420cc4cc80bf08ee3664ad79c
Tiago-Salles <tiagosalles20@gmail.com>
2025-02-05 11:54:58 +0000

    ✨(activate) improve activate script
    - Added in the activate script the possibility to
    select the database and the languages that the activating
    site will support. It also reflects in changes in the docker
    compose file in order to run the site with the selected database.

--- cookiecutter/{{cookiecutter.organization}}-richie-site-factory/template/{{cookiecutter.site}}/src/backend/{{cookiecutter.site}}/settings.py
+++ cookiecutter/{{cookiecutter.organization}}-richie-site-factory/template/{{cookiecutter.site}}/src/backend/{{cookiecutter.site}}/settings.py
@@ -2,6 +2,7 @@
 Django settings for the richie {{cookiecutter.site}} project.
 """
 
+import ast
 import json
 import os
 
@@ -469,6 +470,19 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
         "django.contrib.humanize",
     )
 
+    RFC_5646_LOCALES = [
+        "en-US",
+        "es-ES",
+        "pt-PT",
+        "pt-BR",
+        "fr-FR",
+        "fr-CA",
+        "ru-RU",
+        "vi-VN",
+        "ar-SA",
+        "ko-KR",
+    ]
+
     # Languages
     # - Django
     LANGUAGE_CODE = "en"
@@ -477,7 +491,14 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
     # fallback/default languages throughout the app.
     # Use "en" as default as it is the language that is most likely to be spoken by any visitor
     # when their preferred language, whatever it is, is unavailable
-    LANGUAGES = (("en", _("English")), ("fr", _("French")))
+    LANGUAGES = os.getenv("LANGUAGES") or [("en", _("English")), ("fr", _("French"))]
+
+    if isinstance(LANGUAGES, str):
+        try:
+            LANGUAGES = ast.literal_eval(LANGUAGES)
+            LANGUAGES = [(code, _(name)) for code, name in LANGUAGES]
+        except:
+            LANGUAGES = [("en", _("English")), ("fr", _("French"))]
 
     # - Django CMS
     CMS_LANGUAGES = {
@@ -490,20 +511,13 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
         1: [
             {
                 "public": True,
-                "code": "en",
+                "code": code,
                 "hide_untranslated": False,
-                "name": _("English"),
-                "fallbacks": ["fr"],
-                "redirect_on_fallback": False,
-            },
-            {
-                "public": True,
-                "code": "fr",
-                "hide_untranslated": False,
-                "name": _("French"),
+                "name": _(name),
                 "fallbacks": ["en"],
                 "redirect_on_fallback": False,
-            },
+            }
+            for code, name in LANGUAGES
         ],
     }
 
