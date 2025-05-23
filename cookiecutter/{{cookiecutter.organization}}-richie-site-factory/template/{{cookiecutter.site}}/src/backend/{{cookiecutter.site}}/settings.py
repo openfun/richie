7a7a6bce86d3320348db49e18a996c375a96e937
Nicolas Clerc <kernicpanel@nclerc.fr>
2025-05-23 12:25:59 +0200

    🩹(backend) add logging to cookiecutter settings
    When the logging setting has been added, cookiecutter has been forgot.

--- cookiecutter/{{cookiecutter.organization}}-richie-site-factory/template/{{cookiecutter.site}}/src/backend/{{cookiecutter.site}}/settings.py
+++ cookiecutter/{{cookiecutter.organization}}-richie-site-factory/template/{{cookiecutter.site}}/src/backend/{{cookiecutter.site}}/settings.py
@@ -545,7 +545,12 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
                 "level": "ERROR",
                 "handlers": ["console"],
                 "propagate": False,
-            }
+            },
+            "richie": {
+                "level": values.Value("ERROR", environ_name="RICHIE_LOGGING_LEVEL"),
+                "handlers": ["console"],
+                "propagate": False,
+            },
         },
     }
 
