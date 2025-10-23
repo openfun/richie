6c53db9d03b1b6fbd10033c9cd72fb79e45cecbe
Liam Le Strat <lestratliam@gmail.com>
2025-10-23 14:33:28 +0200

    ✨(frontend) add agreements to organization dashboard
    The organizations needs to be able to sign the
    agreements for the batch orders.
    That's why we add the
    agreements area to organization
    dashboard.

---
+++ src/frontend/js/pages/TeacherDashboardOrganizationAgreementsLayout/index.tsx
@@ -0,0 +1,25 @@
+import { defineMessages, FormattedMessage } from 'react-intl';
+import TeacherDashboardOrganizationAgreements from 'pages/TeacherDashboardOrganizationAgreements';
+import { DashboardLayout } from 'widgets/Dashboard/components/DashboardLayout';
+import { TeacherDashboardOrganizationSidebar } from 'widgets/Dashboard/components/TeacherDashboardOrganizationSidebar';
+
+const messages = defineMessages({
+  pageTitle: {
+    defaultMessage: 'Manage my agreements',
+    description: 'Use for the page title of the organization agreements',
+    id: 'pages.TeacherDashboardOrganizationAgreementsLayout.pageTitle',
+  },
+});
+
+export const TeacherDashboardOrganizationAgreementsLayout = () => {
+  return (
+    <DashboardLayout sidebar={<TeacherDashboardOrganizationSidebar />}>
+      <div className="dashboard__page_title_container">
+        <h1 className="dashboard__page_title">
+          <FormattedMessage {...messages.pageTitle} />
+        </h1>
+      </div>
+      <TeacherDashboardOrganizationAgreements />
+    </DashboardLayout>
+  );
+};
