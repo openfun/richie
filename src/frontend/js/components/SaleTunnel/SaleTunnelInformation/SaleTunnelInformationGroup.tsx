eee87925d266efa994987296762da4bb76da89e5
Liam Le Strat <lestratliam@gmail.com>
2025-12-19 13:49:17 +0100

    👽️(frontend) update billing batch order property
    The Joanie API use billing_address instead of billing attribute
    so we need to update our code to match the payload.

--- src/frontend/js/components/SaleTunnel/SaleTunnelInformation/SaleTunnelInformationGroup.tsx
+++ src/frontend/js/components/SaleTunnel/SaleTunnelInformation/SaleTunnelInformationGroup.tsx
@@ -105,7 +105,7 @@ export const validationSchema = Yup.object().shape({
   signatory_profession: Yup.string().required(),
   signatory_email: Yup.string().email().required(),
   signatory_telephone: Yup.string().required(),
-  billing: Yup.object().optional().shape({
+  billing_address: Yup.object().optional().shape({
     company_name: Yup.string().optional(),
     identification_number: Yup.string().optional(),
     contact_name: Yup.string().optional(),
