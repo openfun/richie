538a30bd65e19b06a83cdd43b7b406bf398e24ab
Liam Le Strat <lestratliam@gmail.com>
2026-03-24 15:43:51 +0100

    ✅(frontend) add new component in storybook
    Add a new story for syllabus course run
    compacted in storybook.

---
+++ src/frontend/js/widgets/SyllabusCourseRunsList/components/SyllabusCourseRunCompacted/index.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { CourseRunFactory } from 'utils/test/factories/richie';
+import { StorybookHelper } from 'utils/StorybookHelper';
+import { CourseCertificateOffer, CourseOffer } from 'types/Course';
+import { SyllabusCourseRunCompacted } from '.';
+
+export default {
+  component: SyllabusCourseRunCompacted,
+  render: (args) => StorybookHelper.wrapInApp(<SyllabusCourseRunCompacted {...args} />),
+} as Meta<typeof SyllabusCourseRunCompacted>;
+
+type Story = StoryObj<typeof SyllabusCourseRunCompacted>;
+
+const courseRun = CourseRunFactory().one();
+
+const singleDateCourseRun = {
+  ...courseRun,
+  start: null,
+  enrollment_start: null,
+  title: 'Certificate Product',
+  price_currency: 'EUR',
+  offer: CourseOffer.FREE,
+  certificate_offer: CourseCertificateOffer.PAID,
+  certificate_price: 100,
+  discounted_price: null,
+  discount: null,
+} as any;
+
+const course = {
+  id: '038c4f92-542a-4652-8537-322a75297b75',
+  code: 'IYfwh',
+  is_self_paced: true,
+};
+
+export const CertificateSyllabusSingleDate: Story = {
+  args: {
+    courseRun: singleDateCourseRun,
+    course,
+    showLanguages: true,
+  },
+};
