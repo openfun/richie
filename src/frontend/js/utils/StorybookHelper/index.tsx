0052dbc9b3da3134e357d7959cfac3b439ada36c
Liam Le Strat <lestratliam@gmail.com>
2026-03-24 12:08:05 +0100

    ✅(frontend) handle french language in storybook
    Enhance storybook to handle french language
    for all stories and all components.

--- src/frontend/js/utils/StorybookHelper/index.tsx
+++ src/frontend/js/utils/StorybookHelper/index.tsx
@@ -1,6 +1,5 @@
 import countries from 'i18n-iso-countries';
 import { lazy, ReactNode, Suspense } from 'react';
-import { IntlProvider } from 'react-intl';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { User } from 'types/User';
 import { createTestQueryClient, CreateTestQueryClientParams } from '../test/createTestQueryClient';
@@ -16,11 +15,9 @@ export class StorybookHelper {
     countries.registerLocale(require(`i18n-iso-countries/langs/en.json`));
     return (
       <QueryClientProvider client={client}>
-        <IntlProvider locale="en">
-          <Suspense fallback="Loading ...">
-            <LazyJoanieSessionProvider>{children}</LazyJoanieSessionProvider>
-          </Suspense>
-        </IntlProvider>
+        <Suspense fallback="Loading ...">
+          <LazyJoanieSessionProvider>{children}</LazyJoanieSessionProvider>
+        </Suspense>
       </QueryClientProvider>
     );
   }
